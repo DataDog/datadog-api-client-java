636ff4b70005427c00c0e11e2d2115264f74121f
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-11-25 15:24:37 -0500

    Start adding unit test framework

--- src/test/java/com/datadog/api/v1/client/api/V1ApiTest.java
+++ src/test/java/com/datadog/api/v1/client/api/V1ApiTest.java
@@ -1,15 +1,27 @@
 package com.datadog.api.v1.client.api;
 
+import java.io.IOException;
+
 import com.datadog.api.v1.client.ApiClient;
 import com.datadog.api.v1.client.auth.ApiKeyAuth;
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 
 public abstract class V1ApiTest {
     protected static ApiClient generalApiClient = null;
+    protected static ApiClient generalApiUnitTestClient = null;
 
     static String TEST_API_KEY_NAME="DD_TEST_CLIENT_API_KEY";
     static String TEST_APP_KEY_NAME="DD_TEST_CLIENT_APP_KEY";
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule();
 
     @BeforeClass
     public static void initGeneralApiClient() {
@@ -32,5 +44,37 @@ public abstract class V1ApiTest {
         }
         ApiKeyAuth appKeyAuth = (ApiKeyAuth) generalApiClient.getAuthentication("appKeyAuth");
         appKeyAuth.setApiKey(appKey);
+    }
+
+    @BeforeClass
+    public static void initGeneralApiUnitTestClient() {
+        generalApiUnitTestClient = new ApiClient();
+
+        // WireMock defaults to listening on localhost port 8080
+        // http://wiremock.org/docs/configuration/
+        generalApiUnitTestClient.setBasePath("http://localhost:8080");
+
+        // Configure API key authorization with fake key
+        ApiKeyAuth apiKeyAuth = (ApiKeyAuth) generalApiUnitTestClient.getAuthentication("apiKeyAuth");
+        apiKeyAuth.setApiKey(TEST_API_KEY_NAME);
+
+        // Configure API key authorization with fake key
+        ApiKeyAuth appKeyAuth = (ApiKeyAuth) generalApiUnitTestClient.getAuthentication("appKeyAuth");
+        appKeyAuth.setApiKey(TEST_APP_KEY_NAME);
+    }
+
+    public String getFixture(String path) throws IOException {
+        return IOUtils.toString(this.getClass().getResourceAsStream(path), "UTF-8");
+    }
+
+    public MappingBuilder beginStub(String Urlpath, String fixturePath ) throws IOException {
+        MappingBuilder stub = get(urlMatching(Urlpath + "?.*"))
+        .withQueryParam("api_key", equalTo(TEST_API_KEY_NAME))
+        .withQueryParam("application_key", equalTo(TEST_APP_KEY_NAME))
+        .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json")
+            .withBody(this.getFixture(fixturePath)));
+        return stub;
     }
 }
