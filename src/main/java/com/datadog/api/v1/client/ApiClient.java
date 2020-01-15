15701506eb5b1c65ae9522914a0b6fbafceedcf0
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-01-15 15:42:41 +0100

    Update API client

--- src/main/java/com/datadog/api/v1/client/ApiClient.java
+++ src/main/java/com/datadog/api/v1/client/ApiClient.java
@@ -31,7 +31,9 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.TimeZone;
@@ -49,12 +51,87 @@ import com.datadog.api.v1.client.auth.Authentication;
 import com.datadog.api.v1.client.auth.HttpBasicAuth;
 import com.datadog.api.v1.client.auth.HttpBearerAuth;
 import com.datadog.api.v1.client.auth.ApiKeyAuth;
+import com.datadog.api.v1.client.ServerConfiguration;
+import com.datadog.api.v1.client.ServerVariable;
+
 
 
 public class ApiClient {
   protected Map<String, String> defaultHeaderMap = new HashMap<String, String>();
   protected Map<String, String> defaultCookieMap = new HashMap<String, String>();
   protected String basePath = "https://api.datadoghq.com";
+  protected ServerConfiguration[] servers = {
+    new ServerConfiguration(
+      "https://{subdomain}.{site}",
+      "No description provided",
+      new HashMap<String, ServerVariable>() {{
+        put("site", new ServerVariable(
+          "The regional site for our customers.",
+          "datadoghq.com",
+          new HashSet<String>(
+            Arrays.asList(
+              "datadoghq.com",
+              "datadoghq.eu"
+            )
+          )
+        ));
+        put("subdomain", new ServerVariable(
+          "The subdomain where the API is deployed.",
+          "api",
+          new HashSet<String>(
+          )
+        ));
+      }}
+    ),
+    new ServerConfiguration(
+      "{protocol}://{name}",
+      "No description provided",
+      new HashMap<String, ServerVariable>() {{
+        put("name", new ServerVariable(
+          "Full site DNS name.",
+          "api.datadoghq.com",
+          new HashSet<String>(
+          )
+        ));
+        put("protocol", new ServerVariable(
+          "The protocol for accessing the API.",
+          "https",
+          new HashSet<String>(
+          )
+        ));
+      }}
+    )
+  };
+  protected Integer serverIndex = 0;
+  protected Map<String, String> serverVariables = null;
+  protected Map<String, List<ServerConfiguration>> operationServers = new HashMap<String, List<ServerConfiguration>>() {{
+    put("LogsHttpIntakeApi.sendLog", new ArrayList<ServerConfiguration>(Arrays.asList(
+      new ServerConfiguration(
+        "https://{subdomain}.{site}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+            put("site", new ServerVariable(
+            "The regional site for our customers.",
+            "datadoghq.com",
+            new HashSet<String>(
+              Arrays.asList(
+                "datadoghq.com",
+                "datadoghq.eu"
+              )
+            )
+          ));
+          put("subdomain", new ServerVariable(
+            "The subdomain where the API is deployed.",
+            "http-intake.logs",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      )
+    )));
+  }};
+  protected Map<String, Integer> operationServerIndex = new HashMap<String, Integer>();
+  protected Map<String, Map<String, String>> operationServerVariables = new HashMap<String, Map<String, String>>();
   protected boolean debugging = false;
   protected int connectionTimeout = 0;
   private int readTimeout = 0;
@@ -113,6 +190,33 @@ public class ApiClient {
 
   public ApiClient setBasePath(String basePath) {
     this.basePath = basePath;
+    return this;
+  }
+
+  public ServerConfiguration[] getServers() {
+    return servers;
+  }
+
+  public ApiClient setServers(ServerConfiguration[] servers) {
+    this.servers = servers;
+    return this;
+  }
+
+  public Integer getServerIndex() {
+    return serverIndex;
+  }
+
+  public ApiClient setServerIndex(Integer serverIndex) {
+    this.serverIndex = serverIndex;
+    return this;
+  }
+
+  public Map<String, String> getServerVariables() {
+    return serverVariables;
+  }
+
+  public ApiClient setServerVariables(Map<String, String> serverVariables) {
+    this.serverVariables = serverVariables;
     return this;
   }
 
@@ -665,6 +769,7 @@ public class ApiClient {
    * Invoke API by sending HTTP request with the given options.
    *
    * @param <T> Type
+   * @param operation The qualified name of the operation
    * @param path The sub-path of the HTTP URL
    * @param method The request method, one of "GET", "POST", "PUT", "HEAD" and "DELETE"
    * @param queryParams The query parameters
@@ -679,12 +784,24 @@ public class ApiClient {
    * @return The response body in type of string
    * @throws ApiException API exception
    */
-  public <T> ApiResponse<T> invokeAPI(String path, String method, List<Pair> queryParams, Object body, Map<String, String> headerParams, Map<String, String> cookieParams, Map<String, Object> formParams, String accept, String contentType, String[] authNames, GenericType<T> returnType) throws ApiException {
+  public <T> ApiResponse<T> invokeAPI(String operation, String path, String method, List<Pair> queryParams, Object body, Map<String, String> headerParams, Map<String, String> cookieParams, Map<String, Object> formParams, String accept, String contentType, String[] authNames, GenericType<T> returnType) throws ApiException {
     updateParamsForAuth(authNames, queryParams, headerParams, cookieParams);
 
-    // Not using `.target(this.basePath).path(path)` below,
+    // Not using `.target(targetURL).path(path)` below,
     // to support (constant) query string in `path`, e.g. "/posts?draft=1"
-    WebTarget target = httpClient.target(this.basePath + path);
+    String targetURL;
+    if (serverIndex != null) {
+      if (operationServers.containsKey(operation)) {
+        Integer index = operationServerIndex.getOrDefault(operation, serverIndex);
+        Map<String, String> variables = operationServerVariables.getOrDefault(operation, serverVariables);
+        targetURL = operationServers.get(operation)[index].URL(variables);
+      } else {
+        targetURL = servers[serverIndex].URL(serverVariables) + path;
+      }
+    } else {
+      targetURL = this.basePath + path;
+    }
+    WebTarget target = httpClient.target(targetURL);
 
     if (queryParams != null) {
       for (Pair queryParam : queryParams) {
