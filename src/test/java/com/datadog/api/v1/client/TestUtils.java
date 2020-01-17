6d9d02b8d51cd3843ec10ee93b161f51a3b977f9
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2020-01-17 19:22:25 +0100

    Add tests for metrics

---
+++ src/test/java/com/datadog/api/v1/client/TestUtils.java
@@ -0,0 +1,25 @@
+package com.datadog.api.v1.client;
+
+import java.util.function.BooleanSupplier;
+
+public class TestUtils {
+    public static class RetryException extends Exception {
+        public RetryException(String message){
+            super(message);
+        }
+    }
+
+    public static void retry(int interval, int count, BooleanSupplier call) throws RetryException {
+        for(int i = 0; i < count; i++) {
+       		if(call.getAsBoolean()) {
+       			return;
+       		}
+       		try {
+                Thread.sleep(interval * 1000);
+            } catch(InterruptedException e) {
+                return;
+            }
+       	}
+       	throw new RetryException(String.format("Retry error: failed to satisfy the condition after %d times", count));
+    }
+}
