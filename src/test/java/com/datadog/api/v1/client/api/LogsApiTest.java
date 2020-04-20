e2dcd250887b6d8f2514e2c958d698363288c255
skarimo <sherzod.karimov@COM11062.local>
2020-04-20 11:59:38 -0400

    remove unused imports

--- src/test/java/com/datadog/api/v1/client/api/LogsApiTest.java
+++ src/test/java/com/datadog/api/v1/client/api/LogsApiTest.java
@@ -17,7 +17,6 @@ import org.junit.Test;
 
 import javax.ws.rs.core.GenericType;
 import java.io.IOException;
-import java.time.OffsetDateTime;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
--- src/test/java/com/datadog/api/v1/client/api/LogsIndexesApiTest.java
+++ src/test/java/com/datadog/api/v1/client/api/LogsIndexesApiTest.java
@@ -22,7 +22,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
