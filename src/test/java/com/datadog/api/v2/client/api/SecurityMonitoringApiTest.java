b0f4a7289b67a96752e20e2bda98f48994fd8735
Alexandre Trufanow <alexandre.trufanow@datadoghq.com>
2020-07-02 11:20:43 +0200

    Add tests for SecMon list signals

--- src/test/java/com/datadog/api/v2/client/api/SecurityMonitoringApiTest.java
+++ src/test/java/com/datadog/api/v2/client/api/SecurityMonitoringApiTest.java
@@ -7,17 +7,44 @@
 
 package com.datadog.api.v2.client.api;
 
-import com.datadog.api.v2.client.ApiException;
-import com.datadog.api.v2.client.model.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+
+import com.datadog.api.RecordingMode;
+import com.datadog.api.TestUtils;
+import com.datadog.api.v2.client.ApiException;
+import com.datadog.api.v2.client.model.SecurityMonitoringListRulesResponse;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleCase;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleCreatePayload;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleEvaluationWindow;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleKeepAlive;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleMaxSignalDuration;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleOptions;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleQuery;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleResponse;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleSeverity;
+import com.datadog.api.v2.client.model.SecurityMonitoringRuleUpdatePayload;
+import com.datadog.api.v2.client.model.SignalListRequest;
+import com.datadog.api.v2.client.model.SignalListRequestFilter;
+import com.datadog.api.v2.client.model.SignalListRequestPage;
+import com.datadog.api.v2.client.model.SignalsListResponse;
+import com.datadog.api.v2.client.model.SignalsSort;
+import java.net.URLEncoder;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import javax.ws.rs.core.GenericType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * API tests for SecurityMonitoringApi
@@ -30,6 +57,18 @@ public class SecurityMonitoringApiTest extends V2APITest {
     @Override
     public String getTracingEndpoint() {
         return "security-monitoring";
+    }
+
+    @BeforeClass
+    public static void enableUnstableOperations() {
+        generalApiClient.setUnstableOperationEnabled("listSignals", true);
+        generalApiClient.setUnstableOperationEnabled("listSignalsGet", true);
+    }
+
+    @AfterClass
+    public static void disableUnstableOperations() {
+        generalApiClient.setUnstableOperationEnabled("listSignals", false);
+        generalApiClient.setUnstableOperationEnabled("listSignalsGet", false);
     }
 
     @Before
@@ -126,22 +165,195 @@ public class SecurityMonitoringApiTest extends V2APITest {
         }
     }
 
+    @Test
+    public void listSignalsTest() throws Exception {
+        String uniqueName = getUniqueEntityName();
+        SecurityMonitoringRuleResponse rule = createRule(uniqueName);
+
+        sendLogs(uniqueName);
+
+        SignalListRequestFilter allSignalsFilter = new SignalListRequestFilter()
+                .query(uniqueName)
+                .from(now.minus(Duration.ofHours(1)))
+                .to(now.plus(Duration.ofHours(1)));
+
+        // Make sure both logs are indexed
+        SignalListRequest bothSignalsRequest = new SignalListRequest()
+                .filter(allSignalsFilter);
+        TestUtils.retry(10, 10, () -> {
+            try {
+                SignalsListResponse response = api.listSignals().body(bothSignalsRequest).execute();
+                return response.getData() != null && response.getData().size() == 2;
+            } catch (ApiException ignored) {
+                return false;
+            }
+        });
+
+        // Sort works correctly
+        SignalsListResponse responseAscending = api.listSignals()
+                .body(new SignalListRequest()
+                        .filter(allSignalsFilter)
+                        .sort(SignalsSort.TIMESTAMP_ASCENDING))
+                .execute();
+
+        assertEquals(2, responseAscending.getData().size());
+        OffsetDateTime firstTimestamp = responseAscending.getData().get(0).getAttributes().getTimestamp();
+        OffsetDateTime secondTimestamp = responseAscending.getData().get(1).getAttributes().getTimestamp();
+        assertTrue(firstTimestamp.isBefore(secondTimestamp));
+
+        SignalsListResponse responseDescending = api.listSignals()
+                .body(new SignalListRequest()
+                        .filter(allSignalsFilter)
+                        .sort(SignalsSort.TIMESTAMP_DESCENDING))
+                .execute();
+
+        assertEquals(2, responseDescending.getData().size());
+        firstTimestamp = responseDescending.getData().get(0).getAttributes().getTimestamp();
+        secondTimestamp = responseDescending.getData().get(1).getAttributes().getTimestamp();
+        assertTrue(firstTimestamp.isAfter(secondTimestamp));
+
+        // Paging
+        SignalsListResponse pageOneResponse = api.listSignals()
+                .body(new SignalListRequest()
+                        .filter(allSignalsFilter)
+                        .page(new SignalListRequestPage().limit(1)))
+                .execute();
+        assertEquals(1, pageOneResponse.getData().size());
+
+        String cursor = pageOneResponse.getMeta().getPage().getAfter();
+        assertTrue(pageOneResponse.getLinks().getNext().contains(URLEncoder.encode(cursor)));
+
+        SignalsListResponse pageTwoResponse = api.listSignals()
+                .body(new SignalListRequest()
+                        .filter(allSignalsFilter)
+                        .page(new SignalListRequestPage()
+                                .cursor(cursor)
+                                .limit(1)))
+                .execute();
+        assertEquals(1, pageTwoResponse.getData().size());
+
+        assertNotEquals(pageOneResponse.getData().get(0).getId(), pageTwoResponse.getData().get(0).getId());
+    }
+
+    @Test
+    public void listSignalsGet() throws Exception {
+        String uniqueName = getUniqueEntityName();
+        SecurityMonitoringRuleResponse rule = createRule(uniqueName);
+
+        sendLogs(uniqueName);
+
+        TestUtils.retry(5, 10, () -> {
+            try {
+                SignalsListResponse response = api.listSignalsGet()
+                        .filterQuery(uniqueName)
+                        .filterFrom(now.minus(Duration.ofHours(1)))
+                        .filterTo(now.plus(Duration.ofHours(1)))
+                        .execute();
+                return response.getData() != null && response.getData().size() == 2;
+            } catch (ApiException ignored) {
+                return false;
+            }
+        });
+
+        // Sort works correctly
+        SignalsListResponse responseAscending = api.listSignalsGet()
+                .filterQuery(uniqueName)
+                .filterFrom(now.minus(Duration.ofHours(1)))
+                .filterTo(now.plus(Duration.ofHours(1)))
+                .sort(SignalsSort.TIMESTAMP_ASCENDING)
+                .execute();
+
+        assertEquals(2, responseAscending.getData().size());
+        OffsetDateTime firstTimestamp = responseAscending.getData().get(0).getAttributes().getTimestamp();
+        OffsetDateTime secondTimestamp = responseAscending.getData().get(1).getAttributes().getTimestamp();
+        assertTrue(firstTimestamp.isBefore(secondTimestamp));
+
+        SignalsListResponse responseDescending = api.listSignalsGet()
+                .filterQuery(uniqueName)
+                .filterFrom(now.minus(Duration.ofHours(1)))
+                .filterTo(now.plus(Duration.ofHours(1)))
+                .sort(SignalsSort.TIMESTAMP_DESCENDING)
+                .execute();
+
+        assertEquals(2, responseDescending.getData().size());
+        firstTimestamp = responseAscending.getData().get(0).getAttributes().getTimestamp();
+        secondTimestamp = responseAscending.getData().get(1).getAttributes().getTimestamp();
+        assertTrue(firstTimestamp.isAfter(secondTimestamp));
+
+        // Paging
+        SignalsListResponse pageOneResponse = api.listSignalsGet()
+                .filterQuery(uniqueName)
+                .filterFrom(now.minus(Duration.ofHours(1)))
+                .filterTo(now.plus(Duration.ofHours(1)))
+                .pageLimit(1)
+                .execute();
+
+        assertEquals(1, pageOneResponse.getData().size());
+
+        String cursor = pageOneResponse.getMeta().getPage().getAfter();
+        assertTrue(pageOneResponse.getLinks().getNext().contains(URLEncoder.encode(cursor)));
+
+        SignalsListResponse pageTwoResponse = api.listSignalsGet()
+                .filterQuery(uniqueName)
+                .filterFrom(now.minus(Duration.ofHours(1)))
+                .filterTo(now.plus(Duration.ofHours(1)))
+                .pageLimit(1)
+                .pageCursor(cursor)
+                .execute();
+
+        assertEquals(1, pageTwoResponse.getData().size());
+
+        assertNotEquals(pageOneResponse.getData().get(0).getId(), pageTwoResponse.getData().get(0).getId());
+
+        api.deleteSecurityMonitoringRule(rule.getId());
+    }
+
+    private void sendLogs(String message) throws Exception {
+        String intakeURL;
+        if (TestUtils.getRecordingMode().equals(RecordingMode.MODE_REPLAYING)) {
+            // when running from cassettes, we need to make sure that the default base URL
+            // is used for mock server certificates to work properly
+            intakeURL = "/v1/input";
+        } else {
+            intakeURL = String.format("https://http-intake.logs.%s/v1/input", testDomain());
+        }
+        sendRequest(
+                "POST",
+                intakeURL,
+                String.format(
+                        "{\"ddtags\":\"java,test,list\",\"message\":\"{\\\"timestamp\\\": %d, \\\"message\\\": \\\"%s\\\"}\"}",
+                        (now.toEpochSecond() - 1000) * 1000, message
+                ),
+                new GenericType<String>(String.class)
+        );
+        sendRequest(
+                "POST",
+                intakeURL,
+                String.format(
+                        "{\"ddtags\":\"java,test,list\",\"message\":\"{\\\"timestamp\\\": %d, \\\"message\\\": \\\"%s\\\"}\"}",
+                        now.toEpochSecond() * 1000, message
+                ),
+                new GenericType<String>(String.class)
+        );
+    }
+
     private SecurityMonitoringRuleResponse createRule(String ruleName) throws ApiException {
         SecurityMonitoringRuleCreatePayload createRulePayload = new SecurityMonitoringRuleCreatePayload();
         createRulePayload
                 .name(ruleName)
                 .enabled(true)
                 .addQueriesItem(new SecurityMonitoringRuleQuery()
-                        .name("nevermatch")
-                        .query("thiswillnevernevermatch")
+                        .name("rule")
+                        .query(ruleName)
                         .groupByFields(Collections.emptyList()))
                 .options(new SecurityMonitoringRuleOptions()
-                        .evaluationWindow(SecurityMonitoringRuleEvaluationWindow.FIVE_MINUTES)
-                        .keepAlive(SecurityMonitoringRuleKeepAlive.FIVE_MINUTES)
-                        .maxSignalDuration(SecurityMonitoringRuleMaxSignalDuration.FIVE_MINUTES))
+                        // Generate a signal every log
+                        .evaluationWindow(SecurityMonitoringRuleEvaluationWindow.ZERO_MINUTES)
+                        .keepAlive(SecurityMonitoringRuleKeepAlive.ZERO_MINUTES)
+                        .maxSignalDuration(SecurityMonitoringRuleMaxSignalDuration.ZERO_MINUTES))
                 .addCasesItem(new SecurityMonitoringRuleCase()
                         .name("rule-case")
-                        .condition("nevermatch > 1000")
+                        .condition("rule > 0")
                         .status(SecurityMonitoringRuleSeverity.INFO))
                 .message("Rule message")
                 .tags(Collections.singletonList("datadog-api-client-test-java"));
