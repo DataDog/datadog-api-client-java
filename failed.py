c686db57adeea8ae98eea2e8f2e3dd5e3872f6b4
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2020-07-21 16:12:16 +0200

    Update failed.py

--- failed.py
+++ failed.py
@@ -8,4 +8,3 @@ for path in pathlib.Path('target').rglob('TEST-*.xml'):
     for suite in JUnitXml.fromfile(path):
         if suite.result is not None and suite.result._tag != 'skipped':
             print('mvn', 'test', f'-Dtest={suite.classname}#{suite.name}')
-            # subprocess.run(['mvn', 'test', f'-Dtest={suite.classname}'], shell=True, check=True)
