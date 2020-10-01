bb24fdec6e76f6517d73a641abfbf325c872c790
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2020-10-01 16:05:30 -0400

    Add test for HasSearchBar

--- src/test/java/com/datadog/api/v1/client/api/DashboardsApiTest.java
+++ src/test/java/com/datadog/api/v1/client/api/DashboardsApiTest.java
@@ -417,7 +417,8 @@ public class DashboardsApiTest extends V1ApiTest{
                 ).title("Test Table Widget").titleAlign(WidgetTextAlign.CENTER).titleSize("16").time(new WidgetTime().liveSpan(WidgetLiveSpan.PAST_FIFTEEN_MINUTES))
                 .addCustomLinksItem(new WidgetCustomLink()
                         .label("Test Custom Link label").link("https://app.datadoghq.com/dashboard/lists")
-                );
+                )
+                .hasSearchBar("auto");
         Widget tableWidget = new Widget().definition(new WidgetDefinition(tableWidgetDefinition));
         orderedWidgetList.add(tableWidget);
 
