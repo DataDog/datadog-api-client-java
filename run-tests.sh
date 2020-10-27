561c7d04455b0c7bec44fc2887c9f3964dc7c601
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2020-10-27 16:17:36 +0100

    Update run-tests.sh
    Co-authored-by: Nicholas Muesch <nicholas.muesch@datadoghq.com>

--- run-tests.sh
+++ run-tests.sh
@@ -34,7 +34,7 @@ fi
 RERUN_COUNT=$([ "$CI" == "true" ] && echo "1" || echo "0")
 
 set +e
-mvn --show-version --batch-mode -Dsurefire.rerunFailingTestsCount=${RERUN_COUNT} -Dtest=MonitorsApiTest test
+mvn --show-version --batch-mode -Dsurefire.rerunFailingTestsCount=${RERUN_COUNT} test
 RESULT=$?
 if [ "$RESULT" -ne 0 -a ! -d "target/surefire-reports" ]; then
     # No surefire reports and error means compilation error, don't try to rerun
