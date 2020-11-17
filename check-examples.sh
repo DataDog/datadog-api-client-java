2ac75daffaf9f690442698a7fcf056661b733353
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-11-17 17:14:13 +0100

    fix

--- check-examples.sh
+++ check-examples.sh
@@ -8,7 +8,7 @@ for f in examples/v*/*/*.java ; do
     cp $f $df/Example.java
 done
 
-ls examples/v*/*/*/Example.java | xargs -P $(($(nproc)*2)) -n 1 javac -sourcepath src/main/java/
+ls examples/v*/*/*/Example.java | xargs -P $(($(nproc)*2)) -n 1 javac -cp "target/lib/*" -sourcepath src/main/java/
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
