6f0e39f1eb9fa7b4372036695698c7773d90c60a
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-11-17 16:40:26 +0100

    fix

--- check-examples.sh
+++ check-examples.sh
@@ -2,15 +2,13 @@
 
 ./extract-code-blocks.sh
 
-
 for f in examples/v*/*/*.java ; do
     df=$(dirname $f)/$(basename $f .java)
     mkdir -p $df
     cp $f $df/Example.java
 done
 
-
-ls examples/v*/*/*/Example.java | xargs -P $(($(nproc)*2)) -n 1 javac --source-path src/main/java/
+ls examples/v*/*/*/Example.java | xargs -P $(($(nproc)*2)) -n 1 javac -sourcepath src/main/java/
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
