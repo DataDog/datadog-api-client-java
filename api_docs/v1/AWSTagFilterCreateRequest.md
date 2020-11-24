fd2eee8b01783812bf1bb7481ab144b1a81c1808
ci.datadog-api-spec <packages@datadoghq.com>
2020-11-24 15:02:41 +0000

    Regenerate client from commit 217b6e8 of spec repo

---
+++ api_docs/v1/AWSTagFilterCreateRequest.md
@@ -0,0 +1,15 @@
+
+
+# AWSTagFilterCreateRequest
+
+The objects used to set an AWS tag filter.
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**accountId** | **String** | Your AWS Account ID without dashes. |  [optional]
+**namespace** | [**AWSNamespace**](AWSNamespace.md) |  |  [optional]
+**tagFilterStr** | **String** | The tag filter string. |  [optional]
+
+
+
--- src/main/java/com/datadog/api/v1/client/ApiClient.java
+++ src/main/java/com/datadog/api/v1/client/ApiClient.java
@@ -117,6 +117,132 @@ public class ApiClient extends JavaTimeFormatter {
   protected Integer serverIndex = 0;
   protected Map<String, String> serverVariables = null;
   protected Map<String, List<ServerConfiguration>> operationServers = new HashMap<String, List<ServerConfiguration>>() {{
+    put("AwsIntegrationApi.createAWSTagFilter", new ArrayList<ServerConfiguration>(Arrays.asList(
+      new ServerConfiguration(
+        "https://{subdomain}.{site}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+          put("site", new ServerVariable(
+            "The regional site for our customers.",
+            "datadoghq.com",
+            new HashSet<String>(
+              Arrays.asList(
+                "datadoghq.com"
+              )
+            )
+          ));
+          put("subdomain", new ServerVariable(
+            "The subdomain where the API is deployed.",
+            "api",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      ),
+
+      new ServerConfiguration(
+        "{protocol}://{name}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+          put("name", new ServerVariable(
+            "Full site DNS name.",
+            "api.datadoghq.com",
+            new HashSet<String>(
+            )
+          ));
+          put("protocol", new ServerVariable(
+            "The protocol for accessing the API.",
+            "https",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      )
+    )));
+    put("AwsIntegrationApi.deleteAWSTagFilter", new ArrayList<ServerConfiguration>(Arrays.asList(
+      new ServerConfiguration(
+        "https://{subdomain}.{site}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+          put("site", new ServerVariable(
+            "The regional site for our customers.",
+            "datadoghq.com",
+            new HashSet<String>(
+              Arrays.asList(
+                "datadoghq.com"
+              )
+            )
+          ));
+          put("subdomain", new ServerVariable(
+            "The subdomain where the API is deployed.",
+            "api",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      ),
+
+      new ServerConfiguration(
+        "{protocol}://{name}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+          put("name", new ServerVariable(
+            "Full site DNS name.",
+            "api.datadoghq.com",
+            new HashSet<String>(
+            )
+          ));
+          put("protocol", new ServerVariable(
+            "The protocol for accessing the API.",
+            "https",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      )
+    )));
+    put("AwsIntegrationApi.listAWSTagFilters", new ArrayList<ServerConfiguration>(Arrays.asList(
+      new ServerConfiguration(
+        "https://{subdomain}.{site}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+          put("site", new ServerVariable(
+            "The regional site for our customers.",
+            "datadoghq.com",
+            new HashSet<String>(
+              Arrays.asList(
+                "datadoghq.com"
+              )
+            )
+          ));
+          put("subdomain", new ServerVariable(
+            "The subdomain where the API is deployed.",
+            "api",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      ),
+
+      new ServerConfiguration(
+        "{protocol}://{name}",
+        "No description provided",
+        new HashMap<String, ServerVariable>() {{
+          put("name", new ServerVariable(
+            "Full site DNS name.",
+            "api.datadoghq.com",
+            new HashSet<String>(
+            )
+          ));
+          put("protocol", new ServerVariable(
+            "The protocol for accessing the API.",
+            "https",
+            new HashSet<String>(
+            )
+          ));
+        }}
+      )
+    )));
     put("IpRangesApi.getIPRanges", new ArrayList<ServerConfiguration>(Arrays.asList(
       new ServerConfiguration(
         "https://{subdomain}.{site}",
