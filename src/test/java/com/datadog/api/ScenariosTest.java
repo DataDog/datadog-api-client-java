081cd4af5d2fe431ef43d3cbe701ba6523598a40
Jiri Kuncar <jiri.kuncar@datadoghq.com>
2020-11-25 17:05:27 +0100

    Add DisableTestTrace annotation

--- src/test/java/com/datadog/api/ScenariosTest.java
+++ src/test/java/com/datadog/api/ScenariosTest.java
@@ -4,11 +4,13 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 import org.junit.runner.RunWith;
 
+import datadog.trace.api.DisableTestTrace;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "summary",
         "com.datadog.api.TracePlugin", "rerun:target/rerun.txt" }, snippets = CAMELCASE, tags = "not @skip")
+@DisableTestTrace
 public class ScenariosTest {
 }
