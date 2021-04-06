bd50e76faefe29dacdb999142f1028e6799c0851
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-06 17:44:44 +0200

    added unique_lower_alnum + updated cassettes

--- src/test/java/com/datadog/api/ClientSteps.java
+++ src/test/java/com/datadog/api/ClientSteps.java
@@ -55,6 +55,14 @@ public class ClientSteps {
             m -> {
               return "";
             }));
+    world.context.put(
+        "unique_lower_alnum",
+        World.replace(
+            unique,
+            Pattern.compile("[^a-z0-9]+"),
+            m -> {
+              return "";
+            }));
     world.context.put("now_ts", world.now.toEpochSecond());
     world.context.put("now_iso", world.now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
     world.context.put("hour_later_ts", world.now.plusHours(1).toEpochSecond());
