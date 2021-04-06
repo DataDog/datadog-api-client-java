3db476a634d42de041bdbf5a1f07f7e1cbac2524
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-07 00:20:20 +0200

    refactored unique_lower_alnum

--- src/test/java/com/datadog/api/ClientSteps.java
+++ src/test/java/com/datadog/api/ClientSteps.java
@@ -55,14 +55,7 @@ public class ClientSteps {
             m -> {
               return "";
             }));
-    world.context.put(
-        "unique_lower_alnum",
-        World.replace(
-            unique,
-            Pattern.compile("[^a-z0-9]+"),
-            m -> {
-              return "";
-            }));
+    world.context.put("unique_lower_alnum", world.context.get("unique_alnum").toLowerCase());
     world.context.put("now_ts", world.now.toEpochSecond());
     world.context.put("now_iso", world.now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
     world.context.put("hour_later_ts", world.now.plusHours(1).toEpochSecond());
