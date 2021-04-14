e2885c25ad61dc237dc6a73e5d56173a6ab9b0b6
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-14 22:50:52 +0200

    updated format.sh

--- format.sh
+++ format.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-find ./src -name '*.java' | xargs java -jar /app/google-java-format.jar -i
+docker build -t google-java-format -f Dockerfile.format .
+
+FILES_TO_FORMAT=$(find ./src -name '*.java' | xargs docker run -v ${PWD}/src:/app/src google-java-format --dry-run)
+
+if [ -n "${FILES_TO_FORMAT}" ]; then
+    docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed ${FILES_TO_FORMAT}
+    # How to commit the formatted files?
+fi
