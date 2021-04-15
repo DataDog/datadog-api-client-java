1aba7fa34874eaa27077685fe4ab950e45a55780
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-15 12:36:20 +0200

    check if files are changed before formatting

--- format.sh
+++ format.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-docker build -t google-java-format -f Dockerfile.format .
-
-docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed $@
+if [ $# -ne 0 ]; then
+    docker build -t google-java-format -f Dockerfile.format .
+    docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed $@
+fi
