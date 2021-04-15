bbd820f8c34d13a4b536285432165715e55854a6
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-15 11:59:27 +0200

    used arguments passed from pre-commit instead of running google-java-format in dry mode

--- format.sh
+++ format.sh
@@ -2,9 +2,4 @@
 
 docker build -t google-java-format -f Dockerfile.format .
 
-FILES_TO_FORMAT=$(find ./src -name '*.java' | xargs docker run -v ${PWD}/src:/app/src google-java-format --dry-run)
-
-if [ -n "${FILES_TO_FORMAT}" ]; then
-    docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed ${FILES_TO_FORMAT}
-    # How to commit the formatted files?
-fi
+docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed $@
