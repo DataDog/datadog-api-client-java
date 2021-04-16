3104eda77d4964f44947d61682d277e027f3e951
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-16 18:20:10 +0200

    updated format.sh

--- format.sh
+++ format.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-if [ $# -ne 0 ]; then
-    docker build -t google-java-format -f Dockerfile.format .
-    docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed $@
-fi
+find "$@" -name '*.java' | xargs java -jar /app/google-java-format.jar -i
