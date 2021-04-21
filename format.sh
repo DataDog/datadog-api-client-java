7d6e5abf65becb64fa7555f49a699aefe7ed3b67
Noueman Khalikine <noueman.khal@gmail.com>
2021-04-21 14:55:48 +0200

    added pre-commit hook to format non-generated java files

--- format.sh
+++ format.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-find "$@" -name '*.java' | xargs java -jar /app/google-java-format.jar -i
+if [ $# -ne 0 ]; then
+    JAR_FILE="/app/google-java-format.jar"
+    if [[ -e $JAR_FILE ]]; then
+        find "$@" -name '*.java' | xargs java -jar /app/google-java-format.jar -i
+    else
+        docker build -t google-java-format -f Dockerfile.format .
+        docker run -v ${PWD}/src:/app/src google-java-format --replace --set-exit-if-changed $@
+    fi
+fi
