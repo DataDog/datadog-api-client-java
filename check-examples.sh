bc08824eba67399683d44126254baadee253d13d
Thomas Hervé <thomas.herve@datadoghq.com>
2021-05-05 10:27:00 +0200

    Try to fetch dependencies

--- check-examples.sh
+++ check-examples.sh
@@ -13,9 +13,9 @@ for f in examples/$1/*/*.java ; do
     sed -i.bak "s/public class Example/public class $filename/" $f
 done
 
-mvn -DskipTests -Dmaven.javadoc.skip=true clean install
+mvn --batch-mode  dependency:copy-dependencies
 
-ls examples/$1/*/*.java | xargs javac -cp "target/lib/*" -sourcepath src/main/java/
+ls examples/$1/*/*.java | xargs javac -cp "target/dependency/*" -sourcepath src/main/java/
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
