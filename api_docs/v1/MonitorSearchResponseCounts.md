3c03fb3ffc050da98937e765945e3dd78698a445
ci.datadog-api-spec <packages@datadoghq.com>
2021-05-27 09:22:03 +0000

    Regenerate client from commit 8c512ec of spec repo

---
+++ api_docs/v1/MonitorSearchResponseCounts.md
@@ -0,0 +1,17 @@
+
+
+# MonitorSearchResponseCounts
+
+The counts of monitors per different criteria.
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**muted** | [**List&lt;Object&gt;**](Object.md) | Search facets. |  [optional]
+**status** | [**List&lt;Object&gt;**](Object.md) | Search facets. |  [optional]
+**tag** | [**List&lt;Object&gt;**](Object.md) | Search facets. |  [optional]
+**type** | [**List&lt;Object&gt;**](Object.md) | Search facets. |  [optional]
+
+
+
--- src/main/java/com/datadog/api/v1/client/api/MonitorsApi.java
+++ src/main/java/com/datadog/api/v1/client/api/MonitorsApi.java
@@ -8,6 +8,8 @@ import com.datadog.api.v1.client.Pair;
 import com.datadog.api.v1.client.model.CheckCanDeleteMonitorResponse;
 import com.datadog.api.v1.client.model.DeletedMonitor;
 import com.datadog.api.v1.client.model.Monitor;
+import com.datadog.api.v1.client.model.MonitorGroupSearchResponse;
+import com.datadog.api.v1.client.model.MonitorSearchResponse;
 import com.datadog.api.v1.client.model.MonitorUpdateRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -852,6 +854,328 @@ public class MonitorsApi {
 
     return apiClient.invokeAPI(
         "MonitorsApi.listMonitors",
+        localVarPath,
+        "GET",
+        localVarQueryParams,
+        localVarPostBody,
+        localVarHeaderParams,
+        localVarCookieParams,
+        localVarFormParams,
+        localVarAccept,
+        localVarContentType,
+        localVarAuthNames,
+        localVarReturnType,
+        false);
+  }
+
+  /** Manage optional parameters to searchMonitorGroups. */
+  public static class SearchMonitorGroupsOptionalParameters {
+    private String query;
+    private Long page;
+    private Long perPage;
+    private String sort;
+
+    /**
+     * Set query
+     *
+     * @param query After entering a search query in your [Manage Monitor page][1] use the query
+     *     parameter value in the URL of the page as value for this parameter. Consult the dedicated
+     *     [manage monitor documentation][2] page to learn more. The query can contain any number of
+     *     space-separated monitor attributes, for instance &#x60;query&#x3D;\&quot;type:metric
+     *     status:alert\&quot;&#x60;. [1]: https://app.datadoghq.com/monitors/manage [2]:
+     *     /monitors/manage_monitor/#find-the-monitors (optional)
+     * @return SearchMonitorGroupsOptionalParameters
+     */
+    public SearchMonitorGroupsOptionalParameters query(String query) {
+      this.query = query;
+      return this;
+    }
+
+    /**
+     * Set page
+     *
+     * @param page Page to start paginating from. (optional, default to 0)
+     * @return SearchMonitorGroupsOptionalParameters
+     */
+    public SearchMonitorGroupsOptionalParameters page(Long page) {
+      this.page = page;
+      return this;
+    }
+
+    /**
+     * Set perPage
+     *
+     * @param perPage Number of monitors to return per page. (optional, default to 30)
+     * @return SearchMonitorGroupsOptionalParameters
+     */
+    public SearchMonitorGroupsOptionalParameters perPage(Long perPage) {
+      this.perPage = perPage;
+      return this;
+    }
+
+    /**
+     * Set sort
+     *
+     * @param sort String for sort order, composed of field and sort order separate by a comma, e.g.
+     *     &#x60;name,asc&#x60;. Supported sort directions: &#x60;asc&#x60;, &#x60;desc&#x60;.
+     *     Supported fields: * &#x60;name&#x60; * &#x60;status&#x60; * &#x60;tags&#x60; (optional)
+     * @return SearchMonitorGroupsOptionalParameters
+     */
+    public SearchMonitorGroupsOptionalParameters sort(String sort) {
+      this.sort = sort;
+      return this;
+    }
+  }
+
+  /**
+   * Monitors group search Search and filter your monitor groups details.
+   *
+   * @return MonitorGroupSearchResponse
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table summary="Response Details" border="1">
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public MonitorGroupSearchResponse searchMonitorGroups() throws ApiException {
+    return searchMonitorGroupsWithHttpInfo(new SearchMonitorGroupsOptionalParameters()).getData();
+  }
+
+  /**
+   * Monitors group search Search and filter your monitor groups details.
+   *
+   * @param parameters Optional parameters for the request.
+   * @return MonitorGroupSearchResponse
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table summary="Response Details" border="1">
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public MonitorGroupSearchResponse searchMonitorGroups(
+      SearchMonitorGroupsOptionalParameters parameters) throws ApiException {
+    return searchMonitorGroupsWithHttpInfo(parameters).getData();
+  }
+
+  /**
+   * Monitors group search Search and filter your monitor groups details.
+   *
+   * @param parameters Optional parameters for the request.
+   * @return ApiResponse&lt;MonitorGroupSearchResponse&gt;
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table summary="Response Details" border="1">
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public ApiResponse<MonitorGroupSearchResponse> searchMonitorGroupsWithHttpInfo(
+      SearchMonitorGroupsOptionalParameters parameters) throws ApiException {
+    Object localVarPostBody = null;
+    String query = parameters.query;
+    Long page = parameters.page;
+    Long perPage = parameters.perPage;
+    String sort = parameters.sort;
+    // create path and map variables
+    String localVarPath = "/api/v1/monitor/groups/search";
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "query", query));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "page", page));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "per_page", perPage));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "sort", sort));
+
+    // Set Operation-ID header for telemetry
+    localVarHeaderParams.put("DD-OPERATION-ID", "searchMonitorGroups");
+
+    final String[] localVarAccepts = {"application/json"};
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {};
+
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] {"apiKeyAuth", "appKeyAuth"};
+
+    GenericType<MonitorGroupSearchResponse> localVarReturnType =
+        new GenericType<MonitorGroupSearchResponse>() {};
+
+    return apiClient.invokeAPI(
+        "MonitorsApi.searchMonitorGroups",
+        localVarPath,
+        "GET",
+        localVarQueryParams,
+        localVarPostBody,
+        localVarHeaderParams,
+        localVarCookieParams,
+        localVarFormParams,
+        localVarAccept,
+        localVarContentType,
+        localVarAuthNames,
+        localVarReturnType,
+        false);
+  }
+
+  /** Manage optional parameters to searchMonitors. */
+  public static class SearchMonitorsOptionalParameters {
+    private String query;
+    private Long page;
+    private Long perPage;
+    private String sort;
+
+    /**
+     * Set query
+     *
+     * @param query After entering a search query in your [Manage Monitor page][1] use the query
+     *     parameter value in the URL of the page as value for this parameter. Consult the dedicated
+     *     [manage monitor documentation][2] page to learn more. The query can contain any number of
+     *     space-separated monitor attributes, for instance &#x60;query&#x3D;\&quot;type:metric
+     *     status:alert\&quot;&#x60;. [1]: https://app.datadoghq.com/monitors/manage [2]:
+     *     /monitors/manage_monitor/#find-the-monitors (optional)
+     * @return SearchMonitorsOptionalParameters
+     */
+    public SearchMonitorsOptionalParameters query(String query) {
+      this.query = query;
+      return this;
+    }
+
+    /**
+     * Set page
+     *
+     * @param page Page to start paginating from. (optional, default to 0)
+     * @return SearchMonitorsOptionalParameters
+     */
+    public SearchMonitorsOptionalParameters page(Long page) {
+      this.page = page;
+      return this;
+    }
+
+    /**
+     * Set perPage
+     *
+     * @param perPage Number of monitors to return per page. (optional, default to 30)
+     * @return SearchMonitorsOptionalParameters
+     */
+    public SearchMonitorsOptionalParameters perPage(Long perPage) {
+      this.perPage = perPage;
+      return this;
+    }
+
+    /**
+     * Set sort
+     *
+     * @param sort String for sort order, composed of field and sort order separate by a comma, e.g.
+     *     &#x60;name,asc&#x60;. Supported sort directions: &#x60;asc&#x60;, &#x60;desc&#x60;.
+     *     Supported fields: * &#x60;name&#x60; * &#x60;status&#x60; * &#x60;tags&#x60; (optional)
+     * @return SearchMonitorsOptionalParameters
+     */
+    public SearchMonitorsOptionalParameters sort(String sort) {
+      this.sort = sort;
+      return this;
+    }
+  }
+
+  /**
+   * Monitors search Search and filter your monitors details.
+   *
+   * @return MonitorSearchResponse
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table summary="Response Details" border="1">
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public MonitorSearchResponse searchMonitors() throws ApiException {
+    return searchMonitorsWithHttpInfo(new SearchMonitorsOptionalParameters()).getData();
+  }
+
+  /**
+   * Monitors search Search and filter your monitors details.
+   *
+   * @param parameters Optional parameters for the request.
+   * @return MonitorSearchResponse
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table summary="Response Details" border="1">
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public MonitorSearchResponse searchMonitors(SearchMonitorsOptionalParameters parameters)
+      throws ApiException {
+    return searchMonitorsWithHttpInfo(parameters).getData();
+  }
+
+  /**
+   * Monitors search Search and filter your monitors details.
+   *
+   * @param parameters Optional parameters for the request.
+   * @return ApiResponse&lt;MonitorSearchResponse&gt;
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table summary="Response Details" border="1">
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public ApiResponse<MonitorSearchResponse> searchMonitorsWithHttpInfo(
+      SearchMonitorsOptionalParameters parameters) throws ApiException {
+    Object localVarPostBody = null;
+    String query = parameters.query;
+    Long page = parameters.page;
+    Long perPage = parameters.perPage;
+    String sort = parameters.sort;
+    // create path and map variables
+    String localVarPath = "/api/v1/monitor/search";
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "query", query));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "page", page));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "per_page", perPage));
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "sort", sort));
+
+    // Set Operation-ID header for telemetry
+    localVarHeaderParams.put("DD-OPERATION-ID", "searchMonitors");
+
+    final String[] localVarAccepts = {"application/json"};
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {};
+
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] {"apiKeyAuth", "appKeyAuth"};
+
+    GenericType<MonitorSearchResponse> localVarReturnType =
+        new GenericType<MonitorSearchResponse>() {};
+
+    return apiClient.invokeAPI(
+        "MonitorsApi.searchMonitors",
         localVarPath,
         "GET",
         localVarQueryParams,
