7a079f124a82cae3ff3f3767798820ceae9eaeda
Thomas Hervé <thomas.herve@datadoghq.com>
2021-12-22 10:57:45 +0100

    Use local java for format

--- format.sh
+++ format.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+JAVA=$(which java)
+
 if [ $# -ne 0 ]; then
-    JAR_FILE="/app/google-java-format.jar"
-    if [[ -e $JAR_FILE ]]; then
-        find "$@" -name '*.java' | xargs java -jar /app/google-java-format.jar -i
+    if [[ -x "$JAVA" ]]; then
+        JAR_FILE="google-java-format.jar"
+        if [[ ! -f "$JAR_FILE" ]]; then
+            curl -L https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar -o $JAR_FILE
+        fi
+        find "$@" -name '*.java' | xargs java -jar $JAR_FILE -i
     else
         docker build -t google-java-format -f Dockerfile.format .
         docker run -v ${PWD}:/app/src --workdir /app/src google-java-format --replace --set-exit-if-changed $(find "$@" -name '*.java')
