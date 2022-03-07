4b6d034cb9bf3508be8351e40e339186454483d9
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-07 15:54:54 +0000

    Regenerate client from commit 1eab6b6 of spec repo

---
+++ examples/v2/rum/ListRUMEvents.java
@@ -0,0 +1,27 @@
+// Get a list of RUM events returns "OK" response
+
+import com.datadog.api.v2.client.ApiClient;
+import com.datadog.api.v2.client.ApiException;
+import com.datadog.api.v2.client.Configuration;
+import com.datadog.api.v2.client.api.RumApi;
+import com.datadog.api.v2.client.model.RUMEventsResponse;
+import java.time.*;
+import java.util.*;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = Configuration.getDefaultApiClient();
+    RumApi apiInstance = new RumApi(defaultClient);
+
+    try {
+      RUMEventsResponse result = apiInstance.listRUMEvents();
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling RumApi#listRUMEvents");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
---
+++ examples/v2/rum/SearchRUMEvents.java
@@ -0,0 +1,45 @@
+// Search RUM events returns "OK" response
+
+import com.datadog.api.v2.client.ApiClient;
+import com.datadog.api.v2.client.ApiException;
+import com.datadog.api.v2.client.Configuration;
+import com.datadog.api.v2.client.api.RumApi;
+import com.datadog.api.v2.client.api.RumApi.SearchRUMEventsOptionalParameters;
+import com.datadog.api.v2.client.model.RUMEventsResponse;
+import com.datadog.api.v2.client.model.RUMQueryFilter;
+import com.datadog.api.v2.client.model.RUMQueryOptions;
+import com.datadog.api.v2.client.model.RUMQueryPageOptions;
+import com.datadog.api.v2.client.model.RUMSearchEventsRequest;
+import com.datadog.api.v2.client.model.RUMSort;
+import java.time.*;
+import java.util.*;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = Configuration.getDefaultApiClient();
+    RumApi apiInstance = new RumApi(defaultClient);
+
+    RUMSearchEventsRequest body =
+        new RUMSearchEventsRequest()
+            .filter(
+                new RUMQueryFilter()
+                    .from("now-15m")
+                    .query("@type:session AND @session.type:user")
+                    .to("now"))
+            .options(new RUMQueryOptions().timeOffset(0L).timezone("GMT"))
+            .page(new RUMQueryPageOptions().limit(25))
+            .sort(RUMSort.TIMESTAMP_ASCENDING);
+
+    try {
+      RUMEventsResponse result =
+          apiInstance.searchRUMEvents(new SearchRUMEventsOptionalParameters().body(body));
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling RumApi#searchRUMEvents");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
