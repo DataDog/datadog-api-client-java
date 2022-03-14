ae26579c26e97ffd07944c86db9675f32b0b47bb
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-14 22:38:54 +0000

    Regenerate client from commit 99fc1893 of spec repo

--- src/main/java/com/datadog/api/v1/client/model/FormulaAndFunctionResponseFormat.java
+++ src/main/java/com/datadog/api/v1/client/model/FormulaAndFunctionResponseFormat.java
@@ -23,7 +23,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-/** Timeseries or Scalar response. **This feature is currently in beta.** */
+/** Timeseries or Scalar response. */
 @JsonSerialize(
     using = FormulaAndFunctionResponseFormat.FormulaAndFunctionResponseFormatSerializer.class)
 public class FormulaAndFunctionResponseFormat {
