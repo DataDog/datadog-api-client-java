09f76758301f42097e4e0912d7fe0338606e0985
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-16 14:36:10 +0000

    Regenerate client from commit e10259da of spec repo

---
+++ examples/v1/logs/ListLogs_235998668.java
@@ -0,0 +1,40 @@
+// Search test logs returns "OK" response
+import com.datadog.api.v1.client.ApiClient;
+import com.datadog.api.v1.client.ApiException;
+import com.datadog.api.v1.client.Configuration;
+import com.datadog.api.v1.client.api.LogsApi;
+import com.datadog.api.v1.client.model.LogsListRequest;
+import com.datadog.api.v1.client.model.LogsListRequestTime;
+import com.datadog.api.v1.client.model.LogsListResponse;
+import com.datadog.api.v1.client.model.LogsSort;
+import java.time.*;
+import java.time.OffsetDateTime;
+import java.util.*;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = Configuration.getDefaultApiClient();
+    LogsApi apiInstance = new LogsApi(defaultClient);
+
+    LogsListRequest body =
+        new LogsListRequest()
+            .index("main")
+            .query("service:web*")
+            .sort(LogsSort.TIME_ASCENDING)
+            .time(
+                new LogsListRequestTime()
+                    .from(OffsetDateTime.now().plusHours(-1).toInstant().getEpochSecond())
+                    .to(OffsetDateTime.now().toInstant().getEpochSecond()));
+
+    try {
+      LogsListResponse result = apiInstance.listLogs(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling LogsApi#listLogs");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
