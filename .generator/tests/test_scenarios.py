3bf03dd4a52d6d0b67e21ab708397680fd06c355
Thomas Hervé <thomas.herve@datadoghq.com>
2022-05-04 09:18:46 +0200

    Generate examples in repository
    This moves example generation in pre-commit.

---
+++ .generator/tests/test_scenarios.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""Test scenarios."""
+
+from pytest_bdd import scenarios
+
+scenarios(
+    "../../src/test/resources/com/datadog/api/v1/client/api", "../../src/test/resources/com/datadog/api/v1/client/api"
+)
--- examples/v1/synthetics/CreateSyntheticsAPITest.java
+++ examples/v1/synthetics/CreateSyntheticsAPITest.java
@@ -9,21 +9,16 @@ import com.datadog.api.v1.client.model.SyntheticsAPITest;
 import com.datadog.api.v1.client.model.SyntheticsAPITestConfig;
 import com.datadog.api.v1.client.model.SyntheticsAPITestType;
 import com.datadog.api.v1.client.model.SyntheticsAssertion;
-import com.datadog.api.v1.client.model.SyntheticsAssertionJSONPathOperator;
-import com.datadog.api.v1.client.model.SyntheticsAssertionJSONPathTarget;
-import com.datadog.api.v1.client.model.SyntheticsAssertionJSONPathTargetTarget;
 import com.datadog.api.v1.client.model.SyntheticsAssertionOperator;
 import com.datadog.api.v1.client.model.SyntheticsAssertionTarget;
 import com.datadog.api.v1.client.model.SyntheticsAssertionType;
-import com.datadog.api.v1.client.model.SyntheticsConfigVariable;
-import com.datadog.api.v1.client.model.SyntheticsConfigVariableType;
+import com.datadog.api.v1.client.model.SyntheticsDeviceID;
 import com.datadog.api.v1.client.model.SyntheticsTestDetailsSubType;
 import com.datadog.api.v1.client.model.SyntheticsTestOptions;
+import com.datadog.api.v1.client.model.SyntheticsTestOptionsMonitorOptions;
 import com.datadog.api.v1.client.model.SyntheticsTestOptionsRetry;
+import com.datadog.api.v1.client.model.SyntheticsTestPauseStatus;
 import com.datadog.api.v1.client.model.SyntheticsTestRequest;
-import com.datadog.api.v1.client.model.SyntheticsTestRequestCertificate;
-import com.datadog.api.v1.client.model.SyntheticsTestRequestCertificateItem;
-import com.datadog.api.v1.client.model.SyntheticsTestRequestProxy;
 import java.time.*;
 import java.util.*;
 
@@ -37,98 +32,29 @@ public class Example {
             .config(
                 new SyntheticsAPITestConfig()
                     .assertions(
-                        new ArrayList<SyntheticsAssertion>() {
-                          {
-                            add(
-                                new SyntheticsAssertion(
-                                    new SyntheticsAssertionTarget()
-                                        .operator(SyntheticsAssertionOperator.IS)
-                                        .property("{{ PROPERTY }}")
-                                        .target("text/html")
-                                        .type(SyntheticsAssertionType.HEADER)));
-                            add(
-                                new SyntheticsAssertion(
-                                    new SyntheticsAssertionTarget()
-                                        .operator(SyntheticsAssertionOperator.LESS_THAN)
-                                        .target(2000)
-                                        .type(SyntheticsAssertionType.RESPONSE_TIME)));
-                            add(
-                                new SyntheticsAssertion(
-                                    new SyntheticsAssertionJSONPathTarget()
-                                        .operator(
-                                            SyntheticsAssertionJSONPathOperator.VALIDATES_JSON_PATH)
-                                        .target(
-                                            new SyntheticsAssertionJSONPathTargetTarget()
-                                                .jsonPath("topKey")
-                                                .operator("isNot")
-                                                .targetValue("0"))
-                                        .type(SyntheticsAssertionType.BODY)));
-                          }
-                        })
-                    .configVariables(
-                        new ArrayList<SyntheticsConfigVariable>() {
-                          {
-                            add(
-                                new SyntheticsConfigVariable()
-                                    .example("content-type")
-                                    .name("PROPERTY")
-                                    .pattern("content-type")
-                                    .type(SyntheticsConfigVariableType.TEXT));
-                          }
-                        })
+                        Collections.singletonList(
+                            new SyntheticsAssertion(
+                                new SyntheticsAssertionTarget()
+                                    .operator(SyntheticsAssertionOperator.LESS_THAN)
+                                    .target(1000)
+                                    .type(SyntheticsAssertionType.RESPONSE_TIME))))
                     .request(
                         new SyntheticsTestRequest()
-                            .certificate(
-                                new SyntheticsTestRequestCertificate()
-                                    .cert(
-                                        new SyntheticsTestRequestCertificateItem()
-                                            .content("cert-content")
-                                            .filename("cert-filename")
-                                            .updatedAt("2020-10-16T09:23:24.857Z"))
-                                    .key(
-                                        new SyntheticsTestRequestCertificateItem()
-                                            .content("key-content")
-                                            .filename("key-filename")
-                                            .updatedAt("2020-10-16T09:23:24.857Z")))
-                            .headers(
-                                Map.ofEntries(
-                                    Map.entry(
-                                        "unique",
-                                        "examplecreateanapitestreturnsokreturnsthecreatedtestdetailsresponse")))
                             .method(HTTPMethod.GET)
-                            .timeout(10.0)
-                            .url("https://datadoghq.com")
-                            .proxy(
-                                new SyntheticsTestRequestProxy()
-                                    .url("https://datadoghq.com")
-                                    .headers(Map.ofEntries()))))
-            .locations(
-                new ArrayList<String>() {
-                  {
-                    add("aws:us-east-2");
-                  }
-                })
-            .message("BDD test payload: synthetics_api_test_payload.json")
-            .name("Example-Create_an_API_test_returns_OK_Returns_the_created_test_details_response")
+                            .url("https://example.com")))
+            .locations(Collections.singletonList("aws:eu-west-3"))
+            .message("Notification message")
+            .name("Example test name")
             .options(
                 new SyntheticsTestOptions()
-                    .acceptSelfSigned(false)
-                    .allowInsecure(true)
-                    .followRedirects(true)
-                    .minFailureDuration(10L)
-                    .minLocationFailed(1L)
-                    .monitorName(
-                        "Example-Create_an_API_test_returns_OK_Returns_the_created_test_details_response")
-                    .monitorPriority(5)
-                    .retry(new SyntheticsTestOptionsRetry().count(3L).interval(10.0))
-                    .tickEvery(60L))
+                    .deviceIds(Collections.singletonList(SyntheticsDeviceID.LAPTOP_LARGE))
+                    .monitorOptions(new SyntheticsTestOptionsMonitorOptions())
+                    .restrictedRoles(
+                        Collections.singletonList("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"))
+                    .retry(new SyntheticsTestOptionsRetry()))
+            .status(SyntheticsTestPauseStatus.LIVE)
             .subtype(SyntheticsTestDetailsSubType.HTTP)
-            .tags(
-                new ArrayList<String>() {
-                  {
-                    add("testing:api");
-                  }
-                })
+            .tags(Collections.singletonList("env:production"))
             .type(SyntheticsAPITestType.API);
 
     try {
