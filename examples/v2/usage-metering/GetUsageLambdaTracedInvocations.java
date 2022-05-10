f14141947a87578fb3b9dbf678ba5ca699f77eec
ci.datadog-api-spec <packages@datadoghq.com>
2022-05-10 15:40:55 +0000

    Regenerate client from commit f1e731d3 of spec repo

---
+++ examples/v2/usage-metering/GetUsageLambdaTracedInvocations.java
@@ -0,0 +1,30 @@
+// Get hourly usage for Lambda Traced Invocations returns "OK" response
+import com.datadog.api.v2.client.ApiClient;
+import com.datadog.api.v2.client.ApiException;
+import com.datadog.api.v2.client.Configuration;
+import com.datadog.api.v2.client.api.UsageMeteringApi;
+import com.datadog.api.v2.client.api.UsageMeteringApi.GetUsageLambdaTracedInvocationsOptionalParameters;
+import com.datadog.api.v2.client.model.UsageLambdaTracedInvocationsResponse;
+import java.time.OffsetDateTime;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = Configuration.getDefaultApiClient();
+    UsageMeteringApi apiInstance = new UsageMeteringApi(defaultClient);
+
+    try {
+      UsageLambdaTracedInvocationsResponse result =
+          apiInstance.getUsageLambdaTracedInvocations(
+              OffsetDateTime.now().plusDays(-5),
+              new GetUsageLambdaTracedInvocationsOptionalParameters()
+                  .endHr(OffsetDateTime.now().plusDays(-3)));
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling UsageMeteringApi#getUsageLambdaTracedInvocations");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
