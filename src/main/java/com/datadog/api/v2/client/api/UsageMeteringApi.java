0b5068482fcf22ffdff42d60a88cbce4bb4a3f42
ci.datadog-api-spec <packages@datadoghq.com>
2022-05-24 09:17:17 +0000

    Regenerate client from commit 99066a28 of spec repo

--- src/main/java/com/datadog/api/v2/client/api/UsageMeteringApi.java
+++ src/main/java/com/datadog/api/v2/client/api/UsageMeteringApi.java
@@ -133,7 +133,8 @@ public class UsageMeteringApi {
   }
 
   /**
-   * Get cost across multi-org account.
+   * Get cost across multi-org account. Cost by org data for a given month becomes available no
+   * later than the 16th of the following month.
    *
    * @param startMonth Datetime in ISO-8601 format, UTC, precise to month: <code>[YYYY-MM]</code>
    *     for cost beginning this month. (required)
