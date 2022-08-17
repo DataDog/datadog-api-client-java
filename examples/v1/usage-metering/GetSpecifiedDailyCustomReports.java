4a460e2c186e6f5999d499e4b9983a8964f5260c
ci.datadog-api-spec <packages@datadoghq.com>
2022-08-17 14:33:19 +0000

    Regenerate client from commit a957cd49 of spec repo

--- examples/v1/usage-metering/GetSpecifiedDailyCustomReports.java
+++ examples/v1/usage-metering/GetSpecifiedDailyCustomReports.java
@@ -8,7 +8,6 @@ import com.datadog.api.client.v1.model.UsageSpecifiedCustomReportsResponse;
 public class Example {
   public static void main(String[] args) {
     ApiClient defaultClient = ApiClient.getDefaultApiClient();
-    defaultClient.setUnstableOperationEnabled("v1.getSpecifiedDailyCustomReports", true);
     UsageMeteringApi apiInstance = new UsageMeteringApi(defaultClient);
 
     try {
