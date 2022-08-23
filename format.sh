56825340f7e86d0a9f2a4ca477f3fccd33fb6d9b
Thomas Hervé <thomas.herve@datadoghq.com>
2022-08-23 11:05:47 +0200

    Ease running local formatting
    To not use docker let's nmake the default location for the format jar
    easier to set.

--- format.sh
+++ format.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ $# -ne 0 ]; then
-    JAR_FILE="/app/google-java-format.jar"
+    JAR_FILE="google-java-format.jar"
     if [[ -e $JAR_FILE ]]; then
-        find "$@" -name '*.java' | xargs java -jar /app/google-java-format.jar -i
+        find "$@" -name '*.java' | xargs java -jar $JAR_FILE -i
     else
         docker build -t google-java-format -f Dockerfile.format .
         docker run --rm -v ${PWD}:/app/src --workdir /app/src google-java-format --replace --set-exit-if-changed $(find "$@" -name '*.java')
