b15bc9863f07a02d8378984c5df8504e19c78b06
Thomas Hervé <thomas.herve@datadoghq.com>
2023-02-09 17:11:05 +0100

    Fix searchSLO unstable setting in examples
    There was an extra camel_case call for some reasons.

--- examples/v1/service-level-objectives/SearchSLO.java
+++ examples/v1/service-level-objectives/SearchSLO.java
@@ -9,7 +9,7 @@ import com.datadog.api.client.v1.model.SearchSLOResponse;
 public class Example {
   public static void main(String[] args) {
     ApiClient defaultClient = ApiClient.getDefaultApiClient();
-    defaultClient.setUnstableOperationEnabled("v1.searchSlo", true);
+    defaultClient.setUnstableOperationEnabled("v1.searchSLO", true);
     ServiceLevelObjectivesApi apiInstance = new ServiceLevelObjectivesApi(defaultClient);
 
     // there is a valid "slo" in the system
