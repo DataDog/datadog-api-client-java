f7129be94dbfcdfee81866ad4a0ef91225ef417d
ci.datadog-api-spec <packages@datadoghq.com>
2023-02-17 18:17:38 +0000

    Regenerate client from commit 0a1c3444 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/IPAllowlistEntryData.java
@@ -0,0 +1,151 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.Objects;
+
+/** Data of the IP allowlist entry object. */
+@JsonPropertyOrder({
+  IPAllowlistEntryData.JSON_PROPERTY_ATTRIBUTES,
+  IPAllowlistEntryData.JSON_PROPERTY_ID,
+  IPAllowlistEntryData.JSON_PROPERTY_TYPE
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class IPAllowlistEntryData {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ATTRIBUTES = "attributes";
+  private IPAllowlistEntryAttributes attributes;
+
+  public static final String JSON_PROPERTY_ID = "id";
+  private String id;
+
+  public static final String JSON_PROPERTY_TYPE = "type";
+  private IPAllowlistEntryType type = IPAllowlistEntryType.IP_ALLOWLIST_ENTRY;
+
+  public IPAllowlistEntryData() {}
+
+  @JsonCreator
+  public IPAllowlistEntryData(
+      @JsonProperty(required = true, value = JSON_PROPERTY_TYPE) IPAllowlistEntryType type) {
+    this.type = type;
+    this.unparsed |= !type.isValid();
+  }
+
+  public IPAllowlistEntryData attributes(IPAllowlistEntryAttributes attributes) {
+    this.attributes = attributes;
+    this.unparsed |= attributes.unparsed;
+    return this;
+  }
+
+  /**
+   * Attributes of the IP allowlist entry.
+   *
+   * @return attributes
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ATTRIBUTES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public IPAllowlistEntryAttributes getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(IPAllowlistEntryAttributes attributes) {
+    this.attributes = attributes;
+  }
+
+  public IPAllowlistEntryData id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * The unique identifier of the IP allowlist entry.
+   *
+   * @return id
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ID)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public IPAllowlistEntryData type(IPAllowlistEntryType type) {
+    this.type = type;
+    this.unparsed |= !type.isValid();
+    return this;
+  }
+
+  /**
+   * IP allowlist Entry type.
+   *
+   * @return type
+   */
+  @JsonProperty(JSON_PROPERTY_TYPE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public IPAllowlistEntryType getType() {
+    return type;
+  }
+
+  public void setType(IPAllowlistEntryType type) {
+    if (!type.isValid()) {
+      this.unparsed = true;
+    }
+    this.type = type;
+  }
+
+  /** Return true if this IPAllowlistEntryData object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    IPAllowlistEntryData ipAllowlistEntryData = (IPAllowlistEntryData) o;
+    return Objects.equals(this.attributes, ipAllowlistEntryData.attributes)
+        && Objects.equals(this.id, ipAllowlistEntryData.id)
+        && Objects.equals(this.type, ipAllowlistEntryData.type);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(attributes, id, type);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class IPAllowlistEntryData {\n");
+    sb.append("    attributes: ").append(toIndentedString(attributes)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
