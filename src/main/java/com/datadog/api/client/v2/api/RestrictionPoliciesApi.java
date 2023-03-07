e171911f1dd81197e01e2b625f7b812e9a53b281
ci.datadog-api-spec <packages@datadoghq.com>
2023-03-07 18:55:40 +0000

    Regenerate client from commit f09bf63c of spec repo

--- src/main/java/com/datadog/api/client/v2/api/RestrictionPoliciesApi.java
+++ src/main/java/com/datadog/api/client/v2/api/RestrictionPoliciesApi.java
@@ -51,7 +51,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @throws ApiException if fails to make API call
    */
   public void deleteRestrictionPolicy(String resourceId) throws ApiException {
@@ -65,7 +65,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return CompletableFuture
    */
   public CompletableFuture<Void> deleteRestrictionPolicyAsync(String resourceId) {
@@ -81,7 +81,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return ApiResponse&lt;Void&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -138,7 +138,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return CompletableFuture&lt;ApiResponse&lt;Void&gt;&gt;
    */
   public CompletableFuture<ApiResponse<Void>> deleteRestrictionPolicyWithHttpInfoAsync(
@@ -196,7 +196,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return RestrictionPolicyResponse
    * @throws ApiException if fails to make API call
    */
@@ -211,7 +211,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
    */
   public CompletableFuture<RestrictionPolicyResponse> getRestrictionPolicyAsync(String resourceId) {
@@ -227,7 +227,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return ApiResponse&lt;RestrictionPolicyResponse&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -284,7 +284,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @return CompletableFuture&lt;ApiResponse&lt;RestrictionPolicyResponse&gt;&gt;
    */
   public CompletableFuture<ApiResponse<RestrictionPolicyResponse>>
@@ -342,7 +342,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @param body Restriction policy payload (required)
    * @return RestrictionPolicyResponse
    * @throws ApiException if fails to make API call
@@ -359,7 +359,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @param body Restriction policy payload (required)
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
    */
@@ -379,11 +379,11 @@ public class RestrictionPoliciesApi {
    *
    * <p>Restriction policies can be applied to the following resources: - Connections: <code>
    * connection</code> - Dashboards: <code>dashboard</code> - Notebooks: <code>notebook</code> -
-   * Security Rules: <code>security-rule</code>
+   * Security Rules: <code>security-rule</code> - Service Level Objectives: <code>slo</code>
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @param body Restriction policy payload (required)
    * @return ApiResponse&lt;RestrictionPolicyResponse&gt;
    * @throws ApiException if fails to make API call
@@ -447,7 +447,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>. (required)
+   *     </code>, <code>slo</code>. (required)
    * @param body Restriction policy payload (required)
    * @return CompletableFuture&lt;ApiResponse&lt;RestrictionPolicyResponse&gt;&gt;
    */
