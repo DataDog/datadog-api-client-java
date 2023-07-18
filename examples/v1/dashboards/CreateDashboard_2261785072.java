7b544bf1e02935a695642cd64ce0720e66fc1db7
ci.datadog-api-spec <packages@datadoghq.com>
2023-07-18 13:53:58 +0000

    Regenerate client from commit cbddd387 of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_2261785072.java
@@ -0,0 +1,86 @@
+// Create a new dashboard with a timeseries widget and an overlay request
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.DashboardsApi;
+import com.datadog.api.client.v1.model.Dashboard;
+import com.datadog.api.client.v1.model.DashboardLayoutType;
+import com.datadog.api.client.v1.model.FormulaAndFunctionMetricDataSource;
+import com.datadog.api.client.v1.model.FormulaAndFunctionMetricQueryDefinition;
+import com.datadog.api.client.v1.model.FormulaAndFunctionQueryDefinition;
+import com.datadog.api.client.v1.model.FormulaAndFunctionResponseFormat;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinition;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinitionType;
+import com.datadog.api.client.v1.model.TimeseriesWidgetRequest;
+import com.datadog.api.client.v1.model.Widget;
+import com.datadog.api.client.v1.model.WidgetDefinition;
+import com.datadog.api.client.v1.model.WidgetDisplayType;
+import com.datadog.api.client.v1.model.WidgetLineType;
+import com.datadog.api.client.v1.model.WidgetLineWidth;
+import com.datadog.api.client.v1.model.WidgetRequestStyle;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    Dashboard body =
+        new Dashboard()
+            .layoutType(DashboardLayoutType.ORDERED)
+            .title("Example-Dashboard")
+            .widgets(
+                Collections.singletonList(
+                    new Widget()
+                        .definition(
+                            new WidgetDefinition(
+                                new TimeseriesWidgetDefinition()
+                                    .type(TimeseriesWidgetDefinitionType.TIMESERIES)
+                                    .requests(
+                                        Arrays.asList(
+                                            new TimeseriesWidgetRequest()
+                                                .onRightYaxis(false)
+                                                .queries(
+                                                    Collections.singletonList(
+                                                        new FormulaAndFunctionQueryDefinition(
+                                                            new FormulaAndFunctionMetricQueryDefinition()
+                                                                .dataSource(
+                                                                    FormulaAndFunctionMetricDataSource
+                                                                        .METRICS)
+                                                                .name("mymetric")
+                                                                .query("avg:system.cpu.user{*}"))))
+                                                .responseFormat(
+                                                    FormulaAndFunctionResponseFormat.TIMESERIES)
+                                                .displayType(WidgetDisplayType.LINE),
+                                            new TimeseriesWidgetRequest()
+                                                .responseFormat(
+                                                    FormulaAndFunctionResponseFormat.TIMESERIES)
+                                                .queries(
+                                                    Collections.singletonList(
+                                                        new FormulaAndFunctionQueryDefinition(
+                                                            new FormulaAndFunctionMetricQueryDefinition()
+                                                                .dataSource(
+                                                                    FormulaAndFunctionMetricDataSource
+                                                                        .METRICS)
+                                                                .name("mymetricoverlay")
+                                                                .query("avg:system.cpu.user{*}"))))
+                                                .style(
+                                                    new WidgetRequestStyle()
+                                                        .palette("purple")
+                                                        .lineType(WidgetLineType.SOLID)
+                                                        .lineWidth(WidgetLineWidth.NORMAL))
+                                                .displayType(WidgetDisplayType.OVERLAY)))))));
+
+    try {
+      Dashboard result = apiInstance.createDashboard(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#createDashboard");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
