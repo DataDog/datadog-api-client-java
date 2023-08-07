ef4a9b51b706b0344a097db0d038ef3359b1b2d9
ci.datadog-api-spec <packages@datadoghq.com>
2023-08-07 15:34:44 +0000

    pre-commit fixes

--- src/test/java/com/datadog/api/client/v2/api/RetryTest.java
+++ src/test/java/com/datadog/api/client/v2/api/RetryTest.java
@@ -13,33 +13,34 @@ import org.junit.Test;
 
 public class RetryTest extends V2APITest {
 
-    private static long dashboardListID;
-    private static com.datadog.api.client.v1.api.DashboardListsApi dashboardListsApiV1;
-    private final DashboardListsApi api = new DashboardListsApi(generalApiClientWithRetry);
+  private static long dashboardListID;
+  private static com.datadog.api.client.v1.api.DashboardListsApi dashboardListsApiV1;
+  private final DashboardListsApi api = new DashboardListsApi(generalApiClientWithRetry);
 
-    @Override
-    public String getTracingEndpoint() {
+  @Override
+  public String getTracingEndpoint() {
     return "dashboard-lists";
-    }
+  }
 
-    @After
-    public void deleteDashboardList() throws ApiException {
-        dashboardListsApiV1.deleteDashboardList(dashboardListID);
-    }
+  @After
+  public void deleteDashboardList() throws ApiException {
+    dashboardListsApiV1.deleteDashboardList(dashboardListID);
+  }
 
-    @Before
-    public void createDashboardList() throws ApiException, NoSuchAlgorithmException {
-        dashboardListsApiV1 = new com.datadog.api.client.v1.api.DashboardListsApi(generalApiClientWithRetry);
-        DashboardList res =
-            dashboardListsApiV1.createDashboardList(new DashboardList().name(getUniqueEntityName()));
-        dashboardListID = res.getId();
-    }
+  @Before
+  public void createDashboardList() throws ApiException, NoSuchAlgorithmException {
+    dashboardListsApiV1 =
+        new com.datadog.api.client.v1.api.DashboardListsApi(generalApiClientWithRetry);
+    DashboardList res =
+        dashboardListsApiV1.createDashboardList(new DashboardList().name(getUniqueEntityName()));
+    dashboardListID = res.getId();
+  }
 
-    @Test
-    public void retryWithDashboardListItemGetTest() throws ApiException {
+  @Test
+  public void retryWithDashboardListItemGetTest() throws ApiException {
     DashboardListItems getResponse = api.getDashboardListItems(dashboardListID);
     assertNotNull(getResponse.getTotal());
     assertEquals(0, (long) getResponse.getTotal());
     assertEquals(0, getResponse.getDashboards().size());
-    }
+  }
 }
