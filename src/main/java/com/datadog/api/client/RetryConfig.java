e35b2755a8b7f3ce200394c6ef6a7b4af1def8cc
ci.datadog-api-spec <packages@datadoghq.com>
2023-08-08 17:36:17 +0000

    pre-commit fixes

--- src/main/java/com/datadog/api/client/RetryConfig.java
+++ src/main/java/com/datadog/api/client/RetryConfig.java
@@ -1,55 +1,59 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
 package com.datadog.api.client;
 
 public class RetryConfig {
-    public boolean enableRetry;
-    public int backOffMultiplier;
-    public int backOffBase;
-    public int maxRetries;
+  public boolean enableRetry;
+  public int backOffMultiplier;
+  public int backOffBase;
+  public int maxRetries;
 
-
-     /**
+  /**
    * @param enableRetry Enable retry when rate limited
    * @param backOffMultiplier Multiplier for retry backoff
    * @param backOffBase Base for retry backoff
-   * @param maxRetries Maximum number of retries 
+   * @param maxRetries Maximum number of retries
    */
-
-    public RetryConfig(boolean enableRetry, int backOffMultiplier, int backOffBase, int maxRetries) {
-        this.enableRetry = enableRetry;
-        this.backOffMultiplier = backOffMultiplier;
-        this.backOffBase = backOffBase;
-        this.maxRetries = maxRetries;
-    }
+  public RetryConfig(boolean enableRetry, int backOffMultiplier, int backOffBase, int maxRetries) {
+    this.enableRetry = enableRetry;
+    this.backOffMultiplier = backOffMultiplier;
+    this.backOffBase = backOffBase;
+    this.maxRetries = maxRetries;
+  }
 
   public boolean isEnableRetry() {
-        return enableRetry;
-    }
+    return enableRetry;
+  }
 
-    public int getBackOffMultiplier() {
-        return backOffMultiplier;
-    }
+  public int getBackOffMultiplier() {
+    return backOffMultiplier;
+  }
 
-    public int getBackOffBase() {
-        return backOffBase;
-    }
+  public int getBackOffBase() {
+    return backOffBase;
+  }
 
-    public int getMaxRetries() {
-        return maxRetries;
-    }
+  public int getMaxRetries() {
+    return maxRetries;
+  }
 
-    public void setEnableRetry(boolean enableRetry) {
-        this.enableRetry = enableRetry;
-    }
+  public void setEnableRetry(boolean enableRetry) {
+    this.enableRetry = enableRetry;
+  }
 
-    public void setBackOffMultiplier(int backOffMultiplier) {
-        this.backOffMultiplier = backOffMultiplier;
-    }
+  public void setBackOffMultiplier(int backOffMultiplier) {
+    this.backOffMultiplier = backOffMultiplier;
+  }
 
-    public void setBackOffBase(int backOffBase) {
-        this.backOffBase = backOffBase;
-    }
-    public void setMaxRetries(int maxRetries) {
-        this.maxRetries = maxRetries;
-    }
+  public void setBackOffBase(int backOffBase) {
+    this.backOffBase = backOffBase;
+  }
 
+  public void setMaxRetries(int maxRetries) {
+    this.maxRetries = maxRetries;
+  }
 }
