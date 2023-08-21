c4bda5e437a9cb84af4afcdcf4851e48be65dfb2
ci.datadog-api-spec <packages@datadoghq.com>
2023-08-21 14:23:42 +0000

    Regenerate client from commit 77f8c067 of spec repo

--- src/main/java/com/datadog/api/client/v2/model/TeamAttributes.java
+++ src/main/java/com/datadog/api/client/v2/model/TeamAttributes.java
@@ -14,26 +14,38 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import org.openapitools.jackson.nullable.JsonNullable;
 
 /** Team attributes */
 @JsonPropertyOrder({
+  TeamAttributes.JSON_PROPERTY_AVATAR,
+  TeamAttributes.JSON_PROPERTY_BANNER,
   TeamAttributes.JSON_PROPERTY_CREATED_AT,
   TeamAttributes.JSON_PROPERTY_DESCRIPTION,
   TeamAttributes.JSON_PROPERTY_HANDLE,
+  TeamAttributes.JSON_PROPERTY_HIDDEN_MODULES,
   TeamAttributes.JSON_PROPERTY_LINK_COUNT,
   TeamAttributes.JSON_PROPERTY_MODIFIED_AT,
   TeamAttributes.JSON_PROPERTY_NAME,
   TeamAttributes.JSON_PROPERTY_SUMMARY,
-  TeamAttributes.JSON_PROPERTY_USER_COUNT
+  TeamAttributes.JSON_PROPERTY_USER_COUNT,
+  TeamAttributes.JSON_PROPERTY_VISIBLE_MODULES
 })
 @jakarta.annotation.Generated(
     value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
 public class TeamAttributes {
   @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_AVATAR = "avatar";
+  private JsonNullable<String> avatar = JsonNullable.<String>undefined();
+
+  public static final String JSON_PROPERTY_BANNER = "banner";
+  private JsonNullable<Long> banner = JsonNullable.<Long>undefined();
+
   public static final String JSON_PROPERTY_CREATED_AT = "created_at";
   private OffsetDateTime createdAt;
 
@@ -42,6 +54,9 @@ public class TeamAttributes {
 
   public static final String JSON_PROPERTY_HANDLE = "handle";
   private String handle;
+
+  public static final String JSON_PROPERTY_HIDDEN_MODULES = "hidden_modules";
+  private List<String> hiddenModules = null;
 
   public static final String JSON_PROPERTY_LINK_COUNT = "link_count";
   private Integer linkCount;
@@ -58,6 +73,9 @@ public class TeamAttributes {
   public static final String JSON_PROPERTY_USER_COUNT = "user_count";
   private Integer userCount;
 
+  public static final String JSON_PROPERTY_VISIBLE_MODULES = "visible_modules";
+  private List<String> visibleModules = null;
+
   public TeamAttributes() {}
 
   @JsonCreator
@@ -66,6 +84,68 @@ public class TeamAttributes {
       @JsonProperty(required = true, value = JSON_PROPERTY_NAME) String name) {
     this.handle = handle;
     this.name = name;
+  }
+
+  public TeamAttributes avatar(String avatar) {
+    this.avatar = JsonNullable.<String>of(avatar);
+    return this;
+  }
+
+  /**
+   * Unicode representation of the avatar for the team, limited to a single grapheme
+   *
+   * @return avatar
+   */
+  @jakarta.annotation.Nullable
+  @JsonIgnore
+  public String getAvatar() {
+    return avatar.orElse(null);
+  }
+
+  @JsonProperty(JSON_PROPERTY_AVATAR)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public JsonNullable<String> getAvatar_JsonNullable() {
+    return avatar;
+  }
+
+  @JsonProperty(JSON_PROPERTY_AVATAR)
+  public void setAvatar_JsonNullable(JsonNullable<String> avatar) {
+    this.avatar = avatar;
+  }
+
+  public void setAvatar(String avatar) {
+    this.avatar = JsonNullable.<String>of(avatar);
+  }
+
+  public TeamAttributes banner(Long banner) {
+    this.banner = JsonNullable.<Long>of(banner);
+    return this;
+  }
+
+  /**
+   * Banner selection for the team
+   *
+   * @return banner
+   */
+  @jakarta.annotation.Nullable
+  @JsonIgnore
+  public Long getBanner() {
+    return banner.orElse(null);
+  }
+
+  @JsonProperty(JSON_PROPERTY_BANNER)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public JsonNullable<Long> getBanner_JsonNullable() {
+    return banner;
+  }
+
+  @JsonProperty(JSON_PROPERTY_BANNER)
+  public void setBanner_JsonNullable(JsonNullable<Long> banner) {
+    this.banner = banner;
+  }
+
+  public void setBanner(Long banner) {
+    this.banner = JsonNullable.<Long>of(banner);
   }
 
   public TeamAttributes createdAt(OffsetDateTime createdAt) {
@@ -138,6 +218,35 @@ public class TeamAttributes {
 
   public void setHandle(String handle) {
     this.handle = handle;
+  }
+
+  public TeamAttributes hiddenModules(List<String> hiddenModules) {
+    this.hiddenModules = hiddenModules;
+    return this;
+  }
+
+  public TeamAttributes addHiddenModulesItem(String hiddenModulesItem) {
+    if (this.hiddenModules == null) {
+      this.hiddenModules = new ArrayList<>();
+    }
+    this.hiddenModules.add(hiddenModulesItem);
+    return this;
+  }
+
+  /**
+   * Collection of hidden modules for the team
+   *
+   * @return hiddenModules
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_HIDDEN_MODULES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public List<String> getHiddenModules() {
+    return hiddenModules;
+  }
+
+  public void setHiddenModules(List<String> hiddenModules) {
+    this.hiddenModules = hiddenModules;
   }
 
   /**
@@ -236,6 +345,35 @@ public class TeamAttributes {
     return userCount;
   }
 
+  public TeamAttributes visibleModules(List<String> visibleModules) {
+    this.visibleModules = visibleModules;
+    return this;
+  }
+
+  public TeamAttributes addVisibleModulesItem(String visibleModulesItem) {
+    if (this.visibleModules == null) {
+      this.visibleModules = new ArrayList<>();
+    }
+    this.visibleModules.add(visibleModulesItem);
+    return this;
+  }
+
+  /**
+   * Collection of visible modules for the team
+   *
+   * @return visibleModules
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_VISIBLE_MODULES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public List<String> getVisibleModules() {
+    return visibleModules;
+  }
+
+  public void setVisibleModules(List<String> visibleModules) {
+    this.visibleModules = visibleModules;
+  }
+
   /**
    * A container for additional, undeclared properties. This is a holder for any undeclared
    * properties as specified with the 'additionalProperties' keyword in the OAS document.
@@ -292,28 +430,36 @@ public class TeamAttributes {
       return false;
     }
     TeamAttributes teamAttributes = (TeamAttributes) o;
-    return Objects.equals(this.createdAt, teamAttributes.createdAt)
+    return Objects.equals(this.avatar, teamAttributes.avatar)
+        && Objects.equals(this.banner, teamAttributes.banner)
+        && Objects.equals(this.createdAt, teamAttributes.createdAt)
         && Objects.equals(this.description, teamAttributes.description)
         && Objects.equals(this.handle, teamAttributes.handle)
+        && Objects.equals(this.hiddenModules, teamAttributes.hiddenModules)
         && Objects.equals(this.linkCount, teamAttributes.linkCount)
         && Objects.equals(this.modifiedAt, teamAttributes.modifiedAt)
         && Objects.equals(this.name, teamAttributes.name)
         && Objects.equals(this.summary, teamAttributes.summary)
         && Objects.equals(this.userCount, teamAttributes.userCount)
+        && Objects.equals(this.visibleModules, teamAttributes.visibleModules)
         && Objects.equals(this.additionalProperties, teamAttributes.additionalProperties);
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(
+        avatar,
+        banner,
         createdAt,
         description,
         handle,
+        hiddenModules,
         linkCount,
         modifiedAt,
         name,
         summary,
         userCount,
+        visibleModules,
         additionalProperties);
   }
 
@@ -321,14 +467,18 @@ public class TeamAttributes {
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class TeamAttributes {\n");
+    sb.append("    avatar: ").append(toIndentedString(avatar)).append("\n");
+    sb.append("    banner: ").append(toIndentedString(banner)).append("\n");
     sb.append("    createdAt: ").append(toIndentedString(createdAt)).append("\n");
     sb.append("    description: ").append(toIndentedString(description)).append("\n");
     sb.append("    handle: ").append(toIndentedString(handle)).append("\n");
+    sb.append("    hiddenModules: ").append(toIndentedString(hiddenModules)).append("\n");
     sb.append("    linkCount: ").append(toIndentedString(linkCount)).append("\n");
     sb.append("    modifiedAt: ").append(toIndentedString(modifiedAt)).append("\n");
     sb.append("    name: ").append(toIndentedString(name)).append("\n");
     sb.append("    summary: ").append(toIndentedString(summary)).append("\n");
     sb.append("    userCount: ").append(toIndentedString(userCount)).append("\n");
+    sb.append("    visibleModules: ").append(toIndentedString(visibleModules)).append("\n");
     sb.append("    additionalProperties: ")
         .append(toIndentedString(additionalProperties))
         .append("\n");
