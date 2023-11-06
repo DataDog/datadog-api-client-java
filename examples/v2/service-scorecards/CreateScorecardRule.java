3aaa4996d086210340308b11caff55b5b35c7fd4
ci.datadog-api-spec <packages@datadoghq.com>
2023-11-06 14:33:47 +0000

    pre-commit fixes

--- examples/v2/service-scorecards/CreateScorecardRule.java
+++ examples/v2/service-scorecards/CreateScorecardRule.java
@@ -12,8 +12,8 @@ import com.datadog.api.client.v2.model.RuleType;
 public class Example {
   public static void main(String[] args) {
     ApiClient defaultClient = ApiClient.getDefaultApiClient();
-    defaultClient.setUnstableOperationEnabled("v2.createScorecardRule", true);
     defaultClient.setUnstableOperationEnabled("v2.createScorecardOutcomesBatch", true);
+    defaultClient.setUnstableOperationEnabled("v2.createScorecardRule", true);
     ServiceScorecardsApi apiInstance = new ServiceScorecardsApi(defaultClient);
 
     CreateRuleRequest body =
