d1e165c36707e3b99067ec9f3f2788c17665528b
ci.datadog-api-spec <packages@datadoghq.com>
2023-11-20 19:13:47 +0000

    pre-commit fixes

--- src/main/java/com/datadog/api/client/ApiClient.java
+++ src/main/java/com/datadog/api/client/ApiClient.java
@@ -1560,7 +1560,6 @@ public class ApiClient {
     try {
       int currentRetry = 0;
       while (true) {
-
         response = sendRequest(method, invocationBuilder, entity);
         int statusCode = response.getStatusInfo().getStatusCode();
         Map<String, List<String>> responseHeaders = buildResponseHeaders(response);
