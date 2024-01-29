2208fb8e07e0a18db9c6a96b38c5893517208902
ci.datadog-api-spec <packages@datadoghq.com>
2024-01-29 18:57:35 +0000

    Regenerate client from commit 5ded48a3 of spec repo

--- examples/v2/okta-integration/CreateOktaAccount.java
+++ examples/v2/okta-integration/CreateOktaAccount.java
@@ -22,7 +22,7 @@ public class Example {
                         new OktaAccountAttributes()
                             .authMethod("oauth")
                             .domain("https://example.okta.com/")
-                            .name("Okta_Prod")
+                            .name("exampleoktaintegration")
                             .clientId("client_id")
                             .clientSecret("client_secret"))
                     .id("f749daaf-682e-4208-a38d-c9b43162c609")
