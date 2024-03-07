229185c9dbb05af9af1cb97aa09a58b1f8e318cd
ci.datadog-api-spec <packages@datadoghq.com>
2024-03-07 14:50:23 +0000

    Regenerate client from commit dbf17f07 of spec repo

--- src/main/java/com/datadog/api/client/v2/model/DowntimeScheduleRecurrencesUpdateRequest.java
+++ src/main/java/com/datadog/api/client/v2/model/DowntimeScheduleRecurrencesUpdateRequest.java
@@ -6,16 +6,12 @@
 
 package com.datadog.api.client.v2.model;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 /** A recurring downtime schedule definition. */
@@ -89,52 +85,6 @@ public class DowntimeScheduleRecurrencesUpdateRequest {
     this.timezone = timezone;
   }
 
-  /**
-   * A container for additional, undeclared properties. This is a holder for any undeclared
-   * properties as specified with the 'additionalProperties' keyword in the OAS document.
-   */
-  private Map<String, Object> additionalProperties;
-
-  /**
-   * Set the additional (undeclared) property with the specified name and value. If the property
-   * does not already exist, create it otherwise replace it.
-   *
-   * @param key The arbitrary key to set
-   * @param value The associated value
-   * @return DowntimeScheduleRecurrencesUpdateRequest
-   */
-  @JsonAnySetter
-  public DowntimeScheduleRecurrencesUpdateRequest putAdditionalProperty(String key, Object value) {
-    if (this.additionalProperties == null) {
-      this.additionalProperties = new HashMap<String, Object>();
-    }
-    this.additionalProperties.put(key, value);
-    return this;
-  }
-
-  /**
-   * Return the additional (undeclared) property.
-   *
-   * @return The additional properties
-   */
-  @JsonAnyGetter
-  public Map<String, Object> getAdditionalProperties() {
-    return additionalProperties;
-  }
-
-  /**
-   * Return the additional (undeclared) property with the specified name.
-   *
-   * @param key The arbitrary key to get
-   * @return The specific additional property for the given key
-   */
-  public Object getAdditionalProperty(String key) {
-    if (this.additionalProperties == null) {
-      return null;
-    }
-    return this.additionalProperties.get(key);
-  }
-
   /** Return true if this DowntimeScheduleRecurrencesUpdateRequest object is equal to o. */
   @Override
   public boolean equals(Object o) {
@@ -147,15 +97,12 @@ public class DowntimeScheduleRecurrencesUpdateRequest {
     DowntimeScheduleRecurrencesUpdateRequest downtimeScheduleRecurrencesUpdateRequest =
         (DowntimeScheduleRecurrencesUpdateRequest) o;
     return Objects.equals(this.recurrences, downtimeScheduleRecurrencesUpdateRequest.recurrences)
-        && Objects.equals(this.timezone, downtimeScheduleRecurrencesUpdateRequest.timezone)
-        && Objects.equals(
-            this.additionalProperties,
-            downtimeScheduleRecurrencesUpdateRequest.additionalProperties);
+        && Objects.equals(this.timezone, downtimeScheduleRecurrencesUpdateRequest.timezone);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(recurrences, timezone, additionalProperties);
+    return Objects.hash(recurrences, timezone);
   }
 
   @Override
@@ -164,9 +111,6 @@ public class DowntimeScheduleRecurrencesUpdateRequest {
     sb.append("class DowntimeScheduleRecurrencesUpdateRequest {\n");
     sb.append("    recurrences: ").append(toIndentedString(recurrences)).append("\n");
     sb.append("    timezone: ").append(toIndentedString(timezone)).append("\n");
-    sb.append("    additionalProperties: ")
-        .append(toIndentedString(additionalProperties))
-        .append("\n");
     sb.append('}');
     return sb.toString();
   }
