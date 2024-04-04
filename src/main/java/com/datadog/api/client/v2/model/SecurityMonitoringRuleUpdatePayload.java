eb8bdf69966eaa133c34df9b11a7df1fd8b52a6a
ci.datadog-api-spec <packages@datadoghq.com>
2024-04-04 17:58:03 +0000

    Regenerate client from commit bf950df2 of spec repo

--- src/main/java/com/datadog/api/client/v2/model/SecurityMonitoringRuleUpdatePayload.java
+++ src/main/java/com/datadog/api/client/v2/model/SecurityMonitoringRuleUpdatePayload.java
@@ -148,7 +148,8 @@ public class SecurityMonitoringRuleUpdatePayload {
   }
 
   /**
-   * Additional queries to filter matched events before they are processed.
+   * Additional queries to filter matched events before they are processed. This field is deprecated
+   * for log detection, signal correlation, and workload security rules.
    *
    * @return filters
    */
--- src/main/java/com/datadog/api/client/v2/model/SecurityMonitoringSignalRuleCreatePayload.java
+++ src/main/java/com/datadog/api/client/v2/model/SecurityMonitoringSignalRuleCreatePayload.java
@@ -138,7 +138,8 @@ public class SecurityMonitoringSignalRuleCreatePayload {
   }
 
   /**
-   * Additional queries to filter matched events before they are processed.
+   * Additional queries to filter matched events before they are processed. This field is deprecated
+   * for log detection, signal correlation, and workload security rules.
    *
    * @return filters
    */
