b77fe5899ea95d32659031116d7b2376d6ec680f
Jack Edmonds <jack.edmonds@datadoghq.com>
2024-06-12 10:53:12 -0400

    Add comments.

--- shade-test.sh
+++ shade-test.sh
@@ -1,4 +1,8 @@
+# Create the package. Skip tests since this is just for testing the contents of the jar. We've already run the tests in another step.
 mvn package -DskipTests
+# List everything in the shaded jar. Anything in com/datadog is good since that's either shaded or our own code.
+# We intentionally avoid shading anything in META-INF in the pom.xml.
+# And there are some native libraries in other directories that we don't shade so we only want to check for .class files.
 if unzip -Z1 target/datadog-api-client-*-shaded-jar.jar | grep -v "^com/datadog" | grep -v "^META-INF" | grep "\.class$"; then
   exit 1
 else
