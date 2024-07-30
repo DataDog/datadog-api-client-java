0f479dae85cdca08b982f7e3264d4be5efd9d7ac
ci.datadog-api-spec <packages@datadoghq.com>
2024-07-30 19:26:36 +0000

    Regenerate client from commit edbd396a of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/WorkflowInstanceCreateMeta.java
@@ -0,0 +1,144 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** Additional information for creating a workflow instance. */
+@JsonPropertyOrder({WorkflowInstanceCreateMeta.JSON_PROPERTY_PAYLOAD})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class WorkflowInstanceCreateMeta {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_PAYLOAD = "payload";
+  private Map<String, Object> payload = null;
+
+  public WorkflowInstanceCreateMeta payload(Map<String, Object> payload) {
+    this.payload = payload;
+    return this;
+  }
+
+  public WorkflowInstanceCreateMeta putPayloadItem(String key, Object payloadItem) {
+    if (this.payload == null) {
+      this.payload = new HashMap<>();
+    }
+    this.payload.put(key, payloadItem);
+    return this;
+  }
+
+  /**
+   * The input parameters to the workflow.
+   *
+   * @return payload
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_PAYLOAD)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public Map<String, Object> getPayload() {
+    return payload;
+  }
+
+  public void setPayload(Map<String, Object> payload) {
+    this.payload = payload;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return WorkflowInstanceCreateMeta
+   */
+  @JsonAnySetter
+  public WorkflowInstanceCreateMeta putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this WorkflowInstanceCreateMeta object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    WorkflowInstanceCreateMeta workflowInstanceCreateMeta = (WorkflowInstanceCreateMeta) o;
+    return Objects.equals(this.payload, workflowInstanceCreateMeta.payload)
+        && Objects.equals(
+            this.additionalProperties, workflowInstanceCreateMeta.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(payload, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class WorkflowInstanceCreateMeta {\n");
+    sb.append("    payload: ").append(toIndentedString(payload)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
