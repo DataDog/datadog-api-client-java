3973cfd25c995b7dac31b11d36ae258673970075
ci.datadog-api-spec <packages@datadoghq.com>
2024-08-01 17:07:32 +0000

    Regenerate client from commit 98b3cbb2 of spec repo

--- examples/v1/synthetics/CreateGlobalVariable_3298562511.java
+++ examples/v1/synthetics/CreateGlobalVariable_3298562511.java
@@ -1,33 +1,21 @@
-// Create a global variable from test returns "OK" response
+// Create a FIDO global variable returns "OK" response
 
 import com.datadog.api.client.ApiClient;
 import com.datadog.api.client.ApiException;
 import com.datadog.api.client.v1.api.SyntheticsApi;
 import com.datadog.api.client.v1.model.SyntheticsGlobalVariable;
-import com.datadog.api.client.v1.model.SyntheticsGlobalVariableParseTestOptions;
-import com.datadog.api.client.v1.model.SyntheticsGlobalVariableParseTestOptionsType;
 import com.datadog.api.client.v1.model.SyntheticsGlobalVariableRequest;
-import com.datadog.api.client.v1.model.SyntheticsGlobalVariableValue;
 
 public class Example {
   public static void main(String[] args) {
     ApiClient defaultClient = ApiClient.getDefaultApiClient();
     SyntheticsApi apiInstance = new SyntheticsApi(defaultClient);
 
-    // there is a valid "synthetics_api_test_multi_step" in the system
-    String SYNTHETICS_API_TEST_MULTI_STEP_PUBLIC_ID =
-        System.getenv("SYNTHETICS_API_TEST_MULTI_STEP_PUBLIC_ID");
-
     SyntheticsGlobalVariableRequest body =
         new SyntheticsGlobalVariableRequest()
             .description("")
-            .name("GLOBAL_VARIABLE_FROM_TEST_PAYLOAD_EXAMPLESYNTHETIC")
-            .value(new SyntheticsGlobalVariableValue().secure(false).value(""))
-            .parseTestPublicId(SYNTHETICS_API_TEST_MULTI_STEP_PUBLIC_ID)
-            .parseTestOptions(
-                new SyntheticsGlobalVariableParseTestOptions()
-                    .type(SyntheticsGlobalVariableParseTestOptionsType.LOCAL_VARIABLE)
-                    .localVariableName("EXTRACTED_VALUE"));
+            .isFido(true)
+            .name("GLOBAL_VARIABLE_FIDO_PAYLOAD_EXAMPLESYNTHETIC");
 
     try {
       SyntheticsGlobalVariable result = apiInstance.createGlobalVariable(body);
