d949c9e314da0d2b9836eb6cfdcdd776fb45f6f8
ci.datadog-api-spec <packages@datadoghq.com>
2024-08-27 08:17:02 +0000

    Regenerate client from commit f621bf7a of spec repo

--- src/main/java/com/datadog/api/client/v2/api/RestrictionPoliciesApi.java
+++ src/main/java/com/datadog/api/client/v2/api/RestrictionPoliciesApi.java
@@ -51,7 +51,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @throws ApiException if fails to make API call
    */
   public void deleteRestrictionPolicy(String resourceId) throws ApiException {
@@ -65,7 +65,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return CompletableFuture
    */
   public CompletableFuture<Void> deleteRestrictionPolicyAsync(String resourceId) {
@@ -81,7 +81,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return ApiResponse&lt;Void&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -138,7 +138,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return CompletableFuture&lt;ApiResponse&lt;Void&gt;&gt;
    */
   public CompletableFuture<ApiResponse<Void>> deleteRestrictionPolicyWithHttpInfoAsync(
@@ -196,7 +196,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return RestrictionPolicyResponse
    * @throws ApiException if fails to make API call
    */
@@ -211,7 +211,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
    */
   public CompletableFuture<RestrictionPolicyResponse> getRestrictionPolicyAsync(String resourceId) {
@@ -227,7 +227,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return ApiResponse&lt;RestrictionPolicyResponse&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -284,7 +284,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @return CompletableFuture&lt;ApiResponse&lt;RestrictionPolicyResponse&gt;&gt;
    */
   public CompletableFuture<ApiResponse<RestrictionPolicyResponse>>
@@ -342,7 +342,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @param body Restriction policy payload (required)
    * @return RestrictionPolicyResponse
    * @throws ApiException if fails to make API call
@@ -359,7 +359,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @param body Restriction policy payload (required)
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
    */
@@ -382,7 +382,7 @@ public class RestrictionPoliciesApi {
    * Security Rules: <code>security-rule</code> - Service Level Objectives: <code>slo</code> -
    * Synthetic Global Variables: <code>synthetics-global-variable</code> - Synthetic Tests: <code>
    * synthetics-test</code> - Synthetic Private Locations: <code>synthetics-private-location</code>
-   * - Monitors: <code>monitor</code>
+   * - Monitors: <code>monitor</code> - Suppression Rules: <code>suppression-rule</code>
    *
    * <h3>Supported relations for resources</h3>
    *
@@ -392,11 +392,12 @@ public class RestrictionPoliciesApi {
    * viewer</code>, <code>editor</code> Service Level Objectives | <code>viewer</code>, <code>editor
    * </code> Synthetic Global Variables | <code>viewer</code>, <code>editor</code> Synthetic Tests |
    * <code>viewer</code>, <code>editor</code> Synthetic Private Locations | <code>viewer</code>,
-   * <code>editor</code> Monitors | <code>viewer</code>, <code>editor</code>
+   * <code>editor</code> Monitors | <code>viewer</code>, <code>editor</code> Suppression Rules |
+   * <code>viewer</code>, <code>editor</code>
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @param body Restriction policy payload (required)
    * @return ApiResponse&lt;RestrictionPolicyResponse&gt;
    * @throws ApiException if fails to make API call
@@ -460,7 +461,7 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>notebook</code>, <code>security-rule
-   *     </code>, <code>slo</code>. (required)
+   *     </code>, <code>slo</code>, <code>suppression-rule</code>. (required)
    * @param body Restriction policy payload (required)
    * @return CompletableFuture&lt;ApiResponse&lt;RestrictionPolicyResponse&gt;&gt;
    */
