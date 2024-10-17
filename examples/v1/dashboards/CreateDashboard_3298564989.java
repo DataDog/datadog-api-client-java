7172c01f13fe52709e6e44476419cc8a8d3b7f58
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-17 14:16:20 +0000

    Regenerate client from commit fb024a45 of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_3298564989.java
@@ -0,0 +1,91 @@
+// Create a new dashboard with llm_observability_stream list_stream widget
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.DashboardsApi;
+import com.datadog.api.client.v1.model.Dashboard;
+import com.datadog.api.client.v1.model.DashboardLayoutType;
+import com.datadog.api.client.v1.model.ListStreamColumn;
+import com.datadog.api.client.v1.model.ListStreamColumnWidth;
+import com.datadog.api.client.v1.model.ListStreamQuery;
+import com.datadog.api.client.v1.model.ListStreamResponseFormat;
+import com.datadog.api.client.v1.model.ListStreamSource;
+import com.datadog.api.client.v1.model.ListStreamWidgetDefinition;
+import com.datadog.api.client.v1.model.ListStreamWidgetDefinitionType;
+import com.datadog.api.client.v1.model.ListStreamWidgetRequest;
+import com.datadog.api.client.v1.model.Widget;
+import com.datadog.api.client.v1.model.WidgetDefinition;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    Dashboard body =
+        new Dashboard()
+            .layoutType(DashboardLayoutType.ORDERED)
+            .title("Example-Dashboard with list_stream widget")
+            .widgets(
+                Collections.singletonList(
+                    new Widget()
+                        .definition(
+                            new WidgetDefinition(
+                                new ListStreamWidgetDefinition()
+                                    .type(ListStreamWidgetDefinitionType.LIST_STREAM)
+                                    .requests(
+                                        Collections.singletonList(
+                                            new ListStreamWidgetRequest()
+                                                .responseFormat(ListStreamResponseFormat.EVENT_LIST)
+                                                .query(
+                                                    new ListStreamQuery()
+                                                        .dataSource(
+                                                            ListStreamSource
+                                                                .LLM_OBSERVABILITY_STREAM)
+                                                        .queryString(
+                                                            "@event_type:span"
+                                                                + " @parent_id:undefined"))
+                                                .columns(
+                                                    Arrays.asList(
+                                                        new ListStreamColumn()
+                                                            .field("@status")
+                                                            .width(ListStreamColumnWidth.COMPACT),
+                                                        new ListStreamColumn()
+                                                            .field("@content.prompt")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("@content.response.content")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("timestamp")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("@ml_app")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("service")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("@meta.evaluations.quality")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("@meta.evaluations.security")
+                                                            .width(ListStreamColumnWidth.AUTO),
+                                                        new ListStreamColumn()
+                                                            .field("@duration")
+                                                            .width(
+                                                                ListStreamColumnWidth.AUTO)))))))));
+
+    try {
+      Dashboard result = apiInstance.createDashboard(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#createDashboard");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
