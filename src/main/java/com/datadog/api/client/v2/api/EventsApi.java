187a7ffa5ecb066f917e19fde8a222b24a19ed4f
ci.datadog-api-spec <packages@datadoghq.com>
2024-11-27 13:10:57 +0000

    Regenerate client from commit fd3dd4d0 of spec repo

--- src/main/java/com/datadog/api/client/v2/api/EventsApi.java
+++ src/main/java/com/datadog/api/client/v2/api/EventsApi.java
@@ -124,7 +124,7 @@ public class EventsApi {
             localVarHeaderParams,
             new HashMap<String, String>(),
             new String[] {"application/json"},
-            new String[] {"apiKeyAuth"});
+            new String[] {"apiKeyAuth", "appKeyAuth"});
     return apiClient.invokeAPI(
         "POST",
         builder,
@@ -170,7 +170,7 @@ public class EventsApi {
               localVarHeaderParams,
               new HashMap<String, String>(),
               new String[] {"application/json"},
-              new String[] {"apiKeyAuth"});
+              new String[] {"apiKeyAuth", "appKeyAuth"});
     } catch (ApiException ex) {
       CompletableFuture<ApiResponse<EventCreateResponsePayload>> result = new CompletableFuture<>();
       result.completeExceptionally(ex);
