70090d0f64112332da2c139018ec7b75cf11d760
ci.datadog-api-spec <packages@datadoghq.com>
2024-12-03 17:58:00 +0000

    Regenerate client from commit 88f11f7c of spec repo

--- src/main/java/com/datadog/api/client/v2/api/SecurityMonitoringApi.java
+++ src/main/java/com/datadog/api/client/v2/api/SecurityMonitoringApi.java
@@ -459,7 +459,7 @@ public class SecurityMonitoringApi {
             localVarHeaderParams,
             new HashMap<String, String>(),
             new String[] {"*/*"},
-            new String[] {"AuthZ", "apiKeyAuth", "appKeyAuth"});
+            new String[] {"apiKeyAuth", "appKeyAuth"});
     return apiClient.invokeAPI(
         "POST",
         builder,
@@ -516,7 +516,7 @@ public class SecurityMonitoringApi {
               localVarHeaderParams,
               new HashMap<String, String>(),
               new String[] {"*/*"},
-              new String[] {"AuthZ", "apiKeyAuth", "appKeyAuth"});
+              new String[] {"apiKeyAuth", "appKeyAuth"});
     } catch (ApiException ex) {
       CompletableFuture<ApiResponse<Void>> result = new CompletableFuture<>();
       result.completeExceptionally(ex);
