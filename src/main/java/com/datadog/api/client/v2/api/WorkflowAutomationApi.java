a4b3a4c076bccce6f90ff25b96a841c1311962f9
ci.datadog-api-spec <packages@datadoghq.com>
2024-12-10 15:40:22 +0000

    Regenerate client from commit aee36841 of spec repo

--- src/main/java/com/datadog/api/client/v2/api/WorkflowAutomationApi.java
+++ src/main/java/com/datadog/api/client/v2/api/WorkflowAutomationApi.java
@@ -299,7 +299,7 @@ public class WorkflowAutomationApi {
             localVarHeaderParams,
             new HashMap<String, String>(),
             new String[] {"application/json"},
-            new String[] {"apiKeyAuth", "appKeyAuth"});
+            new String[] {"AuthZ", "apiKeyAuth", "appKeyAuth"});
     return apiClient.invokeAPI(
         "POST",
         builder,
@@ -363,7 +363,7 @@ public class WorkflowAutomationApi {
               localVarHeaderParams,
               new HashMap<String, String>(),
               new String[] {"application/json"},
-              new String[] {"apiKeyAuth", "appKeyAuth"});
+              new String[] {"AuthZ", "apiKeyAuth", "appKeyAuth"});
     } catch (ApiException ex) {
       CompletableFuture<ApiResponse<WorkflowInstanceCreateResponse>> result =
           new CompletableFuture<>();
