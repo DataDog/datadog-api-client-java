dac8a09c27819b61102aeb25dba53434876e4f6c
Nikita Tkachenko <121111529+nikita-tkachenko-datadog@users.noreply.github.com>
2025-01-24 18:57:57 +0100

    Use GitHub action to inject Test Optimization (#2674)
    * Bump Test Optimization version
    
    * Remove GHA run conditions for debugging purposes
    
    * Revert "Remove GHA run conditions for debugging purposes"
    
    This reverts commit fb04f7ca75424169db2e55a59188871662aff1df.
    
    ---------
    
    Co-authored-by: skarimo <40482491+skarimo@users.noreply.github.com>

--- src/test/java/com/datadog/api/ScenariosTest.java
+++ src/test/java/com/datadog/api/ScenariosTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    plugin = {"pretty", "summary", "com.datadog.api.TracePlugin", "rerun:target/rerun.txt"},
+    plugin = {"pretty", "summary", "rerun:target/rerun.txt"},
     snippets = CAMELCASE,
     tags = "not @skip and not @skip-java")
 public class ScenariosTest {}
