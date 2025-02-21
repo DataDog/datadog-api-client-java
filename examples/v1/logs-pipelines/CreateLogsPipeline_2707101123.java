3c42dc76566c04d2408dffcb5e90cc296bb578b2
ci.datadog-api-spec <packages@datadoghq.com>
2025-02-21 15:30:11 +0000

    Regenerate client from commit ee87a556 of spec repo

---
+++ examples/v1/logs-pipelines/CreateLogsPipeline_2707101123.java
@@ -0,0 +1,42 @@
+// Create a pipeline with Span Id Remapper returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.LogsPipelinesApi;
+import com.datadog.api.client.v1.model.LogsFilter;
+import com.datadog.api.client.v1.model.LogsPipeline;
+import com.datadog.api.client.v1.model.LogsProcessor;
+import com.datadog.api.client.v1.model.LogsSpanRemapper;
+import com.datadog.api.client.v1.model.LogsSpanRemapperType;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    LogsPipelinesApi apiInstance = new LogsPipelinesApi(defaultClient);
+
+    LogsPipeline body =
+        new LogsPipeline()
+            .filter(new LogsFilter().query("source:python"))
+            .name("testPipeline")
+            .processors(
+                Collections.singletonList(
+                    new LogsProcessor(
+                        new LogsSpanRemapper()
+                            .type(LogsSpanRemapperType.SPAN_ID_REMAPPER)
+                            .isEnabled(true)
+                            .name("test_filter")
+                            .sources(Collections.singletonList("dd.span_id")))));
+
+    try {
+      LogsPipeline result = apiInstance.createLogsPipeline(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling LogsPipelinesApi#createLogsPipeline");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
