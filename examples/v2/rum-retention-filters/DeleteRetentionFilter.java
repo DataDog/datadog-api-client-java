f7d60db5aa76c024b524d8a5aa06c9ea388dd16f
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-05 20:29:09 +0000

    Regenerate client from commit b51611b6 of spec repo

---
+++ examples/v2/rum-retention-filters/DeleteRetentionFilter.java
@@ -0,0 +1,23 @@
+// Delete a RUM retention filter returns "No Content" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.RumRetentionFiltersApi;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    RumRetentionFiltersApi apiInstance = new RumRetentionFiltersApi(defaultClient);
+
+    try {
+      apiInstance.deleteRetentionFilter(
+          "a33671aa-24fd-4dcd-ba4b-5bbdbafe7690", "fe34ee09-14cf-4976-9362-08044c0dea80");
+    } catch (ApiException e) {
+      System.err.println("Exception when calling RumRetentionFiltersApi#deleteRetentionFilter");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
