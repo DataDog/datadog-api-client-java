2a6fec86f96186ebfbebbcc10af1cde93a91673a
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-06 16:11:56 +0000

    Regenerate client from commit 77e81405 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/LogsArchiveStorageClassS3Type.java
@@ -0,0 +1,67 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.datadog.api.client.ModelEnum;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/** The storage class where the archive will be stored. */
+@JsonSerialize(using = LogsArchiveStorageClassS3Type.LogsArchiveStorageClassS3TypeSerializer.class)
+public class LogsArchiveStorageClassS3Type extends ModelEnum<String> {
+
+  private static final Set<String> allowedValues =
+      new HashSet<String>(
+          Arrays.asList(
+              "STANDARD", "STANDARD_IA", "ONEZONE_IA", "INTELLIGENT_TIERING", "GLACIER_IR"));
+
+  public static final LogsArchiveStorageClassS3Type STANDARD =
+      new LogsArchiveStorageClassS3Type("STANDARD");
+  public static final LogsArchiveStorageClassS3Type STANDARD_IA =
+      new LogsArchiveStorageClassS3Type("STANDARD_IA");
+  public static final LogsArchiveStorageClassS3Type ONEZONE_IA =
+      new LogsArchiveStorageClassS3Type("ONEZONE_IA");
+  public static final LogsArchiveStorageClassS3Type INTELLIGENT_TIERING =
+      new LogsArchiveStorageClassS3Type("INTELLIGENT_TIERING");
+  public static final LogsArchiveStorageClassS3Type GLACIER_IR =
+      new LogsArchiveStorageClassS3Type("GLACIER_IR");
+
+  LogsArchiveStorageClassS3Type(String value) {
+    super(value, allowedValues);
+  }
+
+  public static class LogsArchiveStorageClassS3TypeSerializer
+      extends StdSerializer<LogsArchiveStorageClassS3Type> {
+    public LogsArchiveStorageClassS3TypeSerializer(Class<LogsArchiveStorageClassS3Type> t) {
+      super(t);
+    }
+
+    public LogsArchiveStorageClassS3TypeSerializer() {
+      this(null);
+    }
+
+    @Override
+    public void serialize(
+        LogsArchiveStorageClassS3Type value, JsonGenerator jgen, SerializerProvider provider)
+        throws IOException, JsonProcessingException {
+      jgen.writeObject(value.value);
+    }
+  }
+
+  @JsonCreator
+  public static LogsArchiveStorageClassS3Type fromValue(String value) {
+    return new LogsArchiveStorageClassS3Type(value);
+  }
+}
