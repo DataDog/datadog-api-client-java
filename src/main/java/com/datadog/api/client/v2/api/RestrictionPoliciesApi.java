baf1f78a90e4f1aab4ff2b3b5ee6b63bb9391409
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-15 16:48:18 +0000

    Regenerate client from commit 2d6e55b2 of spec repo

--- src/main/java/com/datadog/api/client/v2/api/RestrictionPoliciesApi.java
+++ src/main/java/com/datadog/api/client/v2/api/RestrictionPoliciesApi.java
@@ -52,9 +52,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @throws ApiException if fails to make API call
    */
   public void deleteRestrictionPolicy(String resourceId) throws ApiException {
@@ -68,9 +69,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return CompletableFuture
    */
   public CompletableFuture<Void> deleteRestrictionPolicyAsync(String resourceId) {
@@ -86,9 +88,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return ApiResponse&lt;Void&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -145,9 +148,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return CompletableFuture&lt;ApiResponse&lt;Void&gt;&gt;
    */
   public CompletableFuture<ApiResponse<Void>> deleteRestrictionPolicyWithHttpInfoAsync(
@@ -205,9 +209,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return RestrictionPolicyResponse
    * @throws ApiException if fails to make API call
    */
@@ -222,9 +227,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
    */
   public CompletableFuture<RestrictionPolicyResponse> getRestrictionPolicyAsync(String resourceId) {
@@ -240,9 +246,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return ApiResponse&lt;RestrictionPolicyResponse&gt;
    * @throws ApiException if fails to make API call
    * @http.response.details
@@ -299,9 +306,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @return CompletableFuture&lt;ApiResponse&lt;RestrictionPolicyResponse&gt;&gt;
    */
   public CompletableFuture<ApiResponse<RestrictionPolicyResponse>>
@@ -378,9 +386,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @param body Restriction policy payload (required)
    * @return RestrictionPolicyResponse
    * @throws ApiException if fails to make API call
@@ -399,9 +408,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @param body Restriction policy payload (required)
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
    */
@@ -422,9 +432,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @param body Restriction policy payload (required)
    * @param parameters Optional parameters for the request.
    * @return RestrictionPolicyResponse
@@ -445,9 +456,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @param body Restriction policy payload (required)
    * @param parameters Optional parameters for the request.
    * @return CompletableFuture&lt;RestrictionPolicyResponse&gt;
@@ -470,35 +482,37 @@ public class RestrictionPoliciesApi {
    *
    * <p>Restriction policies can be applied to the following resources: - Dashboards: <code>
    * dashboard</code> - Integration Accounts: <code>integration-account</code> - Integration
-   * Webhooks: <code>integration-webhook</code> - Notebooks: <code>notebook</code> - Powerpacks:
-   * <code>powerpack</code> - Reference Tables: <code>reference-table</code> - Security Rules:
-   * <code>security-rule</code> - Service Level Objectives: <code>slo</code> - Synthetic Global
-   * Variables: <code>synthetics-global-variable</code> - Synthetic Tests: <code>synthetics-test
-   * </code> - Synthetic Private Locations: <code>synthetics-private-location</code> - Monitors:
-   * <code>monitor</code> - Workflows: <code>workflow</code> - App Builder Apps: <code>
-   * app-builder-app</code> - Connections: <code>connection</code> - Connection Groups: <code>
-   * connection-group</code>
+   * Services: <code>integration-service</code> - Integration Webhooks: <code>integration-webhook
+   * </code> - Notebooks: <code>notebook</code> - Powerpacks: <code>powerpack</code> - Reference
+   * Tables: <code>reference-table</code> - Security Rules: <code>security-rule</code> - Service
+   * Level Objectives: <code>slo</code> - Synthetic Global Variables: <code>
+   * synthetics-global-variable</code> - Synthetic Tests: <code>synthetics-test</code> - Synthetic
+   * Private Locations: <code>synthetics-private-location</code> - Monitors: <code>monitor</code> -
+   * Workflows: <code>workflow</code> - App Builder Apps: <code>app-builder-app</code> -
+   * Connections: <code>connection</code> - Connection Groups: <code>connection-group</code>
    *
    * <h3>Supported relations for resources</h3>
    *
    * <p>Resource Type | Supported Relations ----------------------------|--------------------------
    * Dashboards | <code>viewer</code>, <code>editor</code> Integration Accounts | <code>viewer
-   * </code>, <code>editor</code> Integration Webhooks | <code>viewer</code>, <code>editor</code>
-   * Notebooks | <code>viewer</code>, <code>editor</code> Powerpacks | <code>viewer</code>, <code>
-   * editor</code> Security Rules | <code>viewer</code>, <code>editor</code> Service Level
-   * Objectives | <code>viewer</code>, <code>editor</code> Synthetic Global Variables | <code>viewer
-   * </code>, <code>editor</code> Synthetic Tests | <code>viewer</code>, <code>editor</code>
-   * Synthetic Private Locations | <code>viewer</code>, <code>editor</code> Monitors | <code>viewer
-   * </code>, <code>editor</code> Reference Tables | <code>viewer</code>, <code>editor</code>
-   * Workflows | <code>viewer</code>, <code>runner</code>, <code>editor</code> App Builder Apps |
-   * <code>viewer</code>, <code>editor</code> Connections | <code>viewer</code>, <code>resolver
-   * </code>, <code>editor</code> Connection Groups | <code>viewer</code>, <code>editor</code>
+   * </code>, <code>editor</code> Integration Services | <code>viewer</code>, <code>editor</code>
+   * Integration Webhooks | <code>viewer</code>, <code>editor</code> Notebooks | <code>viewer</code>
+   * , <code>editor</code> Powerpacks | <code>viewer</code>, <code>editor</code> Security Rules |
+   * <code>viewer</code>, <code>editor</code> Service Level Objectives | <code>viewer</code>, <code>
+   * editor</code> Synthetic Global Variables | <code>viewer</code>, <code>editor</code> Synthetic
+   * Tests | <code>viewer</code>, <code>editor</code> Synthetic Private Locations | <code>viewer
+   * </code>, <code>editor</code> Monitors | <code>viewer</code>, <code>editor</code> Reference
+   * Tables | <code>viewer</code>, <code>editor</code> Workflows | <code>viewer</code>, <code>runner
+   * </code>, <code>editor</code> App Builder Apps | <code>viewer</code>, <code>editor</code>
+   * Connections | <code>viewer</code>, <code>resolver</code>, <code>editor</code> Connection Groups
+   * | <code>viewer</code>, <code>editor</code>
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @param body Restriction policy payload (required)
    * @param parameters Optional parameters for the request.
    * @return ApiResponse&lt;RestrictionPolicyResponse&gt;
@@ -571,9 +585,10 @@ public class RestrictionPoliciesApi {
    *
    * @param resourceId Identifier, formatted as <code>type:id</code>. Supported types: <code>
    *     connection</code>, <code>dashboard</code>, <code>integration-account</code>, <code>
-   *     integration-webhook</code>, <code>notebook</code>, <code>reference-table</code>, <code>
-   *     security-rule</code>, <code>slo</code>, <code>workflow</code>, <code>app-builder-app</code>
-   *     , <code>connection</code>, <code>connection-group</code>. (required)
+   *     integration-service</code>, <code>integration-webhook</code>, <code>notebook</code>, <code>
+   *     reference-table</code>, <code>security-rule</code>, <code>slo</code>, <code>workflow</code>
+   *     , <code>app-builder-app</code>, <code>connection</code>, <code>connection-group</code>.
+   *     (required)
    * @param body Restriction policy payload (required)
    * @param parameters Optional parameters for the request.
    * @return CompletableFuture&lt;ApiResponse&lt;RestrictionPolicyResponse&gt;&gt;
