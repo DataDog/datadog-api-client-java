8c56b91d4bef187d786b9a391e007b813c362992
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-21 13:01:29 +0000

    Regenerate client from commit 28eb0f35 of spec repo

---
+++ examples/v2/observability-pipelines/ValidatePipeline.java
@@ -0,0 +1,81 @@
+// Validate an observability pipeline returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.ObservabilityPipelinesApi;
+import com.datadog.api.client.v2.model.ObservabilityPipelineConfig;
+import com.datadog.api.client.v2.model.ObservabilityPipelineConfigDestinationItem;
+import com.datadog.api.client.v2.model.ObservabilityPipelineConfigProcessorItem;
+import com.datadog.api.client.v2.model.ObservabilityPipelineConfigSourceItem;
+import com.datadog.api.client.v2.model.ObservabilityPipelineCreateRequest;
+import com.datadog.api.client.v2.model.ObservabilityPipelineCreateRequestData;
+import com.datadog.api.client.v2.model.ObservabilityPipelineDataAttributes;
+import com.datadog.api.client.v2.model.ObservabilityPipelineDatadogAgentSource;
+import com.datadog.api.client.v2.model.ObservabilityPipelineDatadogAgentSourceType;
+import com.datadog.api.client.v2.model.ObservabilityPipelineDatadogLogsDestination;
+import com.datadog.api.client.v2.model.ObservabilityPipelineDatadogLogsDestinationType;
+import com.datadog.api.client.v2.model.ObservabilityPipelineFilterProcessor;
+import com.datadog.api.client.v2.model.ObservabilityPipelineFilterProcessorType;
+import com.datadog.api.client.v2.model.ValidationErrorResponse;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    defaultClient.setUnstableOperationEnabled("v2.validatePipeline", true);
+    ObservabilityPipelinesApi apiInstance = new ObservabilityPipelinesApi(defaultClient);
+
+    ObservabilityPipelineCreateRequest body =
+        new ObservabilityPipelineCreateRequest()
+            .data(
+                new ObservabilityPipelineCreateRequestData()
+                    .attributes(
+                        new ObservabilityPipelineDataAttributes()
+                            .config(
+                                new ObservabilityPipelineConfig()
+                                    .destinations(
+                                        Collections.singletonList(
+                                            new ObservabilityPipelineConfigDestinationItem(
+                                                new ObservabilityPipelineDatadogLogsDestination()
+                                                    .id("datadog-logs-destination")
+                                                    .inputs(
+                                                        Collections.singletonList(
+                                                            "filter-processor"))
+                                                    .type(
+                                                        ObservabilityPipelineDatadogLogsDestinationType
+                                                            .DATADOG_LOGS))))
+                                    .processors(
+                                        Collections.singletonList(
+                                            new ObservabilityPipelineConfigProcessorItem(
+                                                new ObservabilityPipelineFilterProcessor()
+                                                    .id("filter-processor")
+                                                    .include("service:my-service")
+                                                    .inputs(
+                                                        Collections.singletonList(
+                                                            "datadog-agent-source"))
+                                                    .type(
+                                                        ObservabilityPipelineFilterProcessorType
+                                                            .FILTER))))
+                                    .sources(
+                                        Collections.singletonList(
+                                            new ObservabilityPipelineConfigSourceItem(
+                                                new ObservabilityPipelineDatadogAgentSource()
+                                                    .id("datadog-agent-source")
+                                                    .type(
+                                                        ObservabilityPipelineDatadogAgentSourceType
+                                                            .DATADOG_AGENT)))))
+                            .name("Main Observability Pipeline"))
+                    .type("pipelines"));
+
+    try {
+      ValidationErrorResponse result = apiInstance.validatePipeline(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling ObservabilityPipelinesApi#validatePipeline");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/main/java/com/datadog/api/client/v2/model/ValidationError.java
@@ -0,0 +1,172 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** The definition of <code>ValidationError</code> object. */
+@JsonPropertyOrder({ValidationError.JSON_PROPERTY_META, ValidationError.JSON_PROPERTY_TITLE})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class ValidationError {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_META = "meta";
+  private ValidationErrorMeta meta;
+
+  public static final String JSON_PROPERTY_TITLE = "title";
+  private String title;
+
+  public ValidationError() {}
+
+  @JsonCreator
+  public ValidationError(
+      @JsonProperty(required = true, value = JSON_PROPERTY_META) ValidationErrorMeta meta,
+      @JsonProperty(required = true, value = JSON_PROPERTY_TITLE) String title) {
+    this.meta = meta;
+    this.unparsed |= meta.unparsed;
+    this.title = title;
+  }
+
+  public ValidationError meta(ValidationErrorMeta meta) {
+    this.meta = meta;
+    this.unparsed |= meta.unparsed;
+    return this;
+  }
+
+  /**
+   * The definition of <code>ValidationErrorMeta</code> object.
+   *
+   * @return meta
+   */
+  @JsonProperty(JSON_PROPERTY_META)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public ValidationErrorMeta getMeta() {
+    return meta;
+  }
+
+  public void setMeta(ValidationErrorMeta meta) {
+    this.meta = meta;
+  }
+
+  public ValidationError title(String title) {
+    this.title = title;
+    return this;
+  }
+
+  /**
+   * A short, human-readable summary of the error.
+   *
+   * @return title
+   */
+  @JsonProperty(JSON_PROPERTY_TITLE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return ValidationError
+   */
+  @JsonAnySetter
+  public ValidationError putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this ValidationError object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ValidationError validationError = (ValidationError) o;
+    return Objects.equals(this.meta, validationError.meta)
+        && Objects.equals(this.title, validationError.title)
+        && Objects.equals(this.additionalProperties, validationError.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(meta, title, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ValidationError {\n");
+    sb.append("    meta: ").append(toIndentedString(meta)).append("\n");
+    sb.append("    title: ").append(toIndentedString(title)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
