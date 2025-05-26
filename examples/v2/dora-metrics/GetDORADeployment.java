92bbd54018d967049adf0b519db9e31102e9bdd4
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-26 12:47:55 +0000

    Regenerate client from commit a6954c1c of spec repo

---
+++ examples/v2/dora-metrics/GetDORADeployment.java
@@ -0,0 +1,24 @@
+// Get a deployment event returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.DoraMetricsApi;
+import com.datadog.api.client.v2.model.DORAFetchResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DoraMetricsApi apiInstance = new DoraMetricsApi(defaultClient);
+
+    try {
+      DORAFetchResponse result = apiInstance.getDORADeployment("deployment_id");
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DoraMetricsApi#getDORADeployment");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
