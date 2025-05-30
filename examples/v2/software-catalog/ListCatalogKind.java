439764442612058966085a24f41d00f90815f581
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-30 12:40:46 +0000

    Regenerate client from commit e4b2672f of spec repo

---
+++ examples/v2/software-catalog/ListCatalogKind.java
@@ -0,0 +1,24 @@
+// Get a list of entity kinds returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.SoftwareCatalogApi;
+import com.datadog.api.client.v2.model.ListKindCatalogResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    SoftwareCatalogApi apiInstance = new SoftwareCatalogApi(defaultClient);
+
+    try {
+      ListKindCatalogResponse result = apiInstance.listCatalogKind();
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling SoftwareCatalogApi#listCatalogKind");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
