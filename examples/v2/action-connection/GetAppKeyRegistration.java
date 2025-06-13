e4a5f25b4b86c948c94078f54334a32c75eb8417
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-13 18:20:30 +0000

    Regenerate client from commit 49a0d05b of spec repo

---
+++ examples/v2/action-connection/GetAppKeyRegistration.java
@@ -0,0 +1,25 @@
+// Get an existing App Key Registration returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.ActionConnectionApi;
+import com.datadog.api.client.v2.model.GetAppKeyRegistrationResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    ActionConnectionApi apiInstance = new ActionConnectionApi(defaultClient);
+
+    try {
+      GetAppKeyRegistrationResponse result =
+          apiInstance.getAppKeyRegistration("b7feea52-994e-4714-a100-1bd9eff5aee1");
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling ActionConnectionApi#getAppKeyRegistration");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
