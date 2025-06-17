327d23f581bb4b887e27b0b4042d79e046d3096b
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-17 14:57:51 +0000

    Regenerate client from commit b359fdcc of spec repo

--- src/main/java/com/datadog/api/client/ApiClient.java
+++ src/main/java/com/datadog/api/client/ApiClient.java
@@ -689,6 +689,9 @@ public class ApiClient {
           put("v2.createSLOReportJob", false);
           put("v2.getSLOReport", false);
           put("v2.getSLOReportJobStatus", false);
+          put("v2.addMemberTeam", false);
+          put("v2.listMemberTeams", false);
+          put("v2.removeMemberTeam", false);
           put("v2.createIncidentTeam", false);
           put("v2.deleteIncidentTeam", false);
           put("v2.getIncidentTeam", false);
