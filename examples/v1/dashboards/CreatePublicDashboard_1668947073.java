56b13517fbef595f7bf36f5c4a5b4303121f8d90
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-27 20:54:49 +0000

    Regenerate client from commit 81bebed1 of spec repo

---
+++ examples/v1/dashboards/CreatePublicDashboard_1668947073.java
@@ -0,0 +1,49 @@
+// Create a shared dashboard with a group template variable returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.DashboardsApi;
+import com.datadog.api.client.v1.model.DashboardGlobalTime;
+import com.datadog.api.client.v1.model.DashboardGlobalTimeLiveSpan;
+import com.datadog.api.client.v1.model.DashboardShareType;
+import com.datadog.api.client.v1.model.DashboardType;
+import com.datadog.api.client.v1.model.SelectableTemplateVariableItems;
+import com.datadog.api.client.v1.model.SharedDashboard;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    // there is a valid "dashboard" in the system
+    String DASHBOARD_ID = System.getenv("DASHBOARD_ID");
+
+    SharedDashboard body =
+        new SharedDashboard()
+            .dashboardId(DASHBOARD_ID)
+            .dashboardType(DashboardType.CUSTOM_TIMEBOARD)
+            .shareType(DashboardShareType.OPEN)
+            .globalTime(
+                new DashboardGlobalTime().liveSpan(DashboardGlobalTimeLiveSpan.PAST_ONE_HOUR))
+            .selectableTemplateVars(
+                Collections.singletonList(
+                    new SelectableTemplateVariableItems()
+                        .defaultValue("*")
+                        .name("group_by_var")
+                        .type("group")
+                        .visibleTags(Arrays.asList("selectableValue1", "selectableValue2"))));
+
+    try {
+      SharedDashboard result = apiInstance.createPublicDashboard(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#createPublicDashboard");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
