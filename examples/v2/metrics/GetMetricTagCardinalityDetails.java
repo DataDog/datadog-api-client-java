dee73b33e0f1b5059560676d57181c790a443f3d
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-08 18:42:09 +0000

    Regenerate client from commit 6fc2e412 of spec repo

---
+++ examples/v2/metrics/GetMetricTagCardinalityDetails.java
@@ -0,0 +1,25 @@
+// Get tag key cardinality details returns "Success" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.MetricsApi;
+import com.datadog.api.client.v2.model.MetricTagCardinalitiesResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    MetricsApi apiInstance = new MetricsApi(defaultClient);
+
+    try {
+      MetricTagCardinalitiesResponse result =
+          apiInstance.getMetricTagCardinalityDetails("metric_name");
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling MetricsApi#getMetricTagCardinalityDetails");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
