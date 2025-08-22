dbb737f2f42a22c4a31528b4d37f012adf33eb9a
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-22 15:00:29 +0000

    Regenerate client from commit a35d4f4 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/UpdateOutcomesAsyncRequestData.java
@@ -0,0 +1,171 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** Scorecard outcomes batch request data. */
+@JsonPropertyOrder({
+  UpdateOutcomesAsyncRequestData.JSON_PROPERTY_ATTRIBUTES,
+  UpdateOutcomesAsyncRequestData.JSON_PROPERTY_TYPE
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class UpdateOutcomesAsyncRequestData {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ATTRIBUTES = "attributes";
+  private UpdateOutcomesAsyncAttributes attributes;
+
+  public static final String JSON_PROPERTY_TYPE = "type";
+  private UpdateOutcomesAsyncType type = UpdateOutcomesAsyncType.BATCHED_OUTCOME;
+
+  public UpdateOutcomesAsyncRequestData attributes(UpdateOutcomesAsyncAttributes attributes) {
+    this.attributes = attributes;
+    this.unparsed |= attributes.unparsed;
+    return this;
+  }
+
+  /**
+   * The JSON:API attributes for a batched set of scorecard outcomes.
+   *
+   * @return attributes
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ATTRIBUTES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public UpdateOutcomesAsyncAttributes getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(UpdateOutcomesAsyncAttributes attributes) {
+    this.attributes = attributes;
+  }
+
+  public UpdateOutcomesAsyncRequestData type(UpdateOutcomesAsyncType type) {
+    this.type = type;
+    this.unparsed |= !type.isValid();
+    return this;
+  }
+
+  /**
+   * The JSON:API type for scorecard outcomes.
+   *
+   * @return type
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_TYPE)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public UpdateOutcomesAsyncType getType() {
+    return type;
+  }
+
+  public void setType(UpdateOutcomesAsyncType type) {
+    if (!type.isValid()) {
+      this.unparsed = true;
+    }
+    this.type = type;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return UpdateOutcomesAsyncRequestData
+   */
+  @JsonAnySetter
+  public UpdateOutcomesAsyncRequestData putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this UpdateOutcomesAsyncRequestData object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UpdateOutcomesAsyncRequestData updateOutcomesAsyncRequestData =
+        (UpdateOutcomesAsyncRequestData) o;
+    return Objects.equals(this.attributes, updateOutcomesAsyncRequestData.attributes)
+        && Objects.equals(this.type, updateOutcomesAsyncRequestData.type)
+        && Objects.equals(
+            this.additionalProperties, updateOutcomesAsyncRequestData.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(attributes, type, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UpdateOutcomesAsyncRequestData {\n");
+    sb.append("    attributes: ").append(toIndentedString(attributes)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
