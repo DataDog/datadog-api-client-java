ba7bfee24840d592d450e0157f8f2a46106fcf5a
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-25 08:43:17 +0000

    Regenerate client from commit 205813d of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/RecommendationData.java
@@ -0,0 +1,213 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * JSON:API resource object for SPA Recommendation. Includes type, optional ID, and resource
+ * attributes with structured recommendations.
+ */
+@JsonPropertyOrder({
+  RecommendationData.JSON_PROPERTY_ATTRIBUTES,
+  RecommendationData.JSON_PROPERTY_ID,
+  RecommendationData.JSON_PROPERTY_TYPE
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class RecommendationData {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ATTRIBUTES = "attributes";
+  private RecommendationAttributes attributes;
+
+  public static final String JSON_PROPERTY_ID = "id";
+  private String id;
+
+  public static final String JSON_PROPERTY_TYPE = "type";
+  private RecommendationType type = RecommendationType.RECOMMENDATION;
+
+  public RecommendationData() {}
+
+  @JsonCreator
+  public RecommendationData(
+      @JsonProperty(required = true, value = JSON_PROPERTY_ATTRIBUTES)
+          RecommendationAttributes attributes,
+      @JsonProperty(required = true, value = JSON_PROPERTY_TYPE) RecommendationType type) {
+    this.attributes = attributes;
+    this.unparsed |= attributes.unparsed;
+    this.type = type;
+    this.unparsed |= !type.isValid();
+  }
+
+  public RecommendationData attributes(RecommendationAttributes attributes) {
+    this.attributes = attributes;
+    this.unparsed |= attributes.unparsed;
+    return this;
+  }
+
+  /**
+   * Attributes of the SPA Recommendation resource. Contains recommendations for both driver and
+   * executor components.
+   *
+   * @return attributes
+   */
+  @JsonProperty(JSON_PROPERTY_ATTRIBUTES)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public RecommendationAttributes getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(RecommendationAttributes attributes) {
+    this.attributes = attributes;
+  }
+
+  public RecommendationData id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Resource identifier for the recommendation. Optional in responses.
+   *
+   * @return id
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ID)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public RecommendationData type(RecommendationType type) {
+    this.type = type;
+    this.unparsed |= !type.isValid();
+    return this;
+  }
+
+  /**
+   * JSON:API resource type for Spark Pod Autosizing recommendations. Identifies the Recommendation
+   * resource returned by SPA.
+   *
+   * @return type
+   */
+  @JsonProperty(JSON_PROPERTY_TYPE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public RecommendationType getType() {
+    return type;
+  }
+
+  public void setType(RecommendationType type) {
+    if (!type.isValid()) {
+      this.unparsed = true;
+    }
+    this.type = type;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return RecommendationData
+   */
+  @JsonAnySetter
+  public RecommendationData putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this RecommendationData object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    RecommendationData recommendationData = (RecommendationData) o;
+    return Objects.equals(this.attributes, recommendationData.attributes)
+        && Objects.equals(this.id, recommendationData.id)
+        && Objects.equals(this.type, recommendationData.type)
+        && Objects.equals(this.additionalProperties, recommendationData.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(attributes, id, type, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class RecommendationData {\n");
+    sb.append("    attributes: ").append(toIndentedString(attributes)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
