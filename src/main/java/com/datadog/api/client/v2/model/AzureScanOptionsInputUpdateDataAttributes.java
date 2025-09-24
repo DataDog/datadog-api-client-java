8bbe656dfce44b50b362a9b0e686cf901e5b2996
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-24 17:31:18 +0000

    Regenerate client from commit 4a62c07 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/AzureScanOptionsInputUpdateDataAttributes.java
@@ -0,0 +1,168 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** The definition of <code>AzureScanOptionsInputUpdateDataAttributes</code> object. */
+@JsonPropertyOrder({
+  AzureScanOptionsInputUpdateDataAttributes.JSON_PROPERTY_VULN_CONTAINERS_OS,
+  AzureScanOptionsInputUpdateDataAttributes.JSON_PROPERTY_VULN_HOST_OS
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class AzureScanOptionsInputUpdateDataAttributes {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_VULN_CONTAINERS_OS = "vuln_containers_os";
+  private Boolean vulnContainersOs;
+
+  public static final String JSON_PROPERTY_VULN_HOST_OS = "vuln_host_os";
+  private Boolean vulnHostOs;
+
+  public AzureScanOptionsInputUpdateDataAttributes vulnContainersOs(Boolean vulnContainersOs) {
+    this.vulnContainersOs = vulnContainersOs;
+    return this;
+  }
+
+  /**
+   * The <code>attributes</code> <code>vuln_containers_os</code>.
+   *
+   * @return vulnContainersOs
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_VULN_CONTAINERS_OS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public Boolean getVulnContainersOs() {
+    return vulnContainersOs;
+  }
+
+  public void setVulnContainersOs(Boolean vulnContainersOs) {
+    this.vulnContainersOs = vulnContainersOs;
+  }
+
+  public AzureScanOptionsInputUpdateDataAttributes vulnHostOs(Boolean vulnHostOs) {
+    this.vulnHostOs = vulnHostOs;
+    return this;
+  }
+
+  /**
+   * The <code>attributes</code> <code>vuln_host_os</code>.
+   *
+   * @return vulnHostOs
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_VULN_HOST_OS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public Boolean getVulnHostOs() {
+    return vulnHostOs;
+  }
+
+  public void setVulnHostOs(Boolean vulnHostOs) {
+    this.vulnHostOs = vulnHostOs;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return AzureScanOptionsInputUpdateDataAttributes
+   */
+  @JsonAnySetter
+  public AzureScanOptionsInputUpdateDataAttributes putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this AzureScanOptionsInputUpdateDataAttributes object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AzureScanOptionsInputUpdateDataAttributes azureScanOptionsInputUpdateDataAttributes =
+        (AzureScanOptionsInputUpdateDataAttributes) o;
+    return Objects.equals(
+            this.vulnContainersOs, azureScanOptionsInputUpdateDataAttributes.vulnContainersOs)
+        && Objects.equals(this.vulnHostOs, azureScanOptionsInputUpdateDataAttributes.vulnHostOs)
+        && Objects.equals(
+            this.additionalProperties,
+            azureScanOptionsInputUpdateDataAttributes.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(vulnContainersOs, vulnHostOs, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class AzureScanOptionsInputUpdateDataAttributes {\n");
+    sb.append("    vulnContainersOs: ").append(toIndentedString(vulnContainersOs)).append("\n");
+    sb.append("    vulnHostOs: ").append(toIndentedString(vulnHostOs)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
