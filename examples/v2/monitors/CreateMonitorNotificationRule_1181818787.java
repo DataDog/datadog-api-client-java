b8f1fcf030170be3ad643d14e2c7169c2d9614b9
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-29 18:30:38 +0000

    Regenerate client from commit 2c49d12 of spec repo

---
+++ examples/v2/monitors/CreateMonitorNotificationRule_1181818787.java
@@ -0,0 +1,56 @@
+// Create a monitor notification rule with conditional recipients returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.MonitorsApi;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleAttributes;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleCondition;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleConditionalRecipients;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleCreateRequest;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleCreateRequestData;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleFilter;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleFilterTags;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleResourceType;
+import com.datadog.api.client.v2.model.MonitorNotificationRuleResponse;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    MonitorsApi apiInstance = new MonitorsApi(defaultClient);
+
+    MonitorNotificationRuleCreateRequest body =
+        new MonitorNotificationRuleCreateRequest()
+            .data(
+                new MonitorNotificationRuleCreateRequestData()
+                    .attributes(
+                        new MonitorNotificationRuleAttributes()
+                            .filter(
+                                new MonitorNotificationRuleFilter(
+                                    new MonitorNotificationRuleFilterTags()
+                                        .tags(Collections.singletonList("test:example-monitor"))))
+                            .name("test rule")
+                            .conditionalRecipients(
+                                new MonitorNotificationRuleConditionalRecipients()
+                                    .conditions(
+                                        Collections.singletonList(
+                                            new MonitorNotificationRuleCondition()
+                                                .scope("transition_type:is_alert")
+                                                .recipients(
+                                                    Arrays.asList(
+                                                        "slack-test-channel", "jira-test"))))))
+                    .type(MonitorNotificationRuleResourceType.MONITOR_NOTIFICATION_RULE));
+
+    try {
+      MonitorNotificationRuleResponse result = apiInstance.createMonitorNotificationRule(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling MonitorsApi#createMonitorNotificationRule");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
