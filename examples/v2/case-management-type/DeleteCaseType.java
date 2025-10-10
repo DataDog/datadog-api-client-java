fdea4eca728ffa795f2107249a059b65e36351ae
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-10 08:29:51 +0000

    Regenerate client from commit 49563b9 of spec repo

---
+++ examples/v2/case-management-type/DeleteCaseType.java
@@ -0,0 +1,22 @@
+// Delete a case type returns "No Content" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.CaseManagementTypeApi;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    CaseManagementTypeApi apiInstance = new CaseManagementTypeApi(defaultClient);
+
+    try {
+      apiInstance.deleteCaseType("f98a5a5b-e0ff-45d4-b2f5-afe6e74de505");
+    } catch (ApiException e) {
+      System.err.println("Exception when calling CaseManagementTypeApi#deleteCaseType");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
