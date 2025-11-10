ad02c5789a2a22ecb37239a9c496956828ad0a91
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-10 19:27:12 +0000

    Regenerate client from commit 3c17510 of spec repo

--- src/main/java/com/datadog/api/client/v2/model/TableResultV2DataAttributesFileMetadataLocalFile.java
+++ src/main/java/com/datadog/api/client/v2/model/TableResultV2DataAttributesFileMetadataLocalFile.java
@@ -6,14 +6,10 @@
 
 package com.datadog.api.client.v2.model;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -76,53 +72,6 @@ public class TableResultV2DataAttributesFileMetadataLocalFile {
     this.errorRowCount = errorRowCount;
   }
 
-  /**
-   * A container for additional, undeclared properties. This is a holder for any undeclared
-   * properties as specified with the 'additionalProperties' keyword in the OAS document.
-   */
-  private Map<String, Object> additionalProperties;
-
-  /**
-   * Set the additional (undeclared) property with the specified name and value. If the property
-   * does not already exist, create it otherwise replace it.
-   *
-   * @param key The arbitrary key to set
-   * @param value The associated value
-   * @return TableResultV2DataAttributesFileMetadataLocalFile
-   */
-  @JsonAnySetter
-  public TableResultV2DataAttributesFileMetadataLocalFile putAdditionalProperty(
-      String key, Object value) {
-    if (this.additionalProperties == null) {
-      this.additionalProperties = new HashMap<String, Object>();
-    }
-    this.additionalProperties.put(key, value);
-    return this;
-  }
-
-  /**
-   * Return the additional (undeclared) property.
-   *
-   * @return The additional properties
-   */
-  @JsonAnyGetter
-  public Map<String, Object> getAdditionalProperties() {
-    return additionalProperties;
-  }
-
-  /**
-   * Return the additional (undeclared) property with the specified name.
-   *
-   * @param key The arbitrary key to get
-   * @return The specific additional property for the given key
-   */
-  public Object getAdditionalProperty(String key) {
-    if (this.additionalProperties == null) {
-      return null;
-    }
-    return this.additionalProperties.get(key);
-  }
-
   /** Return true if this TableResultV2DataAttributesFileMetadataLocalFile object is equal to o. */
   @Override
   public boolean equals(Object o) {
@@ -138,15 +87,12 @@ public class TableResultV2DataAttributesFileMetadataLocalFile {
     return Objects.equals(
             this.errorMessage, tableResultV2DataAttributesFileMetadataLocalFile.errorMessage)
         && Objects.equals(
-            this.errorRowCount, tableResultV2DataAttributesFileMetadataLocalFile.errorRowCount)
-        && Objects.equals(
-            this.additionalProperties,
-            tableResultV2DataAttributesFileMetadataLocalFile.additionalProperties);
+            this.errorRowCount, tableResultV2DataAttributesFileMetadataLocalFile.errorRowCount);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(errorMessage, errorRowCount, additionalProperties);
+    return Objects.hash(errorMessage, errorRowCount);
   }
 
   @Override
@@ -155,9 +101,6 @@ public class TableResultV2DataAttributesFileMetadataLocalFile {
     sb.append("class TableResultV2DataAttributesFileMetadataLocalFile {\n");
     sb.append("    errorMessage: ").append(toIndentedString(errorMessage)).append("\n");
     sb.append("    errorRowCount: ").append(toIndentedString(errorRowCount)).append("\n");
-    sb.append("    additionalProperties: ")
-        .append(toIndentedString(additionalProperties))
-        .append("\n");
     sb.append('}');
     return sb.toString();
   }
