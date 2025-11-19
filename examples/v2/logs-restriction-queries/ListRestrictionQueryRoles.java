6d9feec40df249354c41c960e523d116f8387f76
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-19 15:52:56 +0000

    Regenerate client from commit 8971e48 of spec repo

---
+++ examples/v2/logs-restriction-queries/ListRestrictionQueryRoles.java
@@ -0,0 +1,30 @@
+// List roles for a restriction query returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.LogsRestrictionQueriesApi;
+import com.datadog.api.client.v2.model.RestrictionQueryRolesResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    defaultClient.setUnstableOperationEnabled("v2.listRestrictionQueryRoles", true);
+    LogsRestrictionQueriesApi apiInstance = new LogsRestrictionQueriesApi(defaultClient);
+
+    // there is a valid "restriction_query" in the system
+    String RESTRICTION_QUERY_DATA_ID = System.getenv("RESTRICTION_QUERY_DATA_ID");
+
+    try {
+      RestrictionQueryRolesResponse result =
+          apiInstance.listRestrictionQueryRoles(RESTRICTION_QUERY_DATA_ID);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println(
+          "Exception when calling LogsRestrictionQueriesApi#listRestrictionQueryRoles");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
