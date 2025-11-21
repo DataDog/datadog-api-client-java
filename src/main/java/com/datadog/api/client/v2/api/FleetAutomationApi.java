929e0a15dc950b0938e6c8187db8f0e1a719862a
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-21 16:57:05 +0000

    Regenerate client from commit 5f572cc of spec repo

--- src/main/java/com/datadog/api/client/v2/api/FleetAutomationApi.java
+++ src/main/java/com/datadog/api/client/v2/api/FleetAutomationApi.java
@@ -4,6 +4,7 @@ import com.datadog.api.client.ApiClient;
 import com.datadog.api.client.ApiException;
 import com.datadog.api.client.ApiResponse;
 import com.datadog.api.client.Pair;
+import com.datadog.api.client.v2.model.FleetAgentInfoResponse;
 import com.datadog.api.client.v2.model.FleetAgentVersionsResponse;
 import com.datadog.api.client.v2.model.FleetDeploymentConfigureCreateRequest;
 import com.datadog.api.client.v2.model.FleetDeploymentPackageUpgradeCreateRequest;
@@ -853,6 +854,163 @@ public class FleetAutomationApi {
         new HashMap<String, Object>(),
         false,
         null);
+  }
+
+  /**
+   * Get detailed information about an agent.
+   *
+   * <p>See {@link #getFleetAgentInfoWithHttpInfo}.
+   *
+   * @param agentKey The unique identifier (agent key) for the Datadog Agent. (required)
+   * @return FleetAgentInfoResponse
+   * @throws ApiException if fails to make API call
+   */
+  public FleetAgentInfoResponse getFleetAgentInfo(String agentKey) throws ApiException {
+    return getFleetAgentInfoWithHttpInfo(agentKey).getData();
+  }
+
+  /**
+   * Get detailed information about an agent.
+   *
+   * <p>See {@link #getFleetAgentInfoWithHttpInfoAsync}.
+   *
+   * @param agentKey The unique identifier (agent key) for the Datadog Agent. (required)
+   * @return CompletableFuture&lt;FleetAgentInfoResponse&gt;
+   */
+  public CompletableFuture<FleetAgentInfoResponse> getFleetAgentInfoAsync(String agentKey) {
+    return getFleetAgentInfoWithHttpInfoAsync(agentKey)
+        .thenApply(
+            response -> {
+              return response.getData();
+            });
+  }
+
+  /**
+   * Retrieve detailed information about a specific Datadog Agent. This endpoint returns
+   * comprehensive information about an agent including: - Agent details and metadata - Configured
+   * integrations organized by status (working, warning, error, missing) - Detected integrations -
+   * Configuration files and layers
+   *
+   * @param agentKey The unique identifier (agent key) for the Datadog Agent. (required)
+   * @return ApiResponse&lt;FleetAgentInfoResponse&gt;
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table border="1">
+   *    <caption>Response details</caption>
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 200 </td><td> OK </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 401 </td><td> Unauthorized </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Forbidden </td><td>  -  </td></tr>
+   *       <tr><td> 404 </td><td> Not Found </td><td>  -  </td></tr>
+   *       <tr><td> 429 </td><td> Too many requests </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public ApiResponse<FleetAgentInfoResponse> getFleetAgentInfoWithHttpInfo(String agentKey)
+      throws ApiException {
+    // Check if unstable operation is enabled
+    String operationId = "getFleetAgentInfo";
+    if (apiClient.isUnstableOperationEnabled("v2." + operationId)) {
+      apiClient.getLogger().warning(String.format("Using unstable operation '%s'", operationId));
+    } else {
+      throw new ApiException(0, String.format("Unstable operation '%s' is disabled", operationId));
+    }
+    Object localVarPostBody = null;
+
+    // verify the required parameter 'agentKey' is set
+    if (agentKey == null) {
+      throw new ApiException(
+          400, "Missing the required parameter 'agentKey' when calling getFleetAgentInfo");
+    }
+    // create path and map variables
+    String localVarPath =
+        "/api/unstable/fleet/agents/{agent_key}"
+            .replaceAll("\\{" + "agent_key" + "\\}", apiClient.escapeString(agentKey.toString()));
+
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+    Invocation.Builder builder =
+        apiClient.createBuilder(
+            "v2.FleetAutomationApi.getFleetAgentInfo",
+            localVarPath,
+            new ArrayList<Pair>(),
+            localVarHeaderParams,
+            new HashMap<String, String>(),
+            new String[] {"application/json"},
+            new String[] {"apiKeyAuth", "appKeyAuth"});
+    return apiClient.invokeAPI(
+        "GET",
+        builder,
+        localVarHeaderParams,
+        new String[] {},
+        localVarPostBody,
+        new HashMap<String, Object>(),
+        false,
+        new GenericType<FleetAgentInfoResponse>() {});
+  }
+
+  /**
+   * Get detailed information about an agent.
+   *
+   * <p>See {@link #getFleetAgentInfoWithHttpInfo}.
+   *
+   * @param agentKey The unique identifier (agent key) for the Datadog Agent. (required)
+   * @return CompletableFuture&lt;ApiResponse&lt;FleetAgentInfoResponse&gt;&gt;
+   */
+  public CompletableFuture<ApiResponse<FleetAgentInfoResponse>> getFleetAgentInfoWithHttpInfoAsync(
+      String agentKey) {
+    // Check if unstable operation is enabled
+    String operationId = "getFleetAgentInfo";
+    if (apiClient.isUnstableOperationEnabled("v2." + operationId)) {
+      apiClient.getLogger().warning(String.format("Using unstable operation '%s'", operationId));
+    } else {
+      CompletableFuture<ApiResponse<FleetAgentInfoResponse>> result = new CompletableFuture<>();
+      result.completeExceptionally(
+          new ApiException(0, String.format("Unstable operation '%s' is disabled", operationId)));
+      return result;
+    }
+    Object localVarPostBody = null;
+
+    // verify the required parameter 'agentKey' is set
+    if (agentKey == null) {
+      CompletableFuture<ApiResponse<FleetAgentInfoResponse>> result = new CompletableFuture<>();
+      result.completeExceptionally(
+          new ApiException(
+              400, "Missing the required parameter 'agentKey' when calling getFleetAgentInfo"));
+      return result;
+    }
+    // create path and map variables
+    String localVarPath =
+        "/api/unstable/fleet/agents/{agent_key}"
+            .replaceAll("\\{" + "agent_key" + "\\}", apiClient.escapeString(agentKey.toString()));
+
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+    Invocation.Builder builder;
+    try {
+      builder =
+          apiClient.createBuilder(
+              "v2.FleetAutomationApi.getFleetAgentInfo",
+              localVarPath,
+              new ArrayList<Pair>(),
+              localVarHeaderParams,
+              new HashMap<String, String>(),
+              new String[] {"application/json"},
+              new String[] {"apiKeyAuth", "appKeyAuth"});
+    } catch (ApiException ex) {
+      CompletableFuture<ApiResponse<FleetAgentInfoResponse>> result = new CompletableFuture<>();
+      result.completeExceptionally(ex);
+      return result;
+    }
+    return apiClient.invokeAPIAsync(
+        "GET",
+        builder,
+        localVarHeaderParams,
+        new String[] {},
+        localVarPostBody,
+        new HashMap<String, Object>(),
+        false,
+        new GenericType<FleetAgentInfoResponse>() {});
   }
 
   /** Manage optional parameters to getFleetDeployment. */
---
+++ src/main/java/com/datadog/api/client/v2/model/FleetConfigurationLayer.java
@@ -0,0 +1,287 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** Configuration information organized by layers. */
+@JsonPropertyOrder({
+  FleetConfigurationLayer.JSON_PROPERTY_COMPILED_CONFIGURATION,
+  FleetConfigurationLayer.JSON_PROPERTY_ENV_CONFIGURATION,
+  FleetConfigurationLayer.JSON_PROPERTY_FILE_CONFIGURATION,
+  FleetConfigurationLayer.JSON_PROPERTY_PARSED_CONFIGURATION,
+  FleetConfigurationLayer.JSON_PROPERTY_REMOTE_CONFIGURATION,
+  FleetConfigurationLayer.JSON_PROPERTY_RUNTIME_CONFIGURATION
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class FleetConfigurationLayer {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_COMPILED_CONFIGURATION = "compiled_configuration";
+  private String compiledConfiguration;
+
+  public static final String JSON_PROPERTY_ENV_CONFIGURATION = "env_configuration";
+  private String envConfiguration;
+
+  public static final String JSON_PROPERTY_FILE_CONFIGURATION = "file_configuration";
+  private String fileConfiguration;
+
+  public static final String JSON_PROPERTY_PARSED_CONFIGURATION = "parsed_configuration";
+  private String parsedConfiguration;
+
+  public static final String JSON_PROPERTY_REMOTE_CONFIGURATION = "remote_configuration";
+  private String remoteConfiguration;
+
+  public static final String JSON_PROPERTY_RUNTIME_CONFIGURATION = "runtime_configuration";
+  private String runtimeConfiguration;
+
+  public FleetConfigurationLayer compiledConfiguration(String compiledConfiguration) {
+    this.compiledConfiguration = compiledConfiguration;
+    return this;
+  }
+
+  /**
+   * The final compiled configuration.
+   *
+   * @return compiledConfiguration
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_COMPILED_CONFIGURATION)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getCompiledConfiguration() {
+    return compiledConfiguration;
+  }
+
+  public void setCompiledConfiguration(String compiledConfiguration) {
+    this.compiledConfiguration = compiledConfiguration;
+  }
+
+  public FleetConfigurationLayer envConfiguration(String envConfiguration) {
+    this.envConfiguration = envConfiguration;
+    return this;
+  }
+
+  /**
+   * Configuration from environment variables.
+   *
+   * @return envConfiguration
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ENV_CONFIGURATION)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getEnvConfiguration() {
+    return envConfiguration;
+  }
+
+  public void setEnvConfiguration(String envConfiguration) {
+    this.envConfiguration = envConfiguration;
+  }
+
+  public FleetConfigurationLayer fileConfiguration(String fileConfiguration) {
+    this.fileConfiguration = fileConfiguration;
+    return this;
+  }
+
+  /**
+   * Configuration from files.
+   *
+   * @return fileConfiguration
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_FILE_CONFIGURATION)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getFileConfiguration() {
+    return fileConfiguration;
+  }
+
+  public void setFileConfiguration(String fileConfiguration) {
+    this.fileConfiguration = fileConfiguration;
+  }
+
+  public FleetConfigurationLayer parsedConfiguration(String parsedConfiguration) {
+    this.parsedConfiguration = parsedConfiguration;
+    return this;
+  }
+
+  /**
+   * Parsed configuration output.
+   *
+   * @return parsedConfiguration
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_PARSED_CONFIGURATION)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getParsedConfiguration() {
+    return parsedConfiguration;
+  }
+
+  public void setParsedConfiguration(String parsedConfiguration) {
+    this.parsedConfiguration = parsedConfiguration;
+  }
+
+  public FleetConfigurationLayer remoteConfiguration(String remoteConfiguration) {
+    this.remoteConfiguration = remoteConfiguration;
+    return this;
+  }
+
+  /**
+   * Remote configuration settings.
+   *
+   * @return remoteConfiguration
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_REMOTE_CONFIGURATION)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getRemoteConfiguration() {
+    return remoteConfiguration;
+  }
+
+  public void setRemoteConfiguration(String remoteConfiguration) {
+    this.remoteConfiguration = remoteConfiguration;
+  }
+
+  public FleetConfigurationLayer runtimeConfiguration(String runtimeConfiguration) {
+    this.runtimeConfiguration = runtimeConfiguration;
+    return this;
+  }
+
+  /**
+   * Runtime configuration.
+   *
+   * @return runtimeConfiguration
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_RUNTIME_CONFIGURATION)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getRuntimeConfiguration() {
+    return runtimeConfiguration;
+  }
+
+  public void setRuntimeConfiguration(String runtimeConfiguration) {
+    this.runtimeConfiguration = runtimeConfiguration;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return FleetConfigurationLayer
+   */
+  @JsonAnySetter
+  public FleetConfigurationLayer putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this FleetConfigurationLayer object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    FleetConfigurationLayer fleetConfigurationLayer = (FleetConfigurationLayer) o;
+    return Objects.equals(this.compiledConfiguration, fleetConfigurationLayer.compiledConfiguration)
+        && Objects.equals(this.envConfiguration, fleetConfigurationLayer.envConfiguration)
+        && Objects.equals(this.fileConfiguration, fleetConfigurationLayer.fileConfiguration)
+        && Objects.equals(this.parsedConfiguration, fleetConfigurationLayer.parsedConfiguration)
+        && Objects.equals(this.remoteConfiguration, fleetConfigurationLayer.remoteConfiguration)
+        && Objects.equals(this.runtimeConfiguration, fleetConfigurationLayer.runtimeConfiguration)
+        && Objects.equals(this.additionalProperties, fleetConfigurationLayer.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(
+        compiledConfiguration,
+        envConfiguration,
+        fileConfiguration,
+        parsedConfiguration,
+        remoteConfiguration,
+        runtimeConfiguration,
+        additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class FleetConfigurationLayer {\n");
+    sb.append("    compiledConfiguration: ")
+        .append(toIndentedString(compiledConfiguration))
+        .append("\n");
+    sb.append("    envConfiguration: ").append(toIndentedString(envConfiguration)).append("\n");
+    sb.append("    fileConfiguration: ").append(toIndentedString(fileConfiguration)).append("\n");
+    sb.append("    parsedConfiguration: ")
+        .append(toIndentedString(parsedConfiguration))
+        .append("\n");
+    sb.append("    remoteConfiguration: ")
+        .append(toIndentedString(remoteConfiguration))
+        .append("\n");
+    sb.append("    runtimeConfiguration: ")
+        .append(toIndentedString(runtimeConfiguration))
+        .append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
