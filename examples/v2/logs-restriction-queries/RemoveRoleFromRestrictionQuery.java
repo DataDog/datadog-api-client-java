ca79db178d6a159f0a6f8c447c74c06fc60ea74b
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-27 10:05:04 +0000

    Regenerate client from commit f1a395c of spec repo

---
+++ examples/v2/logs-restriction-queries/RemoveRoleFromRestrictionQuery.java
@@ -0,0 +1,37 @@
+// Revoke role from a restriction query returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.LogsRestrictionQueriesApi;
+import com.datadog.api.client.v2.model.RelationshipToRole;
+import com.datadog.api.client.v2.model.RelationshipToRoleData;
+import com.datadog.api.client.v2.model.RolesType;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    defaultClient.setUnstableOperationEnabled("v2.removeRoleFromRestrictionQuery", true);
+    LogsRestrictionQueriesApi apiInstance = new LogsRestrictionQueriesApi(defaultClient);
+
+    // there is a valid "restriction_query" in the system
+    String RESTRICTION_QUERY_DATA_ID = System.getenv("RESTRICTION_QUERY_DATA_ID");
+
+    // there is a valid "role" in the system
+    String ROLE_DATA_ID = System.getenv("ROLE_DATA_ID");
+
+    RelationshipToRole body =
+        new RelationshipToRole()
+            .data(new RelationshipToRoleData().id(ROLE_DATA_ID).type(RolesType.ROLES));
+
+    try {
+      apiInstance.removeRoleFromRestrictionQuery(RESTRICTION_QUERY_DATA_ID, body);
+    } catch (ApiException e) {
+      System.err.println(
+          "Exception when calling LogsRestrictionQueriesApi#removeRoleFromRestrictionQuery");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
