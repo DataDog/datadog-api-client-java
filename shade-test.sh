c804b3bff4487fe718518f7c0979c5634dc5503e
skarimo <40482491+skarimo@users.noreply.github.com>
2025-12-01 11:04:32 -0500

    Bump the shade plugin to support Java 17 optimizations (#3281)
    * fix build errors
    
    * exit on error

--- shade-test.sh
+++ shade-test.sh
@@ -1,5 +1,8 @@
 # Create the package. Skip tests since this is just for testing the contents of the jar. We've already run the tests in another step.
 mvn package -DskipTests -Dmaven.javadoc.skip=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
 # List everything in the shaded jar. Anything in com/datadog is good since that's either shaded or our own code.
 # We intentionally avoid shading anything in META-INF in the pom.xml.
 # And there are some native libraries in other directories that we don't shade so we only want to check for .class files.
