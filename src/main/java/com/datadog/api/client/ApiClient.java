cd3a7f4ca84ab474aff1c461a2415f0b0194affc
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-11 21:08:37 +0000

    Regenerate client from commit ba739a2 of spec repo

--- src/main/java/com/datadog/api/client/ApiClient.java
+++ src/main/java/com/datadog/api/client/ApiClient.java
@@ -300,6 +300,7 @@ public class ApiClient {
                                               "us3.datadoghq.com",
                                               "us5.datadoghq.com",
                                               "ap1.datadoghq.com",
+                                              "ap2.datadoghq.com",
                                               "datadoghq.eu",
                                               "ddog-gov.com"))));
                               put(
