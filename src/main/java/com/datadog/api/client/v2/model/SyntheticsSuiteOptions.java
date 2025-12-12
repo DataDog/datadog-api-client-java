27447022a1d8ce30ae0b13efab933a386f507d94
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-12 15:18:03 +0000

    Regenerate client from commit 7177820 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/SyntheticsSuiteOptions.java
@@ -0,0 +1,135 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** Object describing the extra options for a Synthetic suite. */
+@JsonPropertyOrder({SyntheticsSuiteOptions.JSON_PROPERTY_ALERTING_THRESHOLD})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class SyntheticsSuiteOptions {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ALERTING_THRESHOLD = "alerting_threshold";
+  private Double alertingThreshold;
+
+  public SyntheticsSuiteOptions alertingThreshold(Double alertingThreshold) {
+    this.alertingThreshold = alertingThreshold;
+    return this;
+  }
+
+  /**
+   * Percentage of critical tests failure needed for a suite to fail. minimum: 0 maximum: 1
+   *
+   * @return alertingThreshold
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ALERTING_THRESHOLD)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public Double getAlertingThreshold() {
+    return alertingThreshold;
+  }
+
+  public void setAlertingThreshold(Double alertingThreshold) {
+    this.alertingThreshold = alertingThreshold;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return SyntheticsSuiteOptions
+   */
+  @JsonAnySetter
+  public SyntheticsSuiteOptions putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this SyntheticsSuiteOptions object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SyntheticsSuiteOptions syntheticsSuiteOptions = (SyntheticsSuiteOptions) o;
+    return Objects.equals(this.alertingThreshold, syntheticsSuiteOptions.alertingThreshold)
+        && Objects.equals(this.additionalProperties, syntheticsSuiteOptions.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(alertingThreshold, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class SyntheticsSuiteOptions {\n");
+    sb.append("    alertingThreshold: ").append(toIndentedString(alertingThreshold)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
