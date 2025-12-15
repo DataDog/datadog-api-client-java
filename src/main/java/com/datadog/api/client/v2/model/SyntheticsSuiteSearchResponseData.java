ba085dc6cb55a2b59a7c6e928487f246f2af97bb
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-15 14:00:05 +0000

    Regenerate client from commit 536d80d of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/SyntheticsSuiteSearchResponseData.java
@@ -0,0 +1,200 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+/** Synthetics suite search response data */
+@JsonPropertyOrder({
+  SyntheticsSuiteSearchResponseData.JSON_PROPERTY_ATTRIBUTES,
+  SyntheticsSuiteSearchResponseData.JSON_PROPERTY_ID,
+  SyntheticsSuiteSearchResponseData.JSON_PROPERTY_TYPE
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class SyntheticsSuiteSearchResponseData {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ATTRIBUTES = "attributes";
+  private SyntheticsSuiteSearchResponseDataAttributes attributes;
+
+  public static final String JSON_PROPERTY_ID = "id";
+  private UUID id;
+
+  public static final String JSON_PROPERTY_TYPE = "type";
+  private SuiteSearchResponseType type = SuiteSearchResponseType.SUITES_SEARCH;
+
+  public SyntheticsSuiteSearchResponseData attributes(
+      SyntheticsSuiteSearchResponseDataAttributes attributes) {
+    this.attributes = attributes;
+    this.unparsed |= attributes.unparsed;
+    return this;
+  }
+
+  /**
+   * Synthetics suite search response data attributes
+   *
+   * @return attributes
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ATTRIBUTES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public SyntheticsSuiteSearchResponseDataAttributes getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(SyntheticsSuiteSearchResponseDataAttributes attributes) {
+    this.attributes = attributes;
+  }
+
+  public SyntheticsSuiteSearchResponseData id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Getid
+   *
+   * @return id
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ID)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public SyntheticsSuiteSearchResponseData type(SuiteSearchResponseType type) {
+    this.type = type;
+    this.unparsed |= !type.isValid();
+    return this;
+  }
+
+  /**
+   * Gettype
+   *
+   * @return type
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_TYPE)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public SuiteSearchResponseType getType() {
+    return type;
+  }
+
+  public void setType(SuiteSearchResponseType type) {
+    if (!type.isValid()) {
+      this.unparsed = true;
+    }
+    this.type = type;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return SyntheticsSuiteSearchResponseData
+   */
+  @JsonAnySetter
+  public SyntheticsSuiteSearchResponseData putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this SyntheticsSuiteSearchResponseData object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SyntheticsSuiteSearchResponseData syntheticsSuiteSearchResponseData =
+        (SyntheticsSuiteSearchResponseData) o;
+    return Objects.equals(this.attributes, syntheticsSuiteSearchResponseData.attributes)
+        && Objects.equals(this.id, syntheticsSuiteSearchResponseData.id)
+        && Objects.equals(this.type, syntheticsSuiteSearchResponseData.type)
+        && Objects.equals(
+            this.additionalProperties, syntheticsSuiteSearchResponseData.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(attributes, id, type, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class SyntheticsSuiteSearchResponseData {\n");
+    sb.append("    attributes: ").append(toIndentedString(attributes)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
