c57a62938b00418effa7b95a97eb8e301e2a75bd
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-24 11:24:16 +0000

    Regenerate client from commit 1214813 of spec repo

---
+++ examples/v2/teams/GetTeamNotificationRule.java
@@ -0,0 +1,31 @@
+// Get team notification rule returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.TeamsApi;
+import com.datadog.api.client.v2.model.TeamNotificationRuleResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    TeamsApi apiInstance = new TeamsApi(defaultClient);
+
+    // there is a valid "dd_team" in the system
+    String DD_TEAM_DATA_ID = System.getenv("DD_TEAM_DATA_ID");
+
+    // there is a valid "team_notification_rule" in the system
+    String TEAM_NOTIFICATION_RULE_DATA_ID = System.getenv("TEAM_NOTIFICATION_RULE_DATA_ID");
+
+    try {
+      TeamNotificationRuleResponse result =
+          apiInstance.getTeamNotificationRule(DD_TEAM_DATA_ID, TEAM_NOTIFICATION_RULE_DATA_ID);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling TeamsApi#getTeamNotificationRule");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/main/java/com/datadog/api/client/v2/model/TeamNotificationRuleType.java
@@ -0,0 +1,57 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.datadog.api.client.ModelEnum;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/** Team notification rule type */
+@JsonSerialize(using = TeamNotificationRuleType.TeamNotificationRuleTypeSerializer.class)
+public class TeamNotificationRuleType extends ModelEnum<String> {
+
+  private static final Set<String> allowedValues =
+      new HashSet<String>(Arrays.asList("team_notification_rules"));
+
+  public static final TeamNotificationRuleType TEAM_NOTIFICATION_RULES =
+      new TeamNotificationRuleType("team_notification_rules");
+
+  TeamNotificationRuleType(String value) {
+    super(value, allowedValues);
+  }
+
+  public static class TeamNotificationRuleTypeSerializer
+      extends StdSerializer<TeamNotificationRuleType> {
+    public TeamNotificationRuleTypeSerializer(Class<TeamNotificationRuleType> t) {
+      super(t);
+    }
+
+    public TeamNotificationRuleTypeSerializer() {
+      this(null);
+    }
+
+    @Override
+    public void serialize(
+        TeamNotificationRuleType value, JsonGenerator jgen, SerializerProvider provider)
+        throws IOException, JsonProcessingException {
+      jgen.writeObject(value.value);
+    }
+  }
+
+  @JsonCreator
+  public static TeamNotificationRuleType fromValue(String value) {
+    return new TeamNotificationRuleType(value);
+  }
+}
