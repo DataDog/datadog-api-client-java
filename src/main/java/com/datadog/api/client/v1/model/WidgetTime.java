6421881fe6bc8f7e35321c3e69226c2a4154383c
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-09 14:41:37 +0000

    Regenerate client from commit ac4e81b of spec repo

--- src/main/java/com/datadog/api/client/v1/model/WidgetTime.java
+++ src/main/java/com/datadog/api/client/v1/model/WidgetTime.java
@@ -77,51 +77,6 @@ public class WidgetTime extends AbstractOpenApiSchema {
       boolean typeCoercion = ctxt.isEnabled(MapperFeature.ALLOW_COERCION_OF_SCALARS);
       int match = 0;
       JsonToken token = tree.traverse(jp.getCodec()).nextToken();
-      // deserialize WidgetLegacyLiveSpan
-      try {
-        boolean attemptParsing = true;
-        // ensure that we respect type coercion as set on the client ObjectMapper
-        if (WidgetLegacyLiveSpan.class.equals(Integer.class)
-            || WidgetLegacyLiveSpan.class.equals(Long.class)
-            || WidgetLegacyLiveSpan.class.equals(Float.class)
-            || WidgetLegacyLiveSpan.class.equals(Double.class)
-            || WidgetLegacyLiveSpan.class.equals(Boolean.class)
-            || WidgetLegacyLiveSpan.class.equals(String.class)) {
-          attemptParsing = typeCoercion;
-          if (!attemptParsing) {
-            attemptParsing |=
-                ((WidgetLegacyLiveSpan.class.equals(Integer.class)
-                        || WidgetLegacyLiveSpan.class.equals(Long.class))
-                    && token == JsonToken.VALUE_NUMBER_INT);
-            attemptParsing |=
-                ((WidgetLegacyLiveSpan.class.equals(Float.class)
-                        || WidgetLegacyLiveSpan.class.equals(Double.class))
-                    && (token == JsonToken.VALUE_NUMBER_FLOAT
-                        || token == JsonToken.VALUE_NUMBER_INT));
-            attemptParsing |=
-                (WidgetLegacyLiveSpan.class.equals(Boolean.class)
-                    && (token == JsonToken.VALUE_FALSE || token == JsonToken.VALUE_TRUE));
-            attemptParsing |=
-                (WidgetLegacyLiveSpan.class.equals(String.class)
-                    && token == JsonToken.VALUE_STRING);
-          }
-        }
-        if (attemptParsing) {
-          tmp = tree.traverse(jp.getCodec()).readValueAs(WidgetLegacyLiveSpan.class);
-          // TODO: there is no validation against JSON schema constraints
-          // (min, max, enum, pattern...), this does not perform a strict JSON
-          // validation, which means the 'match' count may be higher than it should be.
-          if (!((WidgetLegacyLiveSpan) tmp).unparsed) {
-            deserialized = tmp;
-            match++;
-          }
-          log.log(Level.FINER, "Input data matches schema 'WidgetLegacyLiveSpan'");
-        }
-      } catch (Exception e) {
-        // deserialization failed, continue
-        log.log(Level.FINER, "Input data does not match schema 'WidgetLegacyLiveSpan'", e);
-      }
-
       // deserialize WidgetNewLiveSpan
       try {
         boolean attemptParsing = true;
@@ -210,6 +165,51 @@ public class WidgetTime extends AbstractOpenApiSchema {
         log.log(Level.FINER, "Input data does not match schema 'WidgetNewFixedSpan'", e);
       }
 
+      // deserialize WidgetLegacyLiveSpan
+      try {
+        boolean attemptParsing = true;
+        // ensure that we respect type coercion as set on the client ObjectMapper
+        if (WidgetLegacyLiveSpan.class.equals(Integer.class)
+            || WidgetLegacyLiveSpan.class.equals(Long.class)
+            || WidgetLegacyLiveSpan.class.equals(Float.class)
+            || WidgetLegacyLiveSpan.class.equals(Double.class)
+            || WidgetLegacyLiveSpan.class.equals(Boolean.class)
+            || WidgetLegacyLiveSpan.class.equals(String.class)) {
+          attemptParsing = typeCoercion;
+          if (!attemptParsing) {
+            attemptParsing |=
+                ((WidgetLegacyLiveSpan.class.equals(Integer.class)
+                        || WidgetLegacyLiveSpan.class.equals(Long.class))
+                    && token == JsonToken.VALUE_NUMBER_INT);
+            attemptParsing |=
+                ((WidgetLegacyLiveSpan.class.equals(Float.class)
+                        || WidgetLegacyLiveSpan.class.equals(Double.class))
+                    && (token == JsonToken.VALUE_NUMBER_FLOAT
+                        || token == JsonToken.VALUE_NUMBER_INT));
+            attemptParsing |=
+                (WidgetLegacyLiveSpan.class.equals(Boolean.class)
+                    && (token == JsonToken.VALUE_FALSE || token == JsonToken.VALUE_TRUE));
+            attemptParsing |=
+                (WidgetLegacyLiveSpan.class.equals(String.class)
+                    && token == JsonToken.VALUE_STRING);
+          }
+        }
+        if (attemptParsing) {
+          tmp = tree.traverse(jp.getCodec()).readValueAs(WidgetLegacyLiveSpan.class);
+          // TODO: there is no validation against JSON schema constraints
+          // (min, max, enum, pattern...), this does not perform a strict JSON
+          // validation, which means the 'match' count may be higher than it should be.
+          if (!((WidgetLegacyLiveSpan) tmp).unparsed) {
+            deserialized = tmp;
+            match++;
+          }
+          log.log(Level.FINER, "Input data matches schema 'WidgetLegacyLiveSpan'");
+        }
+      } catch (Exception e) {
+        // deserialization failed, continue
+        log.log(Level.FINER, "Input data does not match schema 'WidgetLegacyLiveSpan'", e);
+      }
+
       WidgetTime ret = new WidgetTime();
       if (match == 1) {
         ret.setActualInstance(deserialized);
@@ -238,11 +238,6 @@ public class WidgetTime extends AbstractOpenApiSchema {
     super("oneOf", Boolean.FALSE);
   }
 
-  public WidgetTime(WidgetLegacyLiveSpan o) {
-    super("oneOf", Boolean.FALSE);
-    setActualInstance(o);
-  }
-
   public WidgetTime(WidgetNewLiveSpan o) {
     super("oneOf", Boolean.FALSE);
     setActualInstance(o);
@@ -253,10 +248,15 @@ public class WidgetTime extends AbstractOpenApiSchema {
     setActualInstance(o);
   }
 
+  public WidgetTime(WidgetLegacyLiveSpan o) {
+    super("oneOf", Boolean.FALSE);
+    setActualInstance(o);
+  }
+
   static {
-    schemas.put("WidgetLegacyLiveSpan", new GenericType<WidgetLegacyLiveSpan>() {});
     schemas.put("WidgetNewLiveSpan", new GenericType<WidgetNewLiveSpan>() {});
     schemas.put("WidgetNewFixedSpan", new GenericType<WidgetNewFixedSpan>() {});
+    schemas.put("WidgetLegacyLiveSpan", new GenericType<WidgetLegacyLiveSpan>() {});
     JSON.registerDescendants(WidgetTime.class, Collections.unmodifiableMap(schemas));
   }
 
@@ -267,22 +267,22 @@ public class WidgetTime extends AbstractOpenApiSchema {
 
   /**
    * Set the instance that matches the oneOf child schema, check the instance parameter is valid
-   * against the oneOf child schemas: WidgetLegacyLiveSpan, WidgetNewLiveSpan, WidgetNewFixedSpan
+   * against the oneOf child schemas: WidgetNewLiveSpan, WidgetNewFixedSpan, WidgetLegacyLiveSpan
    *
    * <p>It could be an instance of the 'oneOf' schemas. The oneOf child schemas may themselves be a
    * composed schema (allOf, anyOf, oneOf).
    */
   @Override
   public void setActualInstance(Object instance) {
-    if (JSON.isInstanceOf(WidgetLegacyLiveSpan.class, instance, new HashSet<Class<?>>())) {
-      super.setActualInstance(instance);
-      return;
-    }
     if (JSON.isInstanceOf(WidgetNewLiveSpan.class, instance, new HashSet<Class<?>>())) {
       super.setActualInstance(instance);
       return;
     }
     if (JSON.isInstanceOf(WidgetNewFixedSpan.class, instance, new HashSet<Class<?>>())) {
+      super.setActualInstance(instance);
+      return;
+    }
+    if (JSON.isInstanceOf(WidgetLegacyLiveSpan.class, instance, new HashSet<Class<?>>())) {
       super.setActualInstance(instance);
       return;
     }
@@ -292,30 +292,19 @@ public class WidgetTime extends AbstractOpenApiSchema {
       return;
     }
     throw new RuntimeException(
-        "Invalid instance type. Must be WidgetLegacyLiveSpan, WidgetNewLiveSpan,"
-            + " WidgetNewFixedSpan");
+        "Invalid instance type. Must be WidgetNewLiveSpan, WidgetNewFixedSpan,"
+            + " WidgetLegacyLiveSpan");
   }
 
   /**
-   * Get the actual instance, which can be the following: WidgetLegacyLiveSpan, WidgetNewLiveSpan,
-   * WidgetNewFixedSpan
+   * Get the actual instance, which can be the following: WidgetNewLiveSpan, WidgetNewFixedSpan,
+   * WidgetLegacyLiveSpan
    *
-   * @return The actual instance (WidgetLegacyLiveSpan, WidgetNewLiveSpan, WidgetNewFixedSpan)
+   * @return The actual instance (WidgetNewLiveSpan, WidgetNewFixedSpan, WidgetLegacyLiveSpan)
    */
   @Override
   public Object getActualInstance() {
     return super.getActualInstance();
-  }
-
-  /**
-   * Get the actual instance of `WidgetLegacyLiveSpan`. If the actual instance is not
-   * `WidgetLegacyLiveSpan`, the ClassCastException will be thrown.
-   *
-   * @return The actual instance of `WidgetLegacyLiveSpan`
-   * @throws ClassCastException if the instance is not `WidgetLegacyLiveSpan`
-   */
-  public WidgetLegacyLiveSpan getWidgetLegacyLiveSpan() throws ClassCastException {
-    return (WidgetLegacyLiveSpan) super.getActualInstance();
   }
 
   /**
@@ -338,5 +327,16 @@ public class WidgetTime extends AbstractOpenApiSchema {
    */
   public WidgetNewFixedSpan getWidgetNewFixedSpan() throws ClassCastException {
     return (WidgetNewFixedSpan) super.getActualInstance();
+  }
+
+  /**
+   * Get the actual instance of `WidgetLegacyLiveSpan`. If the actual instance is not
+   * `WidgetLegacyLiveSpan`, the ClassCastException will be thrown.
+   *
+   * @return The actual instance of `WidgetLegacyLiveSpan`
+   * @throws ClassCastException if the instance is not `WidgetLegacyLiveSpan`
+   */
+  public WidgetLegacyLiveSpan getWidgetLegacyLiveSpan() throws ClassCastException {
+    return (WidgetLegacyLiveSpan) super.getActualInstance();
   }
 }
