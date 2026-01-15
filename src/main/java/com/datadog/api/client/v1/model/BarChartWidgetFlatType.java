f76b74454693cbf4a25ab4b3537451d70ff7fab5
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-15 10:14:20 +0000

    Regenerate client from commit c501bee of spec repo

---
+++ src/main/java/com/datadog/api/client/v1/model/BarChartWidgetFlatType.java
@@ -0,0 +1,55 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v1.model;
+
+import com.datadog.api.client.ModelEnum;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/** Bar chart widget flat display type. */
+@JsonSerialize(using = BarChartWidgetFlatType.BarChartWidgetFlatTypeSerializer.class)
+public class BarChartWidgetFlatType extends ModelEnum<String> {
+
+  private static final Set<String> allowedValues = new HashSet<String>(Arrays.asList("flat"));
+
+  public static final BarChartWidgetFlatType FLAT = new BarChartWidgetFlatType("flat");
+
+  BarChartWidgetFlatType(String value) {
+    super(value, allowedValues);
+  }
+
+  public static class BarChartWidgetFlatTypeSerializer
+      extends StdSerializer<BarChartWidgetFlatType> {
+    public BarChartWidgetFlatTypeSerializer(Class<BarChartWidgetFlatType> t) {
+      super(t);
+    }
+
+    public BarChartWidgetFlatTypeSerializer() {
+      this(null);
+    }
+
+    @Override
+    public void serialize(
+        BarChartWidgetFlatType value, JsonGenerator jgen, SerializerProvider provider)
+        throws IOException, JsonProcessingException {
+      jgen.writeObject(value.value);
+    }
+  }
+
+  @JsonCreator
+  public static BarChartWidgetFlatType fromValue(String value) {
+    return new BarChartWidgetFlatType(value);
+  }
+}
