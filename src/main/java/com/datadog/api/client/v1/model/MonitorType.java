0e2d0e0cc87b736b51c0e554282b102296786ae3
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-20 21:33:18 +0000

    Regenerate client from commit 5db4165 of spec repo

--- src/main/java/com/datadog/api/client/v1/model/MonitorType.java
+++ src/main/java/com/datadog/api/client/v1/model/MonitorType.java
@@ -47,7 +47,8 @@ public class MonitorType extends ModelEnum<String> {
               "database-monitoring alert",
               "network-performance alert",
               "cost alert",
-              "data-quality alert"));
+              "data-quality alert",
+              "network-path alert"));
 
   public static final MonitorType COMPOSITE = new MonitorType("composite");
   public static final MonitorType EVENT_ALERT = new MonitorType("event alert");
@@ -71,6 +72,7 @@ public class MonitorType extends ModelEnum<String> {
       new MonitorType("network-performance alert");
   public static final MonitorType COST_ALERT = new MonitorType("cost alert");
   public static final MonitorType DATA_QUALITY_ALERT = new MonitorType("data-quality alert");
+  public static final MonitorType NETWORK_PATH_ALERT = new MonitorType("network-path alert");
 
   MonitorType(String value) {
     super(value, allowedValues);
