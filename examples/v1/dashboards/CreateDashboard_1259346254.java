461b31844cfcd15c9fe92bba0a4983230b79e8bc
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-22 17:32:56 +0000

    Regenerate client from commit 83ca8fa of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_1259346254.java
@@ -0,0 +1,55 @@
+// Create a new dashboard with timeseries widget using order_by values
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.DashboardsApi;
+import com.datadog.api.client.v1.model.Dashboard;
+import com.datadog.api.client.v1.model.DashboardLayoutType;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinition;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinitionType;
+import com.datadog.api.client.v1.model.TimeseriesWidgetRequest;
+import com.datadog.api.client.v1.model.Widget;
+import com.datadog.api.client.v1.model.WidgetDefinition;
+import com.datadog.api.client.v1.model.WidgetDisplayType;
+import com.datadog.api.client.v1.model.WidgetRequestStyle;
+import com.datadog.api.client.v1.model.WidgetStyleOrderBy;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    Dashboard body =
+        new Dashboard()
+            .layoutType(DashboardLayoutType.ORDERED)
+            .title("Example-Dashboard with order_by values")
+            .widgets(
+                Collections.singletonList(
+                    new Widget()
+                        .definition(
+                            new WidgetDefinition(
+                                new TimeseriesWidgetDefinition()
+                                    .type(TimeseriesWidgetDefinitionType.TIMESERIES)
+                                    .requests(
+                                        Collections.singletonList(
+                                            new TimeseriesWidgetRequest()
+                                                .q("avg:system.cpu.user{*} by {host}")
+                                                .style(
+                                                    new WidgetRequestStyle()
+                                                        .palette("warm")
+                                                        .orderBy(WidgetStyleOrderBy.VALUES))
+                                                .displayType(WidgetDisplayType.LINE)))))));
+
+    try {
+      Dashboard result = apiInstance.createDashboard(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#createDashboard");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
---
+++ examples/v1/dashboards/CreateDashboard_3631423980.java
@@ -0,0 +1,57 @@
+// Create a new dashboard with timeseries widget without order_by for backward compatibility
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.DashboardsApi;
+import com.datadog.api.client.v1.model.Dashboard;
+import com.datadog.api.client.v1.model.DashboardLayoutType;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinition;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinitionType;
+import com.datadog.api.client.v1.model.TimeseriesWidgetRequest;
+import com.datadog.api.client.v1.model.Widget;
+import com.datadog.api.client.v1.model.WidgetDefinition;
+import com.datadog.api.client.v1.model.WidgetDisplayType;
+import com.datadog.api.client.v1.model.WidgetLineType;
+import com.datadog.api.client.v1.model.WidgetLineWidth;
+import com.datadog.api.client.v1.model.WidgetRequestStyle;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    Dashboard body =
+        new Dashboard()
+            .layoutType(DashboardLayoutType.ORDERED)
+            .title("Example-Dashboard without order_by")
+            .widgets(
+                Collections.singletonList(
+                    new Widget()
+                        .definition(
+                            new WidgetDefinition(
+                                new TimeseriesWidgetDefinition()
+                                    .type(TimeseriesWidgetDefinitionType.TIMESERIES)
+                                    .requests(
+                                        Collections.singletonList(
+                                            new TimeseriesWidgetRequest()
+                                                .q("avg:system.cpu.user{*} by {host}")
+                                                .style(
+                                                    new WidgetRequestStyle()
+                                                        .palette("dog_classic")
+                                                        .lineType(WidgetLineType.SOLID)
+                                                        .lineWidth(WidgetLineWidth.NORMAL))
+                                                .displayType(WidgetDisplayType.LINE)))))));
+
+    try {
+      Dashboard result = apiInstance.createDashboard(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#createDashboard");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
