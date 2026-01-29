fe71ffa7f324a8aaac11e53d28b06281b278d658
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-29 10:58:24 +0000

    Regenerate client from commit 4862f88 of spec repo

--- src/main/java/com/datadog/api/client/v2/api/DoraMetricsApi.java
+++ src/main/java/com/datadog/api/client/v2/api/DoraMetricsApi.java
@@ -5,6 +5,7 @@ import com.datadog.api.client.ApiException;
 import com.datadog.api.client.ApiResponse;
 import com.datadog.api.client.Pair;
 import com.datadog.api.client.v2.model.DORADeploymentFetchResponse;
+import com.datadog.api.client.v2.model.DORADeploymentPatchRequest;
 import com.datadog.api.client.v2.model.DORADeploymentRequest;
 import com.datadog.api.client.v2.model.DORADeploymentResponse;
 import com.datadog.api.client.v2.model.DORADeploymentsListResponse;
@@ -1278,5 +1279,163 @@ public class DoraMetricsApi {
         new HashMap<String, Object>(),
         false,
         new GenericType<DORAFailuresListResponse>() {});
+  }
+
+  /**
+   * Patch a deployment event.
+   *
+   * <p>See {@link #patchDORADeploymentWithHttpInfo}.
+   *
+   * @param deploymentId The ID of the deployment event. (required)
+   * @param body (required)
+   * @throws ApiException if fails to make API call
+   */
+  public void patchDORADeployment(String deploymentId, DORADeploymentPatchRequest body)
+      throws ApiException {
+    patchDORADeploymentWithHttpInfo(deploymentId, body);
+  }
+
+  /**
+   * Patch a deployment event.
+   *
+   * <p>See {@link #patchDORADeploymentWithHttpInfoAsync}.
+   *
+   * @param deploymentId The ID of the deployment event. (required)
+   * @param body (required)
+   * @return CompletableFuture
+   */
+  public CompletableFuture<Void> patchDORADeploymentAsync(
+      String deploymentId, DORADeploymentPatchRequest body) {
+    return patchDORADeploymentWithHttpInfoAsync(deploymentId, body)
+        .thenApply(
+            response -> {
+              return response.getData();
+            });
+  }
+
+  /**
+   * Use this API endpoint to patch a deployment event.
+   *
+   * @param deploymentId The ID of the deployment event. (required)
+   * @param body (required)
+   * @return ApiResponse&lt;Void&gt;
+   * @throws ApiException if fails to make API call
+   * @http.response.details
+   *     <table border="1">
+   *    <caption>Response details</caption>
+   *       <tr><td> Status Code </td><td> Description </td><td> Response Headers </td></tr>
+   *       <tr><td> 202 </td><td> Accepted </td><td>  -  </td></tr>
+   *       <tr><td> 400 </td><td> Bad Request </td><td>  -  </td></tr>
+   *       <tr><td> 403 </td><td> Not Authorized </td><td>  -  </td></tr>
+   *       <tr><td> 429 </td><td> Too many requests </td><td>  -  </td></tr>
+   *     </table>
+   */
+  public ApiResponse<Void> patchDORADeploymentWithHttpInfo(
+      String deploymentId, DORADeploymentPatchRequest body) throws ApiException {
+    Object localVarPostBody = body;
+
+    // verify the required parameter 'deploymentId' is set
+    if (deploymentId == null) {
+      throw new ApiException(
+          400, "Missing the required parameter 'deploymentId' when calling patchDORADeployment");
+    }
+
+    // verify the required parameter 'body' is set
+    if (body == null) {
+      throw new ApiException(
+          400, "Missing the required parameter 'body' when calling patchDORADeployment");
+    }
+    // create path and map variables
+    String localVarPath =
+        "/api/v2/dora/deployments/{deployment_id}"
+            .replaceAll(
+                "\\{" + "deployment_id" + "\\}", apiClient.escapeString(deploymentId.toString()));
+
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+    Invocation.Builder builder =
+        apiClient.createBuilder(
+            "v2.DoraMetricsApi.patchDORADeployment",
+            localVarPath,
+            new ArrayList<Pair>(),
+            localVarHeaderParams,
+            new HashMap<String, String>(),
+            new String[] {"*/*"},
+            new String[] {"apiKeyAuth", "appKeyAuth"});
+    return apiClient.invokeAPI(
+        "PATCH",
+        builder,
+        localVarHeaderParams,
+        new String[] {"application/json"},
+        localVarPostBody,
+        new HashMap<String, Object>(),
+        false,
+        null);
+  }
+
+  /**
+   * Patch a deployment event.
+   *
+   * <p>See {@link #patchDORADeploymentWithHttpInfo}.
+   *
+   * @param deploymentId The ID of the deployment event. (required)
+   * @param body (required)
+   * @return CompletableFuture&lt;ApiResponse&lt;Void&gt;&gt;
+   */
+  public CompletableFuture<ApiResponse<Void>> patchDORADeploymentWithHttpInfoAsync(
+      String deploymentId, DORADeploymentPatchRequest body) {
+    Object localVarPostBody = body;
+
+    // verify the required parameter 'deploymentId' is set
+    if (deploymentId == null) {
+      CompletableFuture<ApiResponse<Void>> result = new CompletableFuture<>();
+      result.completeExceptionally(
+          new ApiException(
+              400,
+              "Missing the required parameter 'deploymentId' when calling patchDORADeployment"));
+      return result;
+    }
+
+    // verify the required parameter 'body' is set
+    if (body == null) {
+      CompletableFuture<ApiResponse<Void>> result = new CompletableFuture<>();
+      result.completeExceptionally(
+          new ApiException(
+              400, "Missing the required parameter 'body' when calling patchDORADeployment"));
+      return result;
+    }
+    // create path and map variables
+    String localVarPath =
+        "/api/v2/dora/deployments/{deployment_id}"
+            .replaceAll(
+                "\\{" + "deployment_id" + "\\}", apiClient.escapeString(deploymentId.toString()));
+
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+
+    Invocation.Builder builder;
+    try {
+      builder =
+          apiClient.createBuilder(
+              "v2.DoraMetricsApi.patchDORADeployment",
+              localVarPath,
+              new ArrayList<Pair>(),
+              localVarHeaderParams,
+              new HashMap<String, String>(),
+              new String[] {"*/*"},
+              new String[] {"apiKeyAuth", "appKeyAuth"});
+    } catch (ApiException ex) {
+      CompletableFuture<ApiResponse<Void>> result = new CompletableFuture<>();
+      result.completeExceptionally(ex);
+      return result;
+    }
+    return apiClient.invokeAPIAsync(
+        "PATCH",
+        builder,
+        localVarHeaderParams,
+        new String[] {"application/json"},
+        localVarPostBody,
+        new HashMap<String, Object>(),
+        false,
+        null);
   }
 }
