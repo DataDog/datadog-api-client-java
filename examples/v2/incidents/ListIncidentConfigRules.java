ee0da665c65d764930a45d7747402e7ab4c51de8
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-29 15:46:21 +0000

    Regenerate client from commit a1b9ec2 of spec repo

---
+++ examples/v2/incidents/ListIncidentConfigRules.java
@@ -0,0 +1,25 @@
+// List incident rules returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.IncidentsApi;
+import com.datadog.api.client.v2.model.IncidentRulesResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    defaultClient.setUnstableOperationEnabled("v2.listIncidentConfigRules", true);
+    IncidentsApi apiInstance = new IncidentsApi(defaultClient);
+
+    try {
+      IncidentRulesResponse result = apiInstance.listIncidentConfigRules();
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling IncidentsApi#listIncidentConfigRules");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
