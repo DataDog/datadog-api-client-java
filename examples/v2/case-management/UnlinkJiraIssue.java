4b16dacaa6891aea13c2ce0323611f55569c43db
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-06 15:18:01 +0000

    Regenerate client from commit f52831f of spec repo

---
+++ examples/v2/case-management/UnlinkJiraIssue.java
@@ -0,0 +1,23 @@
+// Remove Jira issue link from case returns "No Content" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.CaseManagementApi;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    defaultClient.setUnstableOperationEnabled("v2.unlinkJiraIssue", true);
+    CaseManagementApi apiInstance = new CaseManagementApi(defaultClient);
+
+    try {
+      apiInstance.unlinkJiraIssue("f98a5a5b-e0ff-45d4-b2f5-afe6e74de504");
+    } catch (ApiException e) {
+      System.err.println("Exception when calling CaseManagementApi#unlinkJiraIssue");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
