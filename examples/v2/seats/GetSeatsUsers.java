c6f4c22d5db68b6aa4c04f8315a8bbb195d513e9
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-11 22:03:03 +0000

    Regenerate client from commit 73a4677 of spec repo

---
+++ examples/v2/seats/GetSeatsUsers.java
@@ -0,0 +1,27 @@
+// Get users with seats returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.SeatsApi;
+import com.datadog.api.client.v2.api.SeatsApi.GetSeatsUsersOptionalParameters;
+import com.datadog.api.client.v2.model.SeatUserDataArray;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    SeatsApi apiInstance = new SeatsApi(defaultClient);
+
+    try {
+      SeatUserDataArray result =
+          apiInstance.getSeatsUsers(
+              "incident_response", new GetSeatsUsersOptionalParameters().pageLimit(100));
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling SeatsApi#getSeatsUsers");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
---
+++ examples/v2/seats/UnassignSeatsUser.java
@@ -0,0 +1,40 @@
+// Unassign seats from users returns "No Content" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.SeatsApi;
+import com.datadog.api.client.v2.model.SeatAssignmentsDataType;
+import com.datadog.api.client.v2.model.UnassignSeatsUserRequest;
+import com.datadog.api.client.v2.model.UnassignSeatsUserRequestData;
+import com.datadog.api.client.v2.model.UnassignSeatsUserRequestDataAttributes;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    SeatsApi apiInstance = new SeatsApi(defaultClient);
+
+    // there is a valid "user" in the system
+    String USER_DATA_ID = System.getenv("USER_DATA_ID");
+
+    UnassignSeatsUserRequest body =
+        new UnassignSeatsUserRequest()
+            .data(
+                new UnassignSeatsUserRequestData()
+                    .type(SeatAssignmentsDataType.SEAT_ASSIGNMENTS)
+                    .attributes(
+                        new UnassignSeatsUserRequestDataAttributes()
+                            .productCode("incident_response")
+                            .userUuids(Collections.singletonList(USER_DATA_ID))));
+
+    try {
+      apiInstance.unassignSeatsUser(body);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling SeatsApi#unassignSeatsUser");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
