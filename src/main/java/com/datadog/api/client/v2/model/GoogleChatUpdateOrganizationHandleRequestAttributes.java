5869f5a21fc4f96f3bebe7be29e8d881385556f4
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-20 16:58:12 +0000

    Regenerate client from commit 23789bc of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/GoogleChatUpdateOrganizationHandleRequestAttributes.java
@@ -0,0 +1,174 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** Organization handle attributes for an update request. */
+@JsonPropertyOrder({
+  GoogleChatUpdateOrganizationHandleRequestAttributes.JSON_PROPERTY_NAME,
+  GoogleChatUpdateOrganizationHandleRequestAttributes.JSON_PROPERTY_SPACE_RESOURCE_NAME
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class GoogleChatUpdateOrganizationHandleRequestAttributes {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_NAME = "name";
+  private String name;
+
+  public static final String JSON_PROPERTY_SPACE_RESOURCE_NAME = "space_resource_name";
+  private String spaceResourceName;
+
+  public GoogleChatUpdateOrganizationHandleRequestAttributes name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Organization handle name.
+   *
+   * @return name
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public GoogleChatUpdateOrganizationHandleRequestAttributes spaceResourceName(
+      String spaceResourceName) {
+    this.spaceResourceName = spaceResourceName;
+    return this;
+  }
+
+  /**
+   * Google space resource name.
+   *
+   * @return spaceResourceName
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_SPACE_RESOURCE_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getSpaceResourceName() {
+    return spaceResourceName;
+  }
+
+  public void setSpaceResourceName(String spaceResourceName) {
+    this.spaceResourceName = spaceResourceName;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return GoogleChatUpdateOrganizationHandleRequestAttributes
+   */
+  @JsonAnySetter
+  public GoogleChatUpdateOrganizationHandleRequestAttributes putAdditionalProperty(
+      String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /**
+   * Return true if this GoogleChatUpdateOrganizationHandleRequestAttributes object is equal to o.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GoogleChatUpdateOrganizationHandleRequestAttributes
+        googleChatUpdateOrganizationHandleRequestAttributes =
+            (GoogleChatUpdateOrganizationHandleRequestAttributes) o;
+    return Objects.equals(this.name, googleChatUpdateOrganizationHandleRequestAttributes.name)
+        && Objects.equals(
+            this.spaceResourceName,
+            googleChatUpdateOrganizationHandleRequestAttributes.spaceResourceName)
+        && Objects.equals(
+            this.additionalProperties,
+            googleChatUpdateOrganizationHandleRequestAttributes.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, spaceResourceName, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class GoogleChatUpdateOrganizationHandleRequestAttributes {\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    spaceResourceName: ").append(toIndentedString(spaceResourceName)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
