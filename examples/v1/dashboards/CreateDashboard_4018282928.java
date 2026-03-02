97093fcaac0d72aad5bdbf2a1a15af27f922834a
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-02 12:05:30 +0000

    Regenerate client from commit 4bfcbd9 of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_4018282928.java
@@ -0,0 +1,84 @@
+// Create a new dashboard with formulas and functions events query using flat group by fields
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v1.api.DashboardsApi;
+import com.datadog.api.client.v1.model.Dashboard;
+import com.datadog.api.client.v1.model.DashboardLayoutType;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventAggregation;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventQueryDefinition;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventQueryDefinitionCompute;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventQueryDefinitionSearch;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventQueryGroupByConfig;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventQueryGroupByFields;
+import com.datadog.api.client.v1.model.FormulaAndFunctionEventsDataSource;
+import com.datadog.api.client.v1.model.FormulaAndFunctionQueryDefinition;
+import com.datadog.api.client.v1.model.FormulaAndFunctionResponseFormat;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinition;
+import com.datadog.api.client.v1.model.TimeseriesWidgetDefinitionType;
+import com.datadog.api.client.v1.model.TimeseriesWidgetRequest;
+import com.datadog.api.client.v1.model.Widget;
+import com.datadog.api.client.v1.model.WidgetDefinition;
+import com.datadog.api.client.v1.model.WidgetLayout;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    Dashboard body =
+        new Dashboard()
+            .title("Example-Dashboard with events flat group_by fields")
+            .widgets(
+                Collections.singletonList(
+                    new Widget()
+                        .definition(
+                            new WidgetDefinition(
+                                new TimeseriesWidgetDefinition()
+                                    .type(TimeseriesWidgetDefinitionType.TIMESERIES)
+                                    .requests(
+                                        Collections.singletonList(
+                                            new TimeseriesWidgetRequest()
+                                                .responseFormat(
+                                                    FormulaAndFunctionResponseFormat.TIMESERIES)
+                                                .queries(
+                                                    Collections.singletonList(
+                                                        new FormulaAndFunctionQueryDefinition(
+                                                            new FormulaAndFunctionEventQueryDefinition()
+                                                                .dataSource(
+                                                                    FormulaAndFunctionEventsDataSource
+                                                                        .EVENTS)
+                                                                .name("query1")
+                                                                .search(
+                                                                    new FormulaAndFunctionEventQueryDefinitionSearch()
+                                                                        .query(""))
+                                                                .compute(
+                                                                    new FormulaAndFunctionEventQueryDefinitionCompute()
+                                                                        .aggregation(
+                                                                            FormulaAndFunctionEventAggregation
+                                                                                .COUNT))
+                                                                .groupBy(
+                                                                    new FormulaAndFunctionEventQueryGroupByConfig(
+                                                                        new FormulaAndFunctionEventQueryGroupByFields()
+                                                                            .fields(
+                                                                                Arrays.asList(
+                                                                                    "service",
+                                                                                    "host"))
+                                                                            .limit(10L))))))))))
+                        .layout(new WidgetLayout().x(0L).y(0L).width(4L).height(2L))))
+            .layoutType(DashboardLayoutType.ORDERED);
+
+    try {
+      Dashboard result = apiInstance.createDashboard(body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#createDashboard");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/main/java/com/datadog/api/client/v1/model/FormulaAndFunctionEventQueryGroupByConfig.java
@@ -0,0 +1,313 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v1.model;
+
+import com.datadog.api.client.AbstractOpenApiSchema;
+import com.datadog.api.client.JSON;
+import com.datadog.api.client.UnparsedObject;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import jakarta.ws.rs.core.GenericType;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+@JsonDeserialize(
+    using =
+        FormulaAndFunctionEventQueryGroupByConfig
+            .FormulaAndFunctionEventQueryGroupByConfigDeserializer.class)
+@JsonSerialize(
+    using =
+        FormulaAndFunctionEventQueryGroupByConfig
+            .FormulaAndFunctionEventQueryGroupByConfigSerializer.class)
+public class FormulaAndFunctionEventQueryGroupByConfig extends AbstractOpenApiSchema {
+  private static final Logger log =
+      Logger.getLogger(FormulaAndFunctionEventQueryGroupByConfig.class.getName());
+
+  @JsonIgnore public boolean unparsed = false;
+
+  public static class FormulaAndFunctionEventQueryGroupByConfigSerializer
+      extends StdSerializer<FormulaAndFunctionEventQueryGroupByConfig> {
+    public FormulaAndFunctionEventQueryGroupByConfigSerializer(
+        Class<FormulaAndFunctionEventQueryGroupByConfig> t) {
+      super(t);
+    }
+
+    public FormulaAndFunctionEventQueryGroupByConfigSerializer() {
+      this(null);
+    }
+
+    @Override
+    public void serialize(
+        FormulaAndFunctionEventQueryGroupByConfig value,
+        JsonGenerator jgen,
+        SerializerProvider provider)
+        throws IOException, JsonProcessingException {
+      jgen.writeObject(value.getActualInstance());
+    }
+  }
+
+  public static class FormulaAndFunctionEventQueryGroupByConfigDeserializer
+      extends StdDeserializer<FormulaAndFunctionEventQueryGroupByConfig> {
+    public FormulaAndFunctionEventQueryGroupByConfigDeserializer() {
+      this(FormulaAndFunctionEventQueryGroupByConfig.class);
+    }
+
+    public FormulaAndFunctionEventQueryGroupByConfigDeserializer(Class<?> vc) {
+      super(vc);
+    }
+
+    @Override
+    public FormulaAndFunctionEventQueryGroupByConfig deserialize(
+        JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+      JsonNode tree = jp.readValueAsTree();
+      Object deserialized = null;
+      Object tmp = null;
+      boolean typeCoercion = ctxt.isEnabled(MapperFeature.ALLOW_COERCION_OF_SCALARS);
+      int match = 0;
+      JsonToken token = tree.traverse(jp.getCodec()).nextToken();
+      // deserialize List<FormulaAndFunctionEventQueryGroupBy>
+      try {
+        boolean attemptParsing = true;
+        // ensure that we respect type coercion as set on the client ObjectMapper
+        if (List.class.equals(Integer.class)
+            || List.class.equals(Long.class)
+            || List.class.equals(Float.class)
+            || List.class.equals(Double.class)
+            || List.class.equals(Boolean.class)
+            || List.class.equals(String.class)) {
+          attemptParsing = typeCoercion;
+          if (!attemptParsing) {
+            attemptParsing |=
+                ((List.class.equals(Integer.class) || List.class.equals(Long.class))
+                    && token == JsonToken.VALUE_NUMBER_INT);
+            attemptParsing |=
+                ((List.class.equals(Float.class) || List.class.equals(Double.class))
+                    && (token == JsonToken.VALUE_NUMBER_FLOAT
+                        || token == JsonToken.VALUE_NUMBER_INT));
+            attemptParsing |=
+                (List.class.equals(Boolean.class)
+                    && (token == JsonToken.VALUE_FALSE || token == JsonToken.VALUE_TRUE));
+            attemptParsing |= (List.class.equals(String.class) && token == JsonToken.VALUE_STRING);
+          }
+        }
+        if (attemptParsing) {
+          tmp =
+              tree.traverse(jp.getCodec())
+                  .readValueAs(new TypeReference<List<FormulaAndFunctionEventQueryGroupBy>>() {});
+          // TODO: there is no validation against JSON schema constraints
+          // (min, max, enum, pattern...), this does not perform a strict JSON
+          // validation, which means the 'match' count may be higher than it should be.
+          deserialized = tmp;
+          match++;
+
+          log.log(
+              Level.FINER, "Input data matches schema 'List<FormulaAndFunctionEventQueryGroupBy>'");
+        }
+      } catch (Exception e) {
+        // deserialization failed, continue
+        log.log(
+            Level.FINER,
+            "Input data does not match schema 'List<FormulaAndFunctionEventQueryGroupBy>'",
+            e);
+      }
+
+      // deserialize FormulaAndFunctionEventQueryGroupByFields
+      try {
+        boolean attemptParsing = true;
+        // ensure that we respect type coercion as set on the client ObjectMapper
+        if (FormulaAndFunctionEventQueryGroupByFields.class.equals(Integer.class)
+            || FormulaAndFunctionEventQueryGroupByFields.class.equals(Long.class)
+            || FormulaAndFunctionEventQueryGroupByFields.class.equals(Float.class)
+            || FormulaAndFunctionEventQueryGroupByFields.class.equals(Double.class)
+            || FormulaAndFunctionEventQueryGroupByFields.class.equals(Boolean.class)
+            || FormulaAndFunctionEventQueryGroupByFields.class.equals(String.class)) {
+          attemptParsing = typeCoercion;
+          if (!attemptParsing) {
+            attemptParsing |=
+                ((FormulaAndFunctionEventQueryGroupByFields.class.equals(Integer.class)
+                        || FormulaAndFunctionEventQueryGroupByFields.class.equals(Long.class))
+                    && token == JsonToken.VALUE_NUMBER_INT);
+            attemptParsing |=
+                ((FormulaAndFunctionEventQueryGroupByFields.class.equals(Float.class)
+                        || FormulaAndFunctionEventQueryGroupByFields.class.equals(Double.class))
+                    && (token == JsonToken.VALUE_NUMBER_FLOAT
+                        || token == JsonToken.VALUE_NUMBER_INT));
+            attemptParsing |=
+                (FormulaAndFunctionEventQueryGroupByFields.class.equals(Boolean.class)
+                    && (token == JsonToken.VALUE_FALSE || token == JsonToken.VALUE_TRUE));
+            attemptParsing |=
+                (FormulaAndFunctionEventQueryGroupByFields.class.equals(String.class)
+                    && token == JsonToken.VALUE_STRING);
+          }
+        }
+        if (attemptParsing) {
+          tmp =
+              tree.traverse(jp.getCodec())
+                  .readValueAs(FormulaAndFunctionEventQueryGroupByFields.class);
+          // TODO: there is no validation against JSON schema constraints
+          // (min, max, enum, pattern...), this does not perform a strict JSON
+          // validation, which means the 'match' count may be higher than it should be.
+          if (!((FormulaAndFunctionEventQueryGroupByFields) tmp).unparsed) {
+            deserialized = tmp;
+            match++;
+          }
+          log.log(
+              Level.FINER, "Input data matches schema 'FormulaAndFunctionEventQueryGroupByFields'");
+        }
+      } catch (Exception e) {
+        // deserialization failed, continue
+        log.log(
+            Level.FINER,
+            "Input data does not match schema 'FormulaAndFunctionEventQueryGroupByFields'",
+            e);
+      }
+
+      FormulaAndFunctionEventQueryGroupByConfig ret =
+          new FormulaAndFunctionEventQueryGroupByConfig();
+      if (match == 1) {
+        ret.setActualInstance(deserialized);
+      } else {
+        Map<String, Object> res =
+            new ObjectMapper()
+                .readValue(
+                    tree.traverse(jp.getCodec()).readValueAsTree().toString(),
+                    new TypeReference<Map<String, Object>>() {});
+        ret.setActualInstance(new UnparsedObject(res));
+      }
+      return ret;
+    }
+
+    /** Handle deserialization of the 'null' value. */
+    @Override
+    public FormulaAndFunctionEventQueryGroupByConfig getNullValue(DeserializationContext ctxt)
+        throws JsonMappingException {
+      throw new JsonMappingException(
+          ctxt.getParser(), "FormulaAndFunctionEventQueryGroupByConfig cannot be null");
+    }
+  }
+
+  // store a list of schema names defined in oneOf
+  public static final Map<String, GenericType> schemas = new HashMap<String, GenericType>();
+
+  public FormulaAndFunctionEventQueryGroupByConfig() {
+    super("oneOf", Boolean.FALSE);
+  }
+
+  public FormulaAndFunctionEventQueryGroupByConfig(List<FormulaAndFunctionEventQueryGroupBy> o) {
+    super("oneOf", Boolean.FALSE);
+    setActualInstance(o);
+  }
+
+  public FormulaAndFunctionEventQueryGroupByConfig(FormulaAndFunctionEventQueryGroupByFields o) {
+    super("oneOf", Boolean.FALSE);
+    setActualInstance(o);
+  }
+
+  static {
+    schemas.put(
+        "List<FormulaAndFunctionEventQueryGroupBy>",
+        new GenericType<List<FormulaAndFunctionEventQueryGroupBy>>() {});
+    schemas.put(
+        "FormulaAndFunctionEventQueryGroupByFields",
+        new GenericType<FormulaAndFunctionEventQueryGroupByFields>() {});
+    JSON.registerDescendants(
+        FormulaAndFunctionEventQueryGroupByConfig.class, Collections.unmodifiableMap(schemas));
+  }
+
+  @Override
+  public Map<String, GenericType> getSchemas() {
+    return FormulaAndFunctionEventQueryGroupByConfig.schemas;
+  }
+
+  /**
+   * Set the instance that matches the oneOf child schema, check the instance parameter is valid
+   * against the oneOf child schemas: List&lt;FormulaAndFunctionEventQueryGroupBy&gt;,
+   * FormulaAndFunctionEventQueryGroupByFields
+   *
+   * <p>It could be an instance of the 'oneOf' schemas. The oneOf child schemas may themselves be a
+   * composed schema (allOf, anyOf, oneOf).
+   */
+  @Override
+  public void setActualInstance(Object instance) {
+    if (JSON.isInstanceOf(List.class, instance, new HashSet<Class<?>>())) {
+      super.setActualInstance(instance);
+      return;
+    }
+    if (JSON.isInstanceOf(
+        FormulaAndFunctionEventQueryGroupByFields.class, instance, new HashSet<Class<?>>())) {
+      super.setActualInstance(instance);
+      return;
+    }
+
+    if (JSON.isInstanceOf(UnparsedObject.class, instance, new HashSet<Class<?>>())) {
+      super.setActualInstance(instance);
+      return;
+    }
+    throw new RuntimeException(
+        "Invalid instance type. Must be List<FormulaAndFunctionEventQueryGroupBy>,"
+            + " FormulaAndFunctionEventQueryGroupByFields");
+  }
+
+  /**
+   * Get the actual instance, which can be the following:
+   * List&lt;FormulaAndFunctionEventQueryGroupBy&gt;, FormulaAndFunctionEventQueryGroupByFields
+   *
+   * @return The actual instance (List&lt;FormulaAndFunctionEventQueryGroupBy&gt;,
+   *     FormulaAndFunctionEventQueryGroupByFields)
+   */
+  @Override
+  public Object getActualInstance() {
+    return super.getActualInstance();
+  }
+
+  /**
+   * Get the actual instance of `List&lt;FormulaAndFunctionEventQueryGroupBy&gt;`. If the actual
+   * instance is not `List&lt;FormulaAndFunctionEventQueryGroupBy&gt;`, the ClassCastException will
+   * be thrown.
+   *
+   * @return The actual instance of `List&lt;FormulaAndFunctionEventQueryGroupBy&gt;`
+   * @throws ClassCastException if the instance is not
+   *     `List&lt;FormulaAndFunctionEventQueryGroupBy&gt;`
+   */
+  public List<FormulaAndFunctionEventQueryGroupBy> getList() throws ClassCastException {
+    return (List<FormulaAndFunctionEventQueryGroupBy>) super.getActualInstance();
+  }
+
+  /**
+   * Get the actual instance of `FormulaAndFunctionEventQueryGroupByFields`. If the actual instance
+   * is not `FormulaAndFunctionEventQueryGroupByFields`, the ClassCastException will be thrown.
+   *
+   * @return The actual instance of `FormulaAndFunctionEventQueryGroupByFields`
+   * @throws ClassCastException if the instance is not `FormulaAndFunctionEventQueryGroupByFields`
+   */
+  public FormulaAndFunctionEventQueryGroupByFields getFormulaAndFunctionEventQueryGroupByFields()
+      throws ClassCastException {
+    return (FormulaAndFunctionEventQueryGroupByFields) super.getActualInstance();
+  }
+}
