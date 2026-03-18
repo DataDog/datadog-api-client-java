beb690fbf305dbd7855333d42aefa2e8e1995816
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-18 09:00:53 +0000

    Regenerate client from commit 0250066 of spec repo

--- src/main/java/com/datadog/api/client/v1/model/MonitorFormulaAndFunctionCostQueryDefinition.java
+++ src/main/java/com/datadog/api/client/v1/model/MonitorFormulaAndFunctionCostQueryDefinition.java
@@ -32,7 +32,7 @@ public class MonitorFormulaAndFunctionCostQueryDefinition {
   private MonitorFormulaAndFunctionCostAggregator aggregator;
 
   public static final String JSON_PROPERTY_DATA_SOURCE = "data_source";
-  private MonitorFormulaAndFunctionCostDataSource dataSource;
+  private MonitorFormulaAndFunctionMetricsDataSource dataSource;
 
   public static final String JSON_PROPERTY_NAME = "name";
   private String name;
@@ -45,7 +45,7 @@ public class MonitorFormulaAndFunctionCostQueryDefinition {
   @JsonCreator
   public MonitorFormulaAndFunctionCostQueryDefinition(
       @JsonProperty(required = true, value = JSON_PROPERTY_DATA_SOURCE)
-          MonitorFormulaAndFunctionCostDataSource dataSource,
+          MonitorFormulaAndFunctionMetricsDataSource dataSource,
       @JsonProperty(required = true, value = JSON_PROPERTY_NAME) String name,
       @JsonProperty(required = true, value = JSON_PROPERTY_QUERY) String query) {
     this.dataSource = dataSource;
@@ -81,24 +81,24 @@ public class MonitorFormulaAndFunctionCostQueryDefinition {
   }
 
   public MonitorFormulaAndFunctionCostQueryDefinition dataSource(
-      MonitorFormulaAndFunctionCostDataSource dataSource) {
+      MonitorFormulaAndFunctionMetricsDataSource dataSource) {
     this.dataSource = dataSource;
     this.unparsed |= !dataSource.isValid();
     return this;
   }
 
   /**
-   * Data source for cost queries.
+   * Data source for metrics queries.
    *
    * @return dataSource
    */
   @JsonProperty(JSON_PROPERTY_DATA_SOURCE)
   @JsonInclude(value = JsonInclude.Include.ALWAYS)
-  public MonitorFormulaAndFunctionCostDataSource getDataSource() {
+  public MonitorFormulaAndFunctionMetricsDataSource getDataSource() {
     return dataSource;
   }
 
-  public void setDataSource(MonitorFormulaAndFunctionCostDataSource dataSource) {
+  public void setDataSource(MonitorFormulaAndFunctionMetricsDataSource dataSource) {
     if (!dataSource.isValid()) {
       this.unparsed = true;
     }
