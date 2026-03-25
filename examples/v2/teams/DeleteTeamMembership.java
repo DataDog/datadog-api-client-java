50d58bee08bb9eb1e97b18298259a9c6abc423c8
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-25 19:00:44 +0000

    Regenerate client from commit 646862c of spec repo

--- examples/v2/teams/DeleteTeamMembership.java
+++ examples/v2/teams/DeleteTeamMembership.java
@@ -12,8 +12,11 @@ public class Example {
     // there is a valid "dd_team" in the system
     String DD_TEAM_DATA_ID = System.getenv("DD_TEAM_DATA_ID");
 
+    // there is a valid "user" in the system
+    String USER_DATA_ID = System.getenv("USER_DATA_ID");
+
     try {
-      apiInstance.deleteTeamMembership(DD_TEAM_DATA_ID, "user_id");
+      apiInstance.deleteTeamMembership(DD_TEAM_DATA_ID, USER_DATA_ID);
     } catch (ApiException e) {
       System.err.println("Exception when calling TeamsApi#deleteTeamMembership");
       System.err.println("Status code: " + e.getCode());
