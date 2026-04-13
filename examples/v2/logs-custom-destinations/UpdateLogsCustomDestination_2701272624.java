05a91696f724684634d82c3b9bf57272f03265e0
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-13 14:48:27 +0000

    Regenerate client from commit f4e1087 of spec repo

---
+++ examples/v2/logs-custom-destinations/UpdateLogsCustomDestination_2701272624.java
@@ -0,0 +1,43 @@
+// Update a Splunk custom destination's attributes preserves the absent sourcetype returns "OK"
+// response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.LogsCustomDestinationsApi;
+import com.datadog.api.client.v2.model.CustomDestinationResponse;
+import com.datadog.api.client.v2.model.CustomDestinationType;
+import com.datadog.api.client.v2.model.CustomDestinationUpdateRequest;
+import com.datadog.api.client.v2.model.CustomDestinationUpdateRequestAttributes;
+import com.datadog.api.client.v2.model.CustomDestinationUpdateRequestDefinition;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    LogsCustomDestinationsApi apiInstance = new LogsCustomDestinationsApi(defaultClient);
+
+    // there is a valid "custom_destination_splunk" in the system
+    String CUSTOM_DESTINATION_SPLUNK_DATA_ID = System.getenv("CUSTOM_DESTINATION_SPLUNK_DATA_ID");
+
+    CustomDestinationUpdateRequest body =
+        new CustomDestinationUpdateRequest()
+            .data(
+                new CustomDestinationUpdateRequestDefinition()
+                    .attributes(
+                        new CustomDestinationUpdateRequestAttributes().name("Nginx logs (Updated)"))
+                    .type(CustomDestinationType.CUSTOM_DESTINATION)
+                    .id(CUSTOM_DESTINATION_SPLUNK_DATA_ID));
+
+    try {
+      CustomDestinationResponse result =
+          apiInstance.updateLogsCustomDestination(CUSTOM_DESTINATION_SPLUNK_DATA_ID, body);
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println(
+          "Exception when calling LogsCustomDestinationsApi#updateLogsCustomDestination");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
