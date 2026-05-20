59047f5ceca6dd208d426831388f331d226f3914
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-20 16:50:30 +0000

    Regenerate client from commit 4bd3443 of spec repo

--- examples/v2/app-builder/UpdateAppFavorite.java
+++ examples/v2/app-builder/UpdateAppFavorite.java
@@ -14,6 +14,14 @@ public class Example {
     ApiClient defaultClient = ApiClient.getDefaultApiClient();
     AppBuilderApi apiInstance = new AppBuilderApi(defaultClient);
 
+    // there is a valid "app" in the system
+    UUID APP_DATA_ID = null;
+    try {
+      APP_DATA_ID = UUID.fromString(System.getenv("APP_DATA_ID"));
+    } catch (IllegalArgumentException e) {
+      System.err.println("Error parsing UUID: " + e.getMessage());
+    }
+
     UpdateAppFavoriteRequest body =
         new UpdateAppFavoriteRequest()
             .data(
@@ -22,7 +30,7 @@ public class Example {
                     .type(AppFavoriteType.FAVORITES));
 
     try {
-      apiInstance.updateAppFavorite(UUID.fromString("65bb1f25-52e1-4510-9f8d-22d1516ed693"), body);
+      apiInstance.updateAppFavorite(APP_DATA_ID, body);
     } catch (ApiException e) {
       System.err.println("Exception when calling AppBuilderApi#updateAppFavorite");
       System.err.println("Status code: " + e.getCode());
