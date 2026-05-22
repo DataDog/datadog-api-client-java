c6fcd0f54eecd694afda8d4f23902190383cd251
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-22 18:29:23 +0000

    Regenerate client from commit 2c81a59 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/LLMObsDatasetDraftStateDataAttributes.java
@@ -0,0 +1,179 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** Attributes of an LLM Observability dataset draft state. */
+@JsonPropertyOrder({
+  LLMObsDatasetDraftStateDataAttributes.JSON_PROPERTY_DRAFTING_SINCE,
+  LLMObsDatasetDraftStateDataAttributes.JSON_PROPERTY_USER
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class LLMObsDatasetDraftStateDataAttributes {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_DRAFTING_SINCE = "drafting_since";
+  private OffsetDateTime draftingSince;
+
+  public static final String JSON_PROPERTY_USER = "user";
+  private LLMObsDatasetDraftStateUser user;
+
+  public LLMObsDatasetDraftStateDataAttributes() {}
+
+  @JsonCreator
+  public LLMObsDatasetDraftStateDataAttributes(
+      @JsonProperty(required = true, value = JSON_PROPERTY_DRAFTING_SINCE)
+          OffsetDateTime draftingSince,
+      @JsonProperty(required = true, value = JSON_PROPERTY_USER) LLMObsDatasetDraftStateUser user) {
+    this.draftingSince = draftingSince;
+    this.user = user;
+    this.unparsed |= user.unparsed;
+  }
+
+  public LLMObsDatasetDraftStateDataAttributes draftingSince(OffsetDateTime draftingSince) {
+    this.draftingSince = draftingSince;
+    return this;
+  }
+
+  /**
+   * Timestamp when the dataset draft session started.
+   *
+   * @return draftingSince
+   */
+  @JsonProperty(JSON_PROPERTY_DRAFTING_SINCE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public OffsetDateTime getDraftingSince() {
+    return draftingSince;
+  }
+
+  public void setDraftingSince(OffsetDateTime draftingSince) {
+    this.draftingSince = draftingSince;
+  }
+
+  public LLMObsDatasetDraftStateDataAttributes user(LLMObsDatasetDraftStateUser user) {
+    this.user = user;
+    this.unparsed |= user.unparsed;
+    return this;
+  }
+
+  /**
+   * User information associated with a dataset draft state.
+   *
+   * @return user
+   */
+  @JsonProperty(JSON_PROPERTY_USER)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public LLMObsDatasetDraftStateUser getUser() {
+    return user;
+  }
+
+  public void setUser(LLMObsDatasetDraftStateUser user) {
+    this.user = user;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return LLMObsDatasetDraftStateDataAttributes
+   */
+  @JsonAnySetter
+  public LLMObsDatasetDraftStateDataAttributes putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this LLMObsDatasetDraftStateDataAttributes object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    LLMObsDatasetDraftStateDataAttributes llmObsDatasetDraftStateDataAttributes =
+        (LLMObsDatasetDraftStateDataAttributes) o;
+    return Objects.equals(this.draftingSince, llmObsDatasetDraftStateDataAttributes.draftingSince)
+        && Objects.equals(this.user, llmObsDatasetDraftStateDataAttributes.user)
+        && Objects.equals(
+            this.additionalProperties, llmObsDatasetDraftStateDataAttributes.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(draftingSince, user, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class LLMObsDatasetDraftStateDataAttributes {\n");
+    sb.append("    draftingSince: ").append(toIndentedString(draftingSince)).append("\n");
+    sb.append("    user: ").append(toIndentedString(user)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ src/main/java/com/datadog/api/client/v2/model/LLMObsDatasetDraftStateUser.java
@@ -0,0 +1,254 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** User information associated with a dataset draft state. */
+@JsonPropertyOrder({
+  LLMObsDatasetDraftStateUser.JSON_PROPERTY_EMAIL,
+  LLMObsDatasetDraftStateUser.JSON_PROPERTY_HANDLE,
+  LLMObsDatasetDraftStateUser.JSON_PROPERTY_ICON,
+  LLMObsDatasetDraftStateUser.JSON_PROPERTY_ID,
+  LLMObsDatasetDraftStateUser.JSON_PROPERTY_NAME
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class LLMObsDatasetDraftStateUser {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_EMAIL = "email";
+  private String email;
+
+  public static final String JSON_PROPERTY_HANDLE = "handle";
+  private String handle;
+
+  public static final String JSON_PROPERTY_ICON = "icon";
+  private String icon;
+
+  public static final String JSON_PROPERTY_ID = "id";
+  private String id;
+
+  public static final String JSON_PROPERTY_NAME = "name";
+  private String name;
+
+  public LLMObsDatasetDraftStateUser() {}
+
+  @JsonCreator
+  public LLMObsDatasetDraftStateUser(
+      @JsonProperty(required = true, value = JSON_PROPERTY_ID) String id) {
+    this.id = id;
+  }
+
+  public LLMObsDatasetDraftStateUser email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  /**
+   * Email address of the user.
+   *
+   * @return email
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_EMAIL)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public LLMObsDatasetDraftStateUser handle(String handle) {
+    this.handle = handle;
+    return this;
+  }
+
+  /**
+   * Handle of the user.
+   *
+   * @return handle
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_HANDLE)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getHandle() {
+    return handle;
+  }
+
+  public void setHandle(String handle) {
+    this.handle = handle;
+  }
+
+  public LLMObsDatasetDraftStateUser icon(String icon) {
+    this.icon = icon;
+    return this;
+  }
+
+  /**
+   * Icon for the user.
+   *
+   * @return icon
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ICON)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getIcon() {
+    return icon;
+  }
+
+  public void setIcon(String icon) {
+    this.icon = icon;
+  }
+
+  public LLMObsDatasetDraftStateUser id(String id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Unique identifier of the user holding the draft lock.
+   *
+   * @return id
+   */
+  @JsonProperty(JSON_PROPERTY_ID)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public LLMObsDatasetDraftStateUser name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Display name of the user.
+   *
+   * @return name
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return LLMObsDatasetDraftStateUser
+   */
+  @JsonAnySetter
+  public LLMObsDatasetDraftStateUser putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this LLMObsDatasetDraftStateUser object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    LLMObsDatasetDraftStateUser llmObsDatasetDraftStateUser = (LLMObsDatasetDraftStateUser) o;
+    return Objects.equals(this.email, llmObsDatasetDraftStateUser.email)
+        && Objects.equals(this.handle, llmObsDatasetDraftStateUser.handle)
+        && Objects.equals(this.icon, llmObsDatasetDraftStateUser.icon)
+        && Objects.equals(this.id, llmObsDatasetDraftStateUser.id)
+        && Objects.equals(this.name, llmObsDatasetDraftStateUser.name)
+        && Objects.equals(
+            this.additionalProperties, llmObsDatasetDraftStateUser.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(email, handle, icon, id, name, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class LLMObsDatasetDraftStateUser {\n");
+    sb.append("    email: ").append(toIndentedString(email)).append("\n");
+    sb.append("    handle: ").append(toIndentedString(handle)).append("\n");
+    sb.append("    icon: ").append(toIndentedString(icon)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
