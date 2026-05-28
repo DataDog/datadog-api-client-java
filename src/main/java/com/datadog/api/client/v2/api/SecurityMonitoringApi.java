24bb77eeb52d3066f5dea28d3d780292ec70d71d
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-28 13:59:18 +0000

    Regenerate client from commit 8593b27 of spec repo

--- src/main/java/com/datadog/api/client/v2/api/SecurityMonitoringApi.java
+++ src/main/java/com/datadog/api/client/v2/api/SecurityMonitoringApi.java
@@ -19991,13 +19991,6 @@ public class SecurityMonitoringApi {
    */
   public ApiResponse<MuteFindingsResponse> muteSecurityFindingsWithHttpInfo(
       MuteFindingsRequest body) throws ApiException {
-    // Check if unstable operation is enabled
-    String operationId = "muteSecurityFindings";
-    if (apiClient.isUnstableOperationEnabled("v2." + operationId)) {
-      apiClient.getLogger().warning(String.format("Using unstable operation '%s'", operationId));
-    } else {
-      throw new ApiException(0, String.format("Unstable operation '%s' is disabled", operationId));
-    }
     Object localVarPostBody = body;
 
     // verify the required parameter 'body' is set
@@ -20040,16 +20033,6 @@ public class SecurityMonitoringApi {
    */
   public CompletableFuture<ApiResponse<MuteFindingsResponse>> muteSecurityFindingsWithHttpInfoAsync(
       MuteFindingsRequest body) {
-    // Check if unstable operation is enabled
-    String operationId = "muteSecurityFindings";
-    if (apiClient.isUnstableOperationEnabled("v2." + operationId)) {
-      apiClient.getLogger().warning(String.format("Using unstable operation '%s'", operationId));
-    } else {
-      CompletableFuture<ApiResponse<MuteFindingsResponse>> result = new CompletableFuture<>();
-      result.completeExceptionally(
-          new ApiException(0, String.format("Unstable operation '%s' is disabled", operationId)));
-      return result;
-    }
     Object localVarPostBody = body;
 
     // verify the required parameter 'body' is set
