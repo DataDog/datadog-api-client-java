40fc22826675bdfcaf7dd3d74bfceb70c9e9ea60
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-01 20:06:24 +0000

    Regenerate client from commit 6de6059 of spec repo

---
+++ examples/v2/dashboards/ListDashboardsUsage_3946782296.java
@@ -0,0 +1,29 @@
+// Get usage stats for all dashboards with viewed_before filter returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.DashboardsApi;
+import com.datadog.api.client.v2.api.DashboardsApi.ListDashboardsUsageOptionalParameters;
+import com.datadog.api.client.v2.model.ListDashboardsUsageResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    defaultClient.setUnstableOperationEnabled("v2.listDashboardsUsage", true);
+    DashboardsApi apiInstance = new DashboardsApi(defaultClient);
+
+    try {
+      ListDashboardsUsageResponse result =
+          apiInstance.listDashboardsUsage(
+              new ListDashboardsUsageOptionalParameters()
+                  .filterViewedBefore("2025-04-26T00:00:00Z"));
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling DashboardsApi#listDashboardsUsage");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
