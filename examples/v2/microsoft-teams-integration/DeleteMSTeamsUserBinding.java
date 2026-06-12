9b0e236ccc07e58e89d3592a6e39794fcaae7849
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-12 08:58:32 +0000

    Regenerate client from commit 34e91d4 of spec repo

---
+++ examples/v2/microsoft-teams-integration/DeleteMSTeamsUserBinding.java
@@ -0,0 +1,23 @@
+// Delete user binding returns "No Content" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.MicrosoftTeamsIntegrationApi;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    MicrosoftTeamsIntegrationApi apiInstance = new MicrosoftTeamsIntegrationApi(defaultClient);
+
+    try {
+      apiInstance.deleteMSTeamsUserBinding("tenant_id");
+    } catch (ApiException e) {
+      System.err.println(
+          "Exception when calling MicrosoftTeamsIntegrationApi#deleteMSTeamsUserBinding");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
