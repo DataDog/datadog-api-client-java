fb29d77a01eaac2bcd657696c666156114a39861
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-01 12:05:05 +0000

    Regenerate client from commit 380f1c0 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/ObservabilityPipelineParseGrokProcessorIncludeRule.java
@@ -0,0 +1,242 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * A Grok parsing rule selected using the <code>include</code> query. Each rule defines how to
+ * extract structured fields from logs matching a Datadog search query.
+ */
+@JsonPropertyOrder({
+  ObservabilityPipelineParseGrokProcessorIncludeRule.JSON_PROPERTY_INCLUDE,
+  ObservabilityPipelineParseGrokProcessorIncludeRule.JSON_PROPERTY_MATCH_RULES,
+  ObservabilityPipelineParseGrokProcessorIncludeRule.JSON_PROPERTY_SUPPORT_RULES
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class ObservabilityPipelineParseGrokProcessorIncludeRule {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_INCLUDE = "include";
+  private String include;
+
+  public static final String JSON_PROPERTY_MATCH_RULES = "match_rules";
+  private List<ObservabilityPipelineParseGrokProcessorRuleMatchRule> matchRules = new ArrayList<>();
+
+  public static final String JSON_PROPERTY_SUPPORT_RULES = "support_rules";
+  private List<ObservabilityPipelineParseGrokProcessorRuleSupportRule> supportRules = null;
+
+  public ObservabilityPipelineParseGrokProcessorIncludeRule() {}
+
+  @JsonCreator
+  public ObservabilityPipelineParseGrokProcessorIncludeRule(
+      @JsonProperty(required = true, value = JSON_PROPERTY_INCLUDE) String include,
+      @JsonProperty(required = true, value = JSON_PROPERTY_MATCH_RULES)
+          List<ObservabilityPipelineParseGrokProcessorRuleMatchRule> matchRules) {
+    this.include = include;
+    this.matchRules = matchRules;
+  }
+
+  public ObservabilityPipelineParseGrokProcessorIncludeRule include(String include) {
+    this.include = include;
+    return this;
+  }
+
+  /**
+   * A Datadog search query used to determine which logs this Grok rule targets.
+   *
+   * @return include
+   */
+  @JsonProperty(JSON_PROPERTY_INCLUDE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public String getInclude() {
+    return include;
+  }
+
+  public void setInclude(String include) {
+    this.include = include;
+  }
+
+  public ObservabilityPipelineParseGrokProcessorIncludeRule matchRules(
+      List<ObservabilityPipelineParseGrokProcessorRuleMatchRule> matchRules) {
+    this.matchRules = matchRules;
+    for (ObservabilityPipelineParseGrokProcessorRuleMatchRule item : matchRules) {
+      this.unparsed |= item.unparsed;
+    }
+    return this;
+  }
+
+  public ObservabilityPipelineParseGrokProcessorIncludeRule addMatchRulesItem(
+      ObservabilityPipelineParseGrokProcessorRuleMatchRule matchRulesItem) {
+    this.matchRules.add(matchRulesItem);
+    this.unparsed |= matchRulesItem.unparsed;
+    return this;
+  }
+
+  /**
+   * A list of Grok parsing rules that define how to extract fields from matching logs. Each rule
+   * must contain a name and a valid Grok pattern.
+   *
+   * @return matchRules
+   */
+  @JsonProperty(JSON_PROPERTY_MATCH_RULES)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public List<ObservabilityPipelineParseGrokProcessorRuleMatchRule> getMatchRules() {
+    return matchRules;
+  }
+
+  public void setMatchRules(List<ObservabilityPipelineParseGrokProcessorRuleMatchRule> matchRules) {
+    this.matchRules = matchRules;
+  }
+
+  public ObservabilityPipelineParseGrokProcessorIncludeRule supportRules(
+      List<ObservabilityPipelineParseGrokProcessorRuleSupportRule> supportRules) {
+    this.supportRules = supportRules;
+    for (ObservabilityPipelineParseGrokProcessorRuleSupportRule item : supportRules) {
+      this.unparsed |= item.unparsed;
+    }
+    return this;
+  }
+
+  public ObservabilityPipelineParseGrokProcessorIncludeRule addSupportRulesItem(
+      ObservabilityPipelineParseGrokProcessorRuleSupportRule supportRulesItem) {
+    if (this.supportRules == null) {
+      this.supportRules = new ArrayList<>();
+    }
+    this.supportRules.add(supportRulesItem);
+    this.unparsed |= supportRulesItem.unparsed;
+    return this;
+  }
+
+  /**
+   * A list of Grok helper rules that can be referenced by the parsing rules.
+   *
+   * @return supportRules
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_SUPPORT_RULES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public List<ObservabilityPipelineParseGrokProcessorRuleSupportRule> getSupportRules() {
+    return supportRules;
+  }
+
+  public void setSupportRules(
+      List<ObservabilityPipelineParseGrokProcessorRuleSupportRule> supportRules) {
+    this.supportRules = supportRules;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return ObservabilityPipelineParseGrokProcessorIncludeRule
+   */
+  @JsonAnySetter
+  public ObservabilityPipelineParseGrokProcessorIncludeRule putAdditionalProperty(
+      String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /**
+   * Return true if this ObservabilityPipelineParseGrokProcessorIncludeRule object is equal to o.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ObservabilityPipelineParseGrokProcessorIncludeRule
+        observabilityPipelineParseGrokProcessorIncludeRule =
+            (ObservabilityPipelineParseGrokProcessorIncludeRule) o;
+    return Objects.equals(this.include, observabilityPipelineParseGrokProcessorIncludeRule.include)
+        && Objects.equals(
+            this.matchRules, observabilityPipelineParseGrokProcessorIncludeRule.matchRules)
+        && Objects.equals(
+            this.supportRules, observabilityPipelineParseGrokProcessorIncludeRule.supportRules)
+        && Objects.equals(
+            this.additionalProperties,
+            observabilityPipelineParseGrokProcessorIncludeRule.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(include, matchRules, supportRules, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ObservabilityPipelineParseGrokProcessorIncludeRule {\n");
+    sb.append("    include: ").append(toIndentedString(include)).append("\n");
+    sb.append("    matchRules: ").append(toIndentedString(matchRules)).append("\n");
+    sb.append("    supportRules: ").append(toIndentedString(supportRules)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
