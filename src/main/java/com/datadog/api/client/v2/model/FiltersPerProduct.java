f813fa44532f2717c103ecdb5a7aa3be90372407
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-08 22:04:29 +0000

    Regenerate client from commit 39c15b7 of spec repo

--- src/main/java/com/datadog/api/client/v2/model/FiltersPerProduct.java
+++ src/main/java/com/datadog/api/client/v2/model/FiltersPerProduct.java
@@ -79,7 +79,7 @@ public class FiltersPerProduct {
 
   /**
    * Name of the product the dataset is for. Possible values are 'apm', 'rum', 'metrics', 'logs',
-   * 'error_tracking', 'cloud_cost', and 'sd_repoinfo'.
+   * 'error_tracking', 'cloud_cost', 'sd_repoinfo', and 'secruntime'.
    *
    * @return product
    */
