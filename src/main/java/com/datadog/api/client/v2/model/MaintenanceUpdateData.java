4e101f40a8ccd0d9f65b2947ebd88d25682e8179
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-07 15:26:12 +0000

    Regenerate client from commit 1e0b6ab of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/MaintenanceUpdateData.java
@@ -0,0 +1,243 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+/** The data object for a maintenance update. */
+@JsonPropertyOrder({
+  MaintenanceUpdateData.JSON_PROPERTY_ATTRIBUTES,
+  MaintenanceUpdateData.JSON_PROPERTY_ID,
+  MaintenanceUpdateData.JSON_PROPERTY_RELATIONSHIPS,
+  MaintenanceUpdateData.JSON_PROPERTY_TYPE
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class MaintenanceUpdateData {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ATTRIBUTES = "attributes";
+  private MaintenanceUpdateDataAttributes attributes;
+
+  public static final String JSON_PROPERTY_ID = "id";
+  private UUID id;
+
+  public static final String JSON_PROPERTY_RELATIONSHIPS = "relationships";
+  private MaintenanceUpdateDataRelationships relationships;
+
+  public static final String JSON_PROPERTY_TYPE = "type";
+  private PatchMaintenanceUpdateRequestDataType type =
+      PatchMaintenanceUpdateRequestDataType.MAINTENANCE_UPDATES;
+
+  public MaintenanceUpdateData() {}
+
+  @JsonCreator
+  public MaintenanceUpdateData(
+      @JsonProperty(required = true, value = JSON_PROPERTY_ID) UUID id,
+      @JsonProperty(required = true, value = JSON_PROPERTY_TYPE)
+          PatchMaintenanceUpdateRequestDataType type) {
+    this.id = id;
+    this.type = type;
+    this.unparsed |= !type.isValid();
+  }
+
+  public MaintenanceUpdateData attributes(MaintenanceUpdateDataAttributes attributes) {
+    this.attributes = attributes;
+    this.unparsed |= attributes.unparsed;
+    return this;
+  }
+
+  /**
+   * Attributes of a maintenance update resource.
+   *
+   * @return attributes
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ATTRIBUTES)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public MaintenanceUpdateDataAttributes getAttributes() {
+    return attributes;
+  }
+
+  public void setAttributes(MaintenanceUpdateDataAttributes attributes) {
+    this.attributes = attributes;
+    if (attributes != null) {
+      this.unparsed |= attributes.unparsed;
+    }
+  }
+
+  public MaintenanceUpdateData id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * The ID of the maintenance update.
+   *
+   * @return id
+   */
+  @JsonProperty(JSON_PROPERTY_ID)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public MaintenanceUpdateData relationships(MaintenanceUpdateDataRelationships relationships) {
+    this.relationships = relationships;
+    this.unparsed |= relationships.unparsed;
+    return this;
+  }
+
+  /**
+   * Relationships of a maintenance update resource.
+   *
+   * @return relationships
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_RELATIONSHIPS)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public MaintenanceUpdateDataRelationships getRelationships() {
+    return relationships;
+  }
+
+  public void setRelationships(MaintenanceUpdateDataRelationships relationships) {
+    this.relationships = relationships;
+    if (relationships != null) {
+      this.unparsed |= relationships.unparsed;
+    }
+  }
+
+  public MaintenanceUpdateData type(PatchMaintenanceUpdateRequestDataType type) {
+    this.type = type;
+    this.unparsed |= !type.isValid();
+    return this;
+  }
+
+  /**
+   * Maintenance updates resource type.
+   *
+   * @return type
+   */
+  @JsonProperty(JSON_PROPERTY_TYPE)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+  public PatchMaintenanceUpdateRequestDataType getType() {
+    return type;
+  }
+
+  public void setType(PatchMaintenanceUpdateRequestDataType type) {
+    if (!type.isValid()) {
+      this.unparsed = true;
+    }
+    this.type = type;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return MaintenanceUpdateData
+   */
+  @JsonAnySetter
+  public MaintenanceUpdateData putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this MaintenanceUpdateData object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MaintenanceUpdateData maintenanceUpdateData = (MaintenanceUpdateData) o;
+    return Objects.equals(this.attributes, maintenanceUpdateData.attributes)
+        && Objects.equals(this.id, maintenanceUpdateData.id)
+        && Objects.equals(this.relationships, maintenanceUpdateData.relationships)
+        && Objects.equals(this.type, maintenanceUpdateData.type)
+        && Objects.equals(this.additionalProperties, maintenanceUpdateData.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(attributes, id, relationships, type, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MaintenanceUpdateData {\n");
+    sb.append("    attributes: ").append(toIndentedString(attributes)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    relationships: ").append(toIndentedString(relationships)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
