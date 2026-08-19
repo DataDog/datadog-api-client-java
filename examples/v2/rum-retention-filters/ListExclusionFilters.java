a18d8f23d14e1522ef8f38d3bcfb3cdff4724812
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-19 21:54:59 +0000

    Regenerate client from commit 5ea08be of spec repo

---
+++ examples/v2/rum-retention-filters/ListExclusionFilters.java
@@ -0,0 +1,24 @@
+// Get all RUM exclusion filters returns "OK" response
+
+import com.datadog.api.client.ApiClient;
+import com.datadog.api.client.ApiException;
+import com.datadog.api.client.v2.api.RumRetentionFiltersApi;
+import com.datadog.api.client.v2.model.RumExclusionFiltersResponse;
+
+public class Example {
+  public static void main(String[] args) {
+    ApiClient defaultClient = ApiClient.getDefaultApiClient();
+    RumRetentionFiltersApi apiInstance = new RumRetentionFiltersApi(defaultClient);
+
+    try {
+      RumExclusionFiltersResponse result = apiInstance.listExclusionFilters("app_id");
+      System.out.println(result);
+    } catch (ApiException e) {
+      System.err.println("Exception when calling RumRetentionFiltersApi#listExclusionFilters");
+      System.err.println("Status code: " + e.getCode());
+      System.err.println("Reason: " + e.getResponseBody());
+      System.err.println("Response headers: " + e.getResponseHeaders());
+      e.printStackTrace();
+    }
+  }
+}
