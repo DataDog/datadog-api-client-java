cae0f0b3018920d98d64d6423f20dab1573b5ac8
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-20 19:59:38 +0000

    Regenerate client from commit eeac4ac of spec repo

--- src/main/java/com/datadog/api/client/v2/model/Spec.java
+++ src/main/java/com/datadog/api/client/v2/model/Spec.java
@@ -73,7 +73,7 @@ public class Spec {
   }
 
   /**
-   * A list of annotations used in the workflow. These are like sticky notes for your workflow!
+   * Up to 100 text annotations displayed on the workflow canvas.
    *
    * @return annotations
    */
