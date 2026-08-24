c5bcfbc603d10e20fce5e4b4d12363f535955be1
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-24 12:31:36 +0000

    Regenerate client from commit 9ac8b28 of spec repo

---
+++ src/main/java/com/datadog/api/client/v2/model/RumExclusionFilterAttributes.java
@@ -0,0 +1,224 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2019-Present Datadog, Inc.
+ */
+
+package com.datadog.api.client.v2.model;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/** The attributes of an exclusion filter. */
+@JsonPropertyOrder({
+  RumExclusionFilterAttributes.JSON_PROPERTY_ENABLED,
+  RumExclusionFilterAttributes.JSON_PROPERTY_EVENT_TYPE,
+  RumExclusionFilterAttributes.JSON_PROPERTY_NAME,
+  RumExclusionFilterAttributes.JSON_PROPERTY_QUERY
+})
+@jakarta.annotation.Generated(
+    value = "https://github.com/DataDog/datadog-api-client-java/blob/master/.generator")
+public class RumExclusionFilterAttributes {
+  @JsonIgnore public boolean unparsed = false;
+  public static final String JSON_PROPERTY_ENABLED = "enabled";
+  private Boolean enabled;
+
+  public static final String JSON_PROPERTY_EVENT_TYPE = "event_type";
+  private RumExclusionFilterEventType eventType;
+
+  public static final String JSON_PROPERTY_NAME = "name";
+  private String name;
+
+  public static final String JSON_PROPERTY_QUERY = "query";
+  private String query;
+
+  public RumExclusionFilterAttributes enabled(Boolean enabled) {
+    this.enabled = enabled;
+    return this;
+  }
+
+  /**
+   * Whether the exclusion filter is active.
+   *
+   * @return enabled
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_ENABLED)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public Boolean getEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(Boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public RumExclusionFilterAttributes eventType(RumExclusionFilterEventType eventType) {
+    this.eventType = eventType;
+    this.unparsed |= !eventType.isValid();
+    return this;
+  }
+
+  /**
+   * The type of RUM events to filter on.
+   *
+   * @return eventType
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_EVENT_TYPE)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public RumExclusionFilterEventType getEventType() {
+    return eventType;
+  }
+
+  public void setEventType(RumExclusionFilterEventType eventType) {
+    if (!eventType.isValid()) {
+      this.unparsed = true;
+    }
+    this.eventType = eventType;
+  }
+
+  public RumExclusionFilterAttributes name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * The name of the exclusion filter.
+   *
+   * @return name
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_NAME)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public RumExclusionFilterAttributes query(String query) {
+    this.query = query;
+    return this;
+  }
+
+  /**
+   * Additional query used to further restrict which RUM events are excluded. Combined with <code>
+   * event_type</code> when both are provided.
+   *
+   * @return query
+   */
+  @jakarta.annotation.Nullable
+  @JsonProperty(JSON_PROPERTY_QUERY)
+  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
+  public String getQuery() {
+    return query;
+  }
+
+  public void setQuery(String query) {
+    this.query = query;
+  }
+
+  /**
+   * A container for additional, undeclared properties. This is a holder for any undeclared
+   * properties as specified with the 'additionalProperties' keyword in the OAS document.
+   */
+  private Map<String, Object> additionalProperties;
+
+  /**
+   * Set the additional (undeclared) property with the specified name and value. If the property
+   * does not already exist, create it otherwise replace it.
+   *
+   * @param key The arbitrary key to set
+   * @param value The associated value
+   * @return RumExclusionFilterAttributes
+   */
+  @JsonAnySetter
+  public RumExclusionFilterAttributes putAdditionalProperty(String key, Object value) {
+    if (this.additionalProperties == null) {
+      this.additionalProperties = new HashMap<String, Object>();
+    }
+    this.additionalProperties.put(key, value);
+    return this;
+  }
+
+  /**
+   * Return the additional (undeclared) property.
+   *
+   * @return The additional properties
+   */
+  @JsonAnyGetter
+  public Map<String, Object> getAdditionalProperties() {
+    return additionalProperties;
+  }
+
+  /**
+   * Return the additional (undeclared) property with the specified name.
+   *
+   * @param key The arbitrary key to get
+   * @return The specific additional property for the given key
+   */
+  public Object getAdditionalProperty(String key) {
+    if (this.additionalProperties == null) {
+      return null;
+    }
+    return this.additionalProperties.get(key);
+  }
+
+  /** Return true if this RumExclusionFilterAttributes object is equal to o. */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    RumExclusionFilterAttributes rumExclusionFilterAttributes = (RumExclusionFilterAttributes) o;
+    return Objects.equals(this.enabled, rumExclusionFilterAttributes.enabled)
+        && Objects.equals(this.eventType, rumExclusionFilterAttributes.eventType)
+        && Objects.equals(this.name, rumExclusionFilterAttributes.name)
+        && Objects.equals(this.query, rumExclusionFilterAttributes.query)
+        && Objects.equals(
+            this.additionalProperties, rumExclusionFilterAttributes.additionalProperties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(enabled, eventType, name, query, additionalProperties);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class RumExclusionFilterAttributes {\n");
+    sb.append("    enabled: ").append(toIndentedString(enabled)).append("\n");
+    sb.append("    eventType: ").append(toIndentedString(eventType)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    query: ").append(toIndentedString(query)).append("\n");
+    sb.append("    additionalProperties: ")
+        .append(toIndentedString(additionalProperties))
+        .append("\n");
+    sb.append('}');
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
