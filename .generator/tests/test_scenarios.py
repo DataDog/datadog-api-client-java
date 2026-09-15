6388e5477eb176402cf3cd88650898067bd0bebc
David Tapiador <nogates@users.noreply.github.com>
2026-09-15 12:38:15 +0200

    Remove local generator code (#4327)

--- .generator/tests/test_scenarios.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-"""Test scenarios."""
-
-from pytest_bdd import scenarios
-
-scenarios(
-    "../../src/test/resources/com/datadog/api/client/v1/api", "../../src/test/resources/com/datadog/api/client/v2/api"
-)
